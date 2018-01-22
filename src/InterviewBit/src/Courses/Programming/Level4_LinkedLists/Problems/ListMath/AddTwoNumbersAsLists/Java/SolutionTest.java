f7c329175fb9816ce2362d3cbbd655a857069268
Manas Talukdar <manas.talukdar@gmail.com>
2018-01-21 21:34:53 -0800

    Add Two Numbers As Lists

---
+++ src/InterviewBit/src/Courses/Programming/Level4_LinkedLists/Problems/ListMath/AddTwoNumbersAsLists/Java/SolutionTest.java
@@ -0,0 +1,43 @@
+package InterviewBit.src.Courses.Programming.Level4_LinkedLists.Problems.ListMath.AddTwoNumbersAsLists.Java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class SolutionTest {
+    
+    Solution solution;
+    
+    @BeforeEach
+    public void setUp() throws Exception {
+        solution = new Solution();
+    }
+    
+    @AfterEach
+    public void tearDown() throws Exception {
+        solution = null;
+    }
+    
+    @Test
+    public void TrivialCase1() {
+        ListNode a1 = new ListNode(2);
+        ListNode a2 = new ListNode(4);
+        ListNode a3 = new ListNode(3);
+        a1.next = a2;
+        a2.next = a3;
+        ListNode b1 = new ListNode(5);
+        ListNode b2 = new ListNode(6);
+        ListNode b3 = new ListNode(4);
+        b1.next = b2;
+        b2.next = b3;
+        assertTimeout(Duration.ofMillis(200), () -> {
+            long expected = 708;
+            ListNode actualListNode = Solution.addTwoNumbers(a1, b1);
+            long actual = Solution.getNumber(actualListNode);
+            assertEquals(expected, actual);
+        });
+    }
+}
