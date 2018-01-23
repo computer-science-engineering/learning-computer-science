471b1f06c2ef5bb81502d41ea702c8b19470e153
Manas Talukdar <manas.talukdar@gmail.com>
2018-01-22 16:40:15 -0800

    Generate All Parentheses

---
+++ src/InterviewBit/src/Courses/Programming/Level4_StacksAndQueues/Examples/GenerateAllParentheses/Java/SolutionTest.java
@@ -0,0 +1,43 @@
+package InterviewBit.src.Courses.Programming.Level4_StacksAndQueues.Examples.GenerateAllParentheses.Java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class SolutionTest {
+    
+    Solution solution;
+    
+    @BeforeEach
+    public void setUp() throws Exception {
+        solution = new Solution();
+    }
+    
+    @AfterEach
+    public void tearDown() throws Exception {
+        solution = null;
+    }
+    
+    @Test
+    public void TrivialCase1() {
+        String input = "()[]{}";
+        assertTimeout(Duration.ofMillis(200), () -> {
+            int expected = 1;
+            int actual = Solution.isValid(input);
+            assertEquals(expected, actual);
+        });
+    }
+
+    @Test
+    public void SpecialCase1() {
+        String input = "])";
+        assertTimeout(Duration.ofMillis(200), () -> {
+            int expected = 0;
+            int actual = Solution.isValid(input);
+            assertEquals(expected, actual);
+        });
+    }
+}
---
+++ src/InterviewBit/src/Courses/Programming/Level4_StacksAndQueues/Examples/GenerateAllParentheses/readme.md
@@ -0,0 +1,22 @@
+# Generate All Parentheses
+
+## Link
+
+[Here](https://www.interviewbit.com/problems/generate-all-parentheses/)
+
+## Description
+
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
+
+Return 0 / 1 ( 0 for false, 1 for true ) for this problem
+
+## Companies asked in
+
+1. Amazon
+
+## Notes
+
+1. [CodeSchool video](https://www.youtube.com/watch?v=QZOLb0xHB_Q)
+1. [Clark Feusier blog](http://clarkfeusier.com/2015/01/16/interview-question-balanced-parentheses-stack.html)
