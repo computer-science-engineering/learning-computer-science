04a7199e75170a8e16d266228913be1a2bcd370e
Manas Talukdar <manas.talukdar@gmail.com>
2018-01-25 23:36:25 -0800

    Evaluate Expression in Reverse Polish Notation.

---
+++ src/InterviewBit/src/Courses/Programming/Level4_StacksAndQueues/Problems/StackMath/EvaluateExpression/Java/SolutionTest.java
@@ -0,0 +1,43 @@
+package InterviewBit.src.Courses.Programming.Level4_StacksAndQueues.Problems.StackMath.EvaluateExpression.Java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class SolutionTest {
+    
+    Solution solution;
+    
+    @BeforeEach
+    public void setUp() throws Exception {
+        solution = new Solution();
+    }
+    
+    @AfterEach
+    public void tearDown() throws Exception {
+        solution = null;
+    }
+    
+    @Test
+    public void TrivialCase1() {
+        String[] input = new String[]{"2", "1", "+", "3", "*"};
+        assertTimeout(Duration.ofMillis(200), () -> {
+            int expected = 9;
+            int actual = Solution.evalRPN(input);
+            assertEquals(expected, actual);
+        });
+    }
+
+    @Test
+    public void TrivialCase2() {
+        String[] input = new String[]{"4", "13", "5", "/", "+"};
+        assertTimeout(Duration.ofMillis(200), () -> {
+            int expected = 6;
+            int actual = Solution.evalRPN(input);
+            assertEquals(expected, actual);
+        });
+    }
+}
