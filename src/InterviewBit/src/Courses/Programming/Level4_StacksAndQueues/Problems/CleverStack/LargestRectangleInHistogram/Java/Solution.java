a84a288136d4fa3e1046759b3f32e2b6aed46d7a
Manas Talukdar <manas.talukdar@gmail.com>
2018-01-25 23:10:37 -0800

    notes.

--- src/InterviewBit/src/Courses/Programming/Level4_StacksAndQueues/Problems/CleverStack/LargestRectangleInHistogram/Java/Solution.java
+++ src/InterviewBit/src/Courses/Programming/Level4_StacksAndQueues/Problems/CleverStack/LargestRectangleInHistogram/Java/Solution.java
@@ -26,4 +26,35 @@ public class Solution {
         }
         return maxArea;
     }
+
+    public static int largestRectangleArea_explained(int[] A) {
+        int max = 0;
+        int size = A.length;
+        Stack<Integer> stack = new Stack<Integer>();
+        for (int i = 0; i <= size;) {
+            // last bar as size 0 to collect all other bars
+            int bar = i < size ? A[i] : 0;
+            if (stack.isEmpty() || bar >= A[stack.peek()]) {
+                // when bar is larger, push bar into stack
+                stack.push(i);
+                // move right boundary
+                i++;
+            } else {
+                // when bar is smaller, use stack's top bar as height
+                int height = A[stack.pop()];
+                // remove bars with the same height from stack
+                while (!stack.isEmpty() && height == A[stack.peek()]) {
+                    stack.pop();
+                }
+                // if stack is NOT empty, left boundary is (stack's top bar + 1)
+                // if stack is empty(bar is smallest), left boundary is 0
+                int left = stack.isEmpty() ? 0 : stack.peek() + 1;
+                // right boundary is (i - 1)
+                // so width is (i - left)
+                max = Math.max(max, height * (i - left));
+                // since we didn't move right boundary, going to calculate all bigger bars in stack
+            }
+        }
+        return max;
+    }
 }
