a8773b8adc95b642d64d213f1cd7d106d0f9a06a
Manas Talukdar <manas.talukdar@gmail.com>
2018-01-26 15:09:34 -0800

    Modular Expression

---
+++ src/InterviewBit/src/Courses/Programming/Level5_Backtracking/Examples/ModularExpression/Java/SolutionTest.java
@@ -0,0 +1,50 @@
+package InterviewBit.src.Courses.Programming.Level5_Backtracking.Examples.ModularExpression.Java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class SolutionTest {
+    
+    Solution solution;
+    
+    @BeforeEach
+    public void setUp() throws Exception {
+        solution = new Solution();
+    }
+    
+    @AfterEach
+    public void tearDown() throws Exception {
+        solution = null;
+    }
+    
+    @Test
+    public void TrivialCase1() {
+        assertTimeout(Duration.ofMillis(200), () -> {
+            int expected = 2;
+            int actual = Solution.Mod(2, 3, 3);
+            assertEquals(expected, actual);
+        });
+    }
+
+    @Test
+    public void SpecialCase1() {
+        assertTimeout(Duration.ofMillis(200), () -> {
+            int expected = 19;
+            int actual = Solution.Mod(-1, 1, 20);
+            assertEquals(expected, actual);
+        });
+    }
+
+    @Test
+    public void SpecialCase2() {
+        assertTimeout(Duration.ofMillis(200), () -> {
+            int expected = 20805472;
+            int actual = Solution.Mod(71045970, 41535484, 64735492);
+            assertEquals(expected, actual);
+        });
+    }
+}
