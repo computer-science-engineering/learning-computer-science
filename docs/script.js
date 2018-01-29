e1e50eadbd81063b0f1fea526fdc8aeec2573cd1
Manas Talukdar <manas.talukdar@gmail.com>
2018-01-28 19:19:26 -0800

    update.

---
+++ docs/script.js
@@ -0,0 +1,44 @@
+fetch('https://github.com/manastalukdar/learning_computer-science/blob/master/build/problems_list.json')
+    .then(res => res.json())
+    .then(json_data => {
+        console.log(json_data)
+    //json variable contains object with data
+})
+
+// EXTRACT VALUE FOR HTML HEADER.
+var col = [];
+for (var i = 0; i < json_data.length; i++) {
+    for (var key in json_data[i]) {
+        if (col.indexOf(key) === -1) {
+            col.push(key);
+        }
+    }
+}
+
+// CREATE DYNAMIC TABLE.
+var table = document.createElement("table");
+
+// CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
+var tr = table.insertRow(-1); // TABLE ROW.
+
+for (var i = 0; i < col.length; i++) {
+    var th = document.createElement("th"); // TABLE HEADER.
+    th.innerHTML = col[i];
+    tr.appendChild(th);
+}
+
+// ADD JSON DATA TO THE TABLE AS ROWS.
+for (var i = 0; i < myBooks.length; i++) {
+
+    tr = table.insertRow(-1);
+
+    for (var j = 0; j < col.length; j++) {
+        var tabCell = tr.insertCell(-1);
+        tabCell.innerHTML = myBooks[i][col[j]];
+    }
+}
+
+// FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
+var divContainer = document.getElementById("showData");
+divContainer.innerHTML = "";
+divContainer.appendChild(table);
