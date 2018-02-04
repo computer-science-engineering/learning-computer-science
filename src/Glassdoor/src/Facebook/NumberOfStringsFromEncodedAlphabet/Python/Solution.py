f28f69f85f4c373549080252bb6adfa9785c0d34
Manas Talukdar <manas.talukdar@gmail.com>
2018-02-04 15:54:50 -0800

    #19 cleanup.

--- src/Glassdoor/src/Facebook/NumberOfStringsFromEncodedAlphabet/Python/Solution.py
+++ src/Glassdoor/src/Facebook/NumberOfStringsFromEncodedAlphabet/Python/Solution.py
@@ -1,20 +1,22 @@
-def find_strings(x):
-    nums = []
-    prev = ""
-    total = 0
-    for i in str(x):
-        if int(i) != 0:
-            total += 1
-            nums.append(int(i))
-        if prev:
-            if int(prev + i) != int(i) and int(prev+i) < 27:
+class Solution:
+    def find_strings(self, x):
+        nums = []
+        prev = ""
+        total = 0
+        for i in str(x):
+            if int(i) != 0:
                 total += 1
-                nums.append(int(prev+i))
-        prev = i
-    return total, nums
+                nums.append(int(i))
+            if prev:
+                if int(prev + i) != int(i) and int(prev+i) < 27:
+                    total += 1
+                    nums.append(int(prev+i))
+            prev = i
+        return total, nums
 
 def main():
-    print(find_strings(23413259802))
+    solution = Solution()
+    print(solution.find_strings(23413259802))
 
 if __name__ == '__main__':
     main()
--- src/Glassdoor/src/Facebook/NumberOfStringsFromEncodedAlphabet/Python/test_solution.py
+++ src/Glassdoor/src/Facebook/NumberOfStringsFromEncodedAlphabet/Python/test_solution.py
@@ -5,7 +5,8 @@ class TrivialCase(unittest.TestCase):
     def testTrivialCase1(self):
         """trivial case 1"""
         expected = (13, [2, 3, 23, 4, 1, 3, 13, 2, 5, 25, 9, 8, 2])
-        actual = solution.find_strings(23413259802)
+        sol = solution.Solution()
+        actual = sol.find_strings(23413259802)
         self.assertEqual(expected, actual)
 
 if __name__ == "__main__":
