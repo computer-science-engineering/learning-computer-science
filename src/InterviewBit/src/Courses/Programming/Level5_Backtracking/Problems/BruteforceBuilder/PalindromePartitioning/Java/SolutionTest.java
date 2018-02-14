0511ecd419b20bc36e08cdb176e159f6e978abe3
Manas Talukdar <manas.talukdar@gmail.com>
2018-02-13 22:41:13 -0800

    Palindrome Partitioning

---
+++ src/InterviewBit/src/Courses/Programming/Level5_Backtracking/Problems/BruteforceBuilder/PalindromePartitioning/Java/SolutionTest.java
@@ -0,0 +1,36 @@
+package InterviewBit.src.Courses.Programming.Level5_Backtracking.Problems.BruteforceBuilder.PalindromePartitioning.Java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
+import java.util.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class SolutionTest {
+    
+    Solution solution;
+    
+    @BeforeEach
+    public void setUp() throws Exception {
+        solution = new Solution();
+    }
+    
+    @AfterEach
+    public void tearDown() throws Exception {
+        solution = null;
+    }
+    
+    @Test
+    public void TrivialCase1() {
+        String input = "aab";
+        assertTimeout(Duration.ofMillis(500), () -> {
+            ArrayList<ArrayList<String>> expected = new ArrayList<ArrayList<String>>();
+            expected.add(new ArrayList<String>(Arrays.asList("a","a","b")));
+            expected.add(new ArrayList<String>(Arrays.asList("aa", "b")));
+            ArrayList<ArrayList<String>> actual = Solution.partition(input);
+            assertEquals(expected, actual);
+        });
+    }
+}
