cdba8c23fc6d50cb9bb366047ade33cbd4d49703
Manas Talukdar <manas.talukdar@gmail.com>
2018-02-16 12:49:57 -0800

    Repeated Elements In An Array

---
+++ src/Firecode/src/Problems/Level1/RepeatedElementsInAnArray/Java/SolutionTest.java
@@ -0,0 +1,63 @@
+package Firecode.src.Problems.Level1.RepeatedElementsInAnArray.Java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class SolutionTest {
+    
+    Solution solution;
+    
+    @BeforeEach
+    public void setUp() throws Exception {
+        solution = new Solution();
+    }
+    
+    @AfterEach
+    public void tearDown() throws Exception {
+        solution = null;
+    }
+    
+    @Test
+    public void TrivialCase1() {
+        int[] input = {1,5,23,2,6,3,1,8,12,3};
+        assertTimeout(Duration.ofMillis(500), () -> {
+            String expected = "[1, 3]";
+            String actual = Solution.duplicate(input);
+            assertEquals(expected, actual);
+        });
+    }
+
+    @Test
+    public void TrivialCase2() {
+        int[] input = {1,3,3,4};
+        assertTimeout(Duration.ofMillis(500), () -> {
+            String expected = "[3]";
+            String actual = Solution.duplicate(input);
+            assertEquals(expected, actual);
+        });
+    }
+
+    @Test
+    public void SpecialCase1() {
+        int[] input = {4};
+        assertTimeout(Duration.ofMillis(500), () -> {
+            String expected = "[]";
+            String actual = Solution.duplicate(input);
+            assertEquals(expected, actual);
+        });
+    }
+
+    @Test
+    public void SpecialCase2() {
+        int[] input = {4, 3, 1};
+        assertTimeout(Duration.ofMillis(500), () -> {
+            String expected = "[]";
+            String actual = Solution.duplicate(input);
+            assertEquals(expected, actual);
+        });
+    }
+}
