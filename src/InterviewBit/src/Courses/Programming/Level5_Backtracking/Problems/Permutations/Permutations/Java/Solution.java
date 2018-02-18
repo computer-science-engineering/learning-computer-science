fdfb304c263f1901dcc7815d4cd1a5accf5f5b91
Manas Talukdar <manas.talukdar@gmail.com>
2018-02-17 17:36:14 -0800

    cleanup.

--- src/InterviewBit/src/Courses/Programming/Level5_Backtracking/Problems/Permutations/Permutations/Java/Solution.java
+++ src/InterviewBit/src/Courses/Programming/Level5_Backtracking/Problems/Permutations/Permutations/Java/Solution.java
@@ -21,12 +21,14 @@ public class Solution {
         return list;
     }
 
-    private static void backtrack(ArrayList<ArrayList<Integer>> list, List<Integer> tempList, ArrayList<Integer> nums){
-        if(tempList.size() == nums.size()){
+    private static void backtrack(ArrayList<ArrayList<Integer>> list, List<Integer> tempList, ArrayList<Integer> nums) {
+        if(tempList.size() == nums.size()) {
             list.add(new ArrayList<>(tempList));
         } else {
-            for(int i = 0; i < nums.size(); i++){ 
-                if(tempList.contains(nums.get(i))) continue; // element already exists, skip
+            for(int i = 0; i < nums.size(); i++) { 
+                if(tempList.contains(nums.get(i))) {
+                    continue; // element already exists, skip
+                }
                 tempList.add(nums.get(i));
                 backtrack(list, tempList, nums);
                 tempList.remove(tempList.size() - 1);
