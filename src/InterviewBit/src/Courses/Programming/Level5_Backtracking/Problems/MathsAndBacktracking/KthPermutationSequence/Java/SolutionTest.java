80392dbac550ce435bc62ae8f95c6b82b8dc206c
Manas Talukdar <manas.talukdar@gmail.com>
2018-02-19 15:15:04 -0800

    Kth Permutation Sequence.

---
+++ src/InterviewBit/src/Courses/Programming/Level5_Backtracking/Problems/MathsAndBacktracking/KthPermutationSequence/Java/SolutionTest.java
@@ -0,0 +1,41 @@
+package InterviewBit.src.Courses.Programming.Level5_Backtracking.Problems.MathsAndBacktracking.KthPermutationSequence.Java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class SolutionTest {
+    
+    Solution solution;
+    
+    @BeforeEach
+    public void setUp() throws Exception {
+        solution = new Solution();
+    }
+    
+    @AfterEach
+    public void tearDown() throws Exception {
+        solution = null;
+    }
+    
+    @Test
+    public void TrivialCase1() {
+        assertTimeout(Duration.ofMillis(500), () -> {
+            String expected = "3142";
+            String actual = Solution.getPermutation(4, 14);
+            assertEquals(expected, actual);
+        });
+    }
+
+    @Test
+    public void TrivialCase2() {
+        assertTimeout(Duration.ofMillis(500), () -> {
+            String expected = "12";
+            String actual = Solution.getPermutation(2, 1);
+            assertEquals(expected, actual);
+        });
+    }
+}
