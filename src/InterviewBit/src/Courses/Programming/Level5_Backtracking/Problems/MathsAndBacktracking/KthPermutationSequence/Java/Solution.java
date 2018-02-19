9bb9f2b15457cb04bfea8d2a9d28496e519b85c9
Manas Talukdar <manas.talukdar@gmail.com>
2018-02-19 15:15:04 -0800

    Kth Permutation Sequence.

---
+++ src/InterviewBit/src/Courses/Programming/Level5_Backtracking/Problems/MathsAndBacktracking/KthPermutationSequence/Java/Solution.java
@@ -0,0 +1,61 @@
+package InterviewBit.src.Courses.Programming.Level5_Backtracking.Problems.MathsAndBacktracking.KthPermutationSequence.Java;
+
+import java.math.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(getPermutation(2147483647, 101));
+    }
+    
+    public static String getPermutation(int A, int B) {
+        List<Integer> numbers = new ArrayList<>();
+        //int[] factorial = new int[A+1];
+        StringBuilder sb = new StringBuilder();
+        
+        // create an array of factorial lookup
+        // int sum = 1;
+        // factorial[0] = 1;
+        // for(int i=1; i<=A; i++) {            
+        //     sum *= i;
+        //     factorial[i] = sum;
+        // }
+        // factorial[] = {1, 1, 2, 6, 24, ... n!}
+        
+        // create a list of numbers to get indices
+        for(int i=1; i<=A; i++) {
+            numbers.add(i);
+        }
+        // numbers = {1, 2, 3, 4}
+        
+        B--;
+        
+        for(int i = 1; i <= A; i++) {
+            //int index = B/factorial[A-i];
+            int fact = factorial(A-i).intValue();
+            // int index = 0;
+            // if (fact > 0) {
+            //     index= B/fact;
+            // }
+            int index= B/fact;
+            sb.append(String.valueOf(numbers.get(index)));
+            numbers.remove(index);
+            B-=index*fact;
+        }
+        
+        return String.valueOf(sb);
+    }
+
+    private static BigInteger factorial(int value){
+        if(value < 0) {
+            throw new IllegalArgumentException("Value must be positive");
+        }
+    
+        BigInteger result = BigInteger.ONE;
+        for (int i = 2; i <= value; i++) {
+            result = result.multiply(BigInteger.valueOf(i));
+        }
+    
+        return result;
+    }
+}
---
+++ src/InterviewBit/src/Courses/Programming/Level5_Backtracking/Problems/MathsAndBacktracking/KthPermutationSequence/readme.md
@@ -0,0 +1,49 @@
+# Kth Permutation Sequence
+
+## Origin
+
+[Link](https://www.interviewbit.com/problems/kth-permutation-sequence/)
+
+## Description
+
+The set [1,2,3,…,n] contains a total of n! unique permutations.
+
+By listing and labeling all of the permutations in order,
+We get the following sequence (ie, for n = 3 ) :
+
+```text
+1. "123"
+2. "132"
+3. "213"
+4. "231"
+5. "312"
+6. "321"
+```
+
+Given n and k, return the kth permutation sequence.
+
+For example, given n = 3, k = 4, ans = "231"
+
+Good questions to ask the interviewer:
+
+* What if n is greater than 10. How should multiple digit numbers be represented in string? --> In this case, just concatenate the number to the answer. So if n = 11, k = 1, ans = "1234567891011"
+* Whats the maximum value of n and k? --> In this case, k will be a positive integer thats less than INT_MAX. n is reasonable enough to make sure the answer does not bloat up a lot.
+
+## Companies
+
+1. Amazon
+
+## Categories
+
+1. Courses
+1. Programming
+
+## Tags
+
+1. Backtracking
+
+## Notes
+
+1. [LeetCode discussion](https://leetcode.com/problems/permutation-sequence/discuss/22507/"Explain-like-I'm-five"-Java-Solution-in-O(n))
+1. [ProgramCreek](https://www.programcreek.com/2013/02/leetcode-permutation-sequence-java/)
+1. [SO - BigInteger based factorial](https://stackoverflow.com/questions/891031/is-there-a-method-that-calculates-a-factorial-in-java/29550283#29550283)
