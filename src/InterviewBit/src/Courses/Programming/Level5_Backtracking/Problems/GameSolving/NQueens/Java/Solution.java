6c1e8f371882fb77674aa71dd64071f747238b26
Manas Talukdar <manas.talukdar@gmail.com>
2018-02-20 00:57:35 -0800

    NQueens

---
+++ src/InterviewBit/src/Courses/Programming/Level5_Backtracking/Problems/GameSolving/NQueens/Java/Solution.java
@@ -0,0 +1,62 @@
+package InterviewBit.src.Courses.Programming.Level5_Backtracking.Problems.GameSolving.NQueens.Java;
+
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        ArrayList<ArrayList<String>> result = solveNQueens(4);
+        for (ArrayList<String> internalList : result) {
+            for (int i=0; i<internalList.size()-1; i++) {           
+                System.out.print(internalList.get(i) + ", ");
+            }
+            System.out.print(internalList.get(internalList.size()-1));
+            System.out.println();          
+        }
+    }
+    
+    public static ArrayList<ArrayList<String>> solveNQueens(int a) {
+        char[][] board = new char[a][a];
+        for(int i = 0; i < a; i++)
+            for(int j = 0; j < a; j++)
+                board[i][j] = '.';
+        ArrayList<ArrayList<String>> res = new ArrayList<ArrayList<String>>();
+        dfs(board, 0, res);
+        return res;
+    }
+    
+    private static void dfs(char[][] board, int colIndex, ArrayList<ArrayList<String>> res) {
+        if(colIndex == board.length) {
+            List<String> temp = construct(board);
+            res.add(new ArrayList<String>(temp));
+            return;
+        }
+        
+        for(int i = 0; i < board.length; i++) {
+            if(validate(board, i, colIndex)) {
+                board[i][colIndex] = 'Q';
+                dfs(board, colIndex + 1, res);
+                board[i][colIndex] = '.';
+            }
+        }
+    }
+
+    private static boolean validate(char[][] board, int x, int y) {
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < y; j++) {
+                if(board[i][j] == 'Q' && (x + j == y + i || x + y == i + j || x == i))
+                    return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    private static List<String> construct(char[][] board) {
+        List<String> res = new LinkedList<String>();
+        for(int i = 0; i < board.length; i++) {
+            String s = new String(board[i]);
+            res.add(s);
+        }
+        return res;
+    }
+}
---
+++ src/InterviewBit/src/Courses/Programming/Level5_Backtracking/Problems/GameSolving/NQueens/readme.md
@@ -0,0 +1,53 @@
+# NQueens
+
+## Origin
+
+[Link](https://www.interviewbit.com/problems/nqueens/)
+
+## Description
+
+The n-queens puzzle is the problem of placing n queens on an n×n chessboard such that no two queens attack each other.
+
+[An example](https://i.imgur.com/yaxpgda.png)
+
+Given an integer n, return all distinct solutions to the n-queens puzzle.
+
+Each solution contains a distinct board configuration of the n-queens’ placement, where `Q` and `.` both indicate a queen and an empty space respectively.
+
+For example,
+There exist two distinct solutions to the 4-queens puzzle:
+
+```text
+[
+ [".Q..",  // Solution 1
+  "...Q",
+  "Q...",
+  "..Q."],
+
+ ["..Q.",  // Solution 2
+  "Q...",
+  "...Q",
+  ".Q.."]
+]
+```
+
+## Companies
+
+1. Qualcomm
+1. Google
+1. Amazon
+1. Splunk
+
+## Categories
+
+1. Courses
+1. Programming
+
+## Tags
+
+1. Backtracking
+
+## Notes
+
+1. [LeetCode discussion](https://leetcode.com/problems/n-queens/discuss/19805/My-easy-understanding-Java-Solution)
+1. [YT video - Tushar Roy](https://www.youtube.com/watch?v=xouin83ebxE)
