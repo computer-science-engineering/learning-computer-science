b0fb6f7ecdcbfa7e5730bae538cf7e6bb430c600
Manas Talukdar <manas.talukdar@gmail.com>
2018-02-20 00:57:35 -0800

    NQueens

---
+++ src/InterviewBit/src/Courses/Programming/Level5_Backtracking/Problems/GameSolving/NQueens/Java/SolutionTest.java
@@ -0,0 +1,35 @@
+package InterviewBit.src.Courses.Programming.Level5_Backtracking.Problems.GameSolving.NQueens.Java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
+import java.util.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class SolutionTest {
+    
+    Solution solution;
+    
+    @BeforeEach
+    public void setUp() throws Exception {
+        solution = new Solution();
+    }
+    
+    @AfterEach
+    public void tearDown() throws Exception {
+        solution = null;
+    }
+    
+    @Test
+    public void TrivialCase1() {
+        assertTimeout(Duration.ofMillis(500), () -> {
+            ArrayList<ArrayList<String>> expected = new ArrayList<ArrayList<String>>();
+            expected.add(new ArrayList<String>(Arrays.asList("..Q.", "Q...", "...Q", ".Q..")));
+            expected.add(new ArrayList<String>(Arrays.asList(".Q..", "...Q", "Q...", "..Q.")));
+            ArrayList<ArrayList<String>> actual = Solution.solveNQueens(4);
+            assertEquals(expected, actual);
+        });
+    }
+}
