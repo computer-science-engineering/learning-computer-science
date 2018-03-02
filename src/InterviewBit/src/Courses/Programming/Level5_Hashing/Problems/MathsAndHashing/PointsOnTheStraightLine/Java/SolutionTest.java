f0af40df095fe58e29c65e6479d58038ec247e33
Manas Talukdar <manas.talukdar@gmail.com>
2018-03-01 23:23:15 -0800

    Points On The Straight Line

---
+++ src/InterviewBit/src/Courses/Programming/Level5_Hashing/Problems/MathsAndHashing/PointsOnTheStraightLine/Java/SolutionTest.java
@@ -0,0 +1,47 @@
+package InterviewBit.src.Courses.Programming.Level5_Hashing.Problems.MathsAndHashing.PointsOnTheStraightLine.Java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class SolutionTest {
+    
+    Solution solution;
+    
+    @BeforeEach
+    public void setUp() throws Exception {
+        solution = new Solution();
+    }
+    
+    @AfterEach
+    public void tearDown() throws Exception {
+        solution = null;
+    }
+    
+    @Test
+    public void TrivialCase1() {
+        ArrayList<Integer> a = new ArrayList<Integer>(Arrays.asList(1, 2));
+        ArrayList<Integer> b = new ArrayList<Integer>(Arrays.asList(1, 2));
+        assertTimeout(Duration.ofMillis(500), () -> {
+            int expected = 2;
+            int actual = Solution.maxPoints(a, b);
+            assertEquals(expected, actual);
+        });
+    }
+
+    @Test
+    public void TrivialCase2() {
+        ArrayList<Integer> a = new ArrayList<Integer>(Arrays.asList(0, 1, -1));
+        ArrayList<Integer> b = new ArrayList<Integer>(Arrays.asList(0, 1, -1));
+        assertTimeout(Duration.ofMillis(500), () -> {
+            int expected = 3;
+            int actual = Solution.maxPoints(a, b);
+            assertEquals(expected, actual);
+        });
+    }
+}
---
+++ src/InterviewBit/src/Courses/Programming/Level5_Hashing/Problems/MathsAndHashing/PointsOnTheStraightLine/readme.md
@@ -0,0 +1,39 @@
+# Points On The Straight Line
+
+## Origin
+
+[Link](https://www.interviewbit.com/problems/points-on-the-straight-line/)
+
+## Description
+
+Given n points on a 2D plane, find the maximum number of points that lie on the same straight line.
+
+Sample Input :
+
+```text
+(1, 1)
+(2, 2)
+```
+
+Sample Output: 2
+
+You will be give 2 arrays X and Y. Each point is represented by (X[i], Y[i])
+
+## Companies
+
+1. Google
+1. Amazon
+1. InMobi
+
+## Categories
+
+1. Courses
+1. Programming
+
+## Tags
+
+1. Hashing
+
+## Notes
+
+1. [LeetCode discussion](https://leetcode.com/problems/max-points-on-a-line/discuss/47113/A-java-solution-with-notes)
