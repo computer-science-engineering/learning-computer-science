5ba00e0a52e81845b8ac02d7932ee4ff94ff1874
Manas Talukdar <manas.talukdar@gmail.com>
2018-03-02 22:39:46 -0800

    Substring Concatenation

---
+++ src/InterviewBit/src/Courses/Programming/Level5_Hashing/Problems/IncrementalHash/SubstringConcatenation/Java/SolutionTest.java
@@ -0,0 +1,39 @@
+package InterviewBit.src.Courses.Programming.Level5_Hashing.Problems.IncrementalHash.SubstringConcatenation.Java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class SolutionTest {
+    
+    Solution solution;
+    
+    @BeforeEach
+    public void setUp() throws Exception {
+        solution = new Solution();
+    }
+    
+    @AfterEach
+    public void tearDown() throws Exception {
+        solution = null;
+    }
+    
+    @Test
+    public void TrivialCase1() {
+        String A = "barfoothefoobarman";
+        List<String> B = new ArrayList<String>(Arrays.asList("foo", "bar"));
+        assertTimeout(Duration.ofMillis(500), () -> {
+            ArrayList<Integer> expected = new ArrayList<Integer>(Arrays.asList(0, 9));
+            ArrayList<Integer> actual1 = Solution.findSubstring(A, B);
+            assertEquals(expected, actual1);
+            ArrayList<Integer> actual2 = Solution.findSubstring_Editorial(A, B);
+            assertEquals(expected, actual2);
+        });
+    }
+}
---
+++ src/InterviewBit/src/Courses/Programming/Level5_Hashing/Problems/IncrementalHash/SubstringConcatenation/readme.md
@@ -0,0 +1,39 @@
+# Substring Concatenation
+
+## Origin
+
+[Link](https://www.interviewbit.com/problems/substring-concatenation/)
+
+## Description
+
+You are given a string, S, and a list of words, L, that are all of the same length.
+
+Find all starting indices of substring(s) in S that is a concatenation of each word in L exactly once and without any intervening characters.
+
+Example :
+
+```text
+S: "barfoothefoobarman"
+L: ["foo", "bar"]
+```
+
+You should return the indices: [0,9]. Order does not matter.
+
+## Companies
+
+1. Facebook
+
+## Categories
+
+1. Courses
+1. Programming
+
+## Tags
+
+1. Hashing
+
+## Notes
+
+1. [LeetCode discussion - easy but sub-optimal solution](https://leetcode.com/problems/substring-with-concatenation-of-all-words/discuss/13658/Easy-Two-Map-Solution-(C++Java))
+1. [LeetCode discussion - 12ms solution](https://leetcode.com/problems/substring-with-concatenation-of-all-words/discuss/13691/Java-12ms-beats-100#)
+1. [LeetCode discussion - O(n) solution](https://leetcode.com/problems/substring-with-concatenation-of-all-words/discuss/13699/92-JAVA-O(N)-with-explaination)
