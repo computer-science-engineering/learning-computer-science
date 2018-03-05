47cf8c43eb6e6e4574c4123a091553a0640d2918
Manas Talukdar <manas.talukdar@gmail.com>
2018-03-05 02:03:21 -0800

    WIP.

---
+++ src/InterviewBit/src/Courses/Programming/Level6_Trees/Examples/NextGreaterNumberBst/Java/Solution.java
@@ -0,0 +1,10 @@
+package InterviewBit.src.Courses.Programming.Level6_Trees.Examples.NextGreaterNumberBst.Java;
+
+public class Solution {
+    public static void main(String[] args) {
+        
+    }
+    
+    public static TreeNode getSuccessor(TreeNode a, int b) {
+	}
+}
