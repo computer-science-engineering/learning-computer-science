0ff8dc92df32e804e9d28c5b89f3ca5c057b2189
Manas Talukdar <manas.talukdar@gmail.com>
2018-03-05 23:40:15 -0800

    Next Greater Number Bst

--- src/InterviewBit/src/Courses/Programming/Level6_Trees/Examples/NextGreaterNumberBst/Java/SolutionTest.java
+++ src/InterviewBit/src/Courses/Programming/Level6_Trees/Examples/NextGreaterNumberBst/Java/SolutionTest.java
@@ -23,11 +23,27 @@ public class SolutionTest {
     
     @Test
     public void TrivialCase1() {
-        // input = ;
+        //root level 0
+        TreeNode a = new TreeNode(100);
+        //Level 1
+        TreeNode b1 = new TreeNode(98);
+        TreeNode b2 = new TreeNode(102);
+        a.left = b1;
+        a.right = b2;        
+        //Level 2
+        TreeNode c1 = new TreeNode(96);
+        TreeNode c2 = new TreeNode(99);
+        b1.left = c1;
+        b1.right = c2;
+        //Level 3
+        TreeNode d = new TreeNode(97);
+        c1.right = d;
         assertTimeout(Duration.ofMillis(500), () -> {
-            // expected = ;
-            // actual = Solution.;
-            // assertEquals(expected, actual);
+            int expected = 98;
+            int actual1 = Solution.getSuccessor(a, 97).val;
+            assertEquals(expected, actual1);
+            int actual2 = Solution.getSuccessor_Editorial(a, 97).val;
+            assertEquals(expected, actual2);
         });
     }
 }
