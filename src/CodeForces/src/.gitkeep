102039b24df204085b6ad9b1330b61771c7295f3
Manas Talukdar <manas.talukdar@gmail.com>
2018-03-07 15:56:54 -0800

    IC - Merge Sorted Arrays, Kth to Last Node in a Singly-Linked List.

---
+++ src/CodeForces/src/.gitkeep
@@ -0,0 +1,1 @@
+http://codeforces.com
---
+++ src/InterviewCake/src/questions/KthToLastNodeInSinglyLinkedList/Java/LinkedListNode.java
@@ -0,0 +1,10 @@
+package InterviewCake.src.questions.KthToLastNodeInSinglyLinkedList.Java;
+
+public class LinkedListNode {
+    public String value;
+    public LinkedListNode next;
+
+    public LinkedListNode(String value) {
+        this.value = value;
+    }
+}
