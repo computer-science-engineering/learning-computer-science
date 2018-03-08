3e128fa163df96d6ded8e4a0b4747853edf15b4c
Manas Talukdar <manas.talukdar@gmail.com>
2018-03-07 23:01:46 -0800

    added main function test.

--- src/InterviewBit/src/Courses/Programming/Level6_Trees/Examples/ValidBinarySearchTree/Java/SolutionTest.java
+++ src/InterviewBit/src/Courses/Programming/Level6_Trees/Examples/ValidBinarySearchTree/Java/SolutionTest.java
@@ -20,6 +20,14 @@ public class SolutionTest {
     public void tearDown() throws Exception {
         solution = null;
     }
+
+    @Test
+    public void MainFunction() {
+        assertTimeout(Duration.ofMillis(500), () -> {
+            String[] args = new String[0];
+            assertAll(() -> Solution.main(args));
+        });
+    }
     
     @Test
     public void TrivialCase1() {
