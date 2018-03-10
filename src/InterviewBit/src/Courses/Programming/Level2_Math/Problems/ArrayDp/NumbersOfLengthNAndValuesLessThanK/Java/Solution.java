58b4dddb2c02c2724219642ae9fa17b26beba3b3
Manas Talukdar <manas.talukdar@gmail.com>
2018-03-09 22:27:39 -0800

    cleanup.

--- src/InterviewBit/src/Courses/Programming/Level2_Math/Problems/ArrayDp/NumbersOfLengthNAndValuesLessThanK/Java/Solution.java
+++ src/InterviewBit/src/Courses/Programming/Level2_Math/Problems/ArrayDp/NumbersOfLengthNAndValuesLessThanK/Java/Solution.java
@@ -54,14 +54,14 @@ public class Solution {
         return dp;
     }
 
-    public int solve_Editorial(ArrayList<Integer> A, int B, int C) {
+    public static int solve_Editorial(ArrayList<Integer> A, int B, int C) {
         String str=Integer.toString(C);
         int cl=str.length();
         int size=A.size();
         int d=size,d2;
         if(B>cl || C==0 || size==0)
             return 0;
-        int zeros=A.lastIndexOf(0)-A.indexOf(0)+1;
+        //int zeros=A.lastIndexOf(0)-A.indexOf(0)+1;
         boolean zero=A.contains(0);
         int ans=0;
         if(B<cl){
@@ -111,4 +111,26 @@ public class Solution {
         }
         return 0;        
     }
+
+    // todo: broken
+    /*public static int solve_Editorial_lightweight(ArrayList<Integer> A, int B, int C) {
+        int cSize = (int) Math.log10(C) + 1;
+        int n = A.size();
+        if (cSize < B || n == 0) return 0;
+        boolean hasZero = A.get(0) == 0;
+        if (cSize > B) return (B > 1 && hasZero ? n - 1 : n) * (int) Math.pow(n, B - 1);
+        // B == cSize
+        int pow10 = (int) Math.pow(10, B - 1), count = 0;
+        for(int i = B; i > 0; i--) {
+            int target = C / pow10, j;
+            C %= pow10;
+            pow10 /= 10;
+            for(j = 0; j < n; j++) {
+                if (A.get(j) >= target) break;
+            }
+            count += (B > 1 && i == B && hasZero ? j - 1 : j) * (int) Math.pow(n, i - 1);
+            if (j == n || A.get(j) > target) break;
+        }
+        return count;
+    }*/
 }
