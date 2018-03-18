de682bca976b1219b67d28a525ea3257c8d48fca
Manas Talukdar <manas.talukdar@gmail.com>
2018-03-18 00:05:05 -0700

    Identical Binary Trees

---
+++ src/InterviewBit/src/Courses/Programming/Level6_Trees/Problems/TwoTrees/IdenticalBinaryTrees/Java/SolutionTest.java
@@ -0,0 +1,65 @@
+package InterviewBit.src.Courses.Programming.Level6_Trees.Problems.TwoTrees.IdenticalBinaryTrees.Java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class SolutionTest {
+    
+    Solution solution;
+    
+    @BeforeEach
+    public void setUp() throws Exception {
+        solution = new Solution();
+    }
+    
+    @AfterEach
+    public void tearDown() throws Exception {
+        solution = null;
+    }
+    
+    @Test
+    public void MainFunction() {
+        assertTimeout(Duration.ofMillis(500), () -> {
+             String[] args = new String[0];
+             assertAll(() -> Solution.main(args));
+        });
+    }
+    
+    @Test
+    public void TrivialCase1() {
+        TreeNode A = new TreeNode(1);        
+        TreeNode B1 = new TreeNode(2);        
+        TreeNode B2 = new TreeNode(3);
+        A.left = B1;
+        A.right = B2;
+        assertTimeout(Duration.ofMillis(500), () -> {
+            int expected = 1;
+            int actual = Solution.isSameTree(A, A);
+            assertEquals(expected, actual);
+        });
+    }
+
+    @Test
+    public void TrivialCase2() {
+        TreeNode A = new TreeNode(1);        
+        TreeNode B1 = new TreeNode(2);        
+        TreeNode B2 = new TreeNode(3);
+        A.left = B1;
+        A.right = B2;
+
+        TreeNode a = new TreeNode(3);        
+        TreeNode b = new TreeNode(2);        
+        TreeNode c = new TreeNode(1);
+        a.left = b;
+        b.left = c;
+        assertTimeout(Duration.ofMillis(500), () -> {
+            int expected = 0;
+            int actual = Solution.isSameTree(A,a);
+            assertEquals(expected, actual);
+        });
+    }
+}
---
+++ src/InterviewBit/src/Courses/Programming/Level6_Trees/Problems/TwoTrees/IdenticalBinaryTrees/readme.md
@@ -0,0 +1,27 @@
+# Problem Definition
+
+## Description
+
+Given two binary trees, write a function to check if they are equal or not.
+
+Two binary trees are considered equal if they are structurally identical and the nodes have the same value.
+
+Return `0 / 1` ( 0 for false, 1 for true ) for this problem
+
+**Example:**
+
+```text
+Input:
+
+   1       1
+  / \     / \
+ 2   3   2   3
+
+Output:
+  1 or True
+```
+
+## Notes
+
+1. [Geeks for Geeks post](https://www.geeksforgeeks.org/write-c-code-to-determine-if-two-trees-are-identical/)
+1. [LeetCode discussion](https://leetcode.com/problems/same-tree/discuss/32687/Five-line-Java-solution-with-recursion)
