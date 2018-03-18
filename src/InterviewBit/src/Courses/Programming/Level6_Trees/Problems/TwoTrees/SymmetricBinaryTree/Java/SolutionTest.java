51fef16750ff92c3b39b635cc0bd0e0dbef38b9f
Manas Talukdar <manas.talukdar@gmail.com>
2018-03-18 00:36:30 -0700

    Symmetric Binary Tree.

---
+++ src/InterviewBit/src/Courses/Programming/Level6_Trees/Problems/TwoTrees/SymmetricBinaryTree/Java/SolutionTest.java
@@ -0,0 +1,71 @@
+package InterviewBit.src.Courses.Programming.Level6_Trees.Problems.TwoTrees.SymmetricBinaryTree.Java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class SolutionTest {
+    
+    Solution solution;
+    
+    @BeforeEach
+    public void setUp() throws Exception {
+        solution = new Solution();
+    }
+    
+    @AfterEach
+    public void tearDown() throws Exception {
+        solution = null;
+    }
+    
+    @Test
+    public void MainFunction() {
+        assertTimeout(Duration.ofMillis(500), () -> {
+             String[] args = new String[0];
+             assertAll(() -> Solution.main(args));
+        });
+    }
+    
+    @Test
+    public void TrivialCase1() {
+        TreeNode A = new TreeNode(1);
+        TreeNode B1 = new TreeNode(2);
+        TreeNode B2 = new TreeNode(2);
+        TreeNode C1 = new TreeNode(3);
+        TreeNode C2 = new TreeNode(4);
+        TreeNode C3 = new TreeNode(4);
+        TreeNode C4 = new TreeNode(3);
+        A.left = B1;
+        A.right = B2;
+        B1.left = C1;
+        B1.right = C2;
+        B2.left = C3;
+        B2.right = C4;
+        assertTimeout(Duration.ofMillis(500), () -> {
+            int expected = 1;
+            int actual = Solution.isSymmetric(A);
+            assertEquals(expected, actual);
+        });
+    }
+
+    @Test
+    public void TrivialCase2() {
+        TreeNode M = new TreeNode(1);
+        TreeNode N1 = new TreeNode(2);
+        TreeNode N2 = new TreeNode(2);
+        TreeNode O1 = new TreeNode(3);
+        TreeNode O2 = new TreeNode(3);
+        M.left = N1;
+        M.right = N2;
+        N1.right = O1;
+        N2.right = O2;
+        assertTimeout(Duration.ofMillis(500), () -> {
+            int expected = 0;
+            int actual = Solution.isSymmetric(M);
+            assertEquals(expected, actual);
+        });
+    }
+}
---
+++ src/InterviewBit/src/Courses/Programming/Level6_Trees/Problems/TwoTrees/SymmetricBinaryTree/readme.md
@@ -0,0 +1,35 @@
+# Problem Definition
+
+## Description
+
+Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+
+Example:
+
+```text
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+```
+
+The above binary tree is symmetric.
+
+But the following is not:
+
+```text
+    1
+   / \
+  2   2
+   \   \
+   3    3
+```
+
+Return `0 / 1` (0 for false, 1 for true) for this problem.
+
+## Notes
+
+1. [Geeks for Geeks post](https://www.geeksforgeeks.org/symmetric-tree-tree-which-is-mirror-image-of-itself/)
+1. [LeetCode discussion - 1ms recursive Java Solution, easy to understand](https://leetcode.com/problems/symmetric-tree/discuss/33104/1ms-recursive-Java-Solution-easy-to-understand)
+1. [LeetCode discussion - Recursive and non-recursive solutions in Java](https://leetcode.com/problems/symmetric-tree/discuss/33054/Recursive-and-non-recursive-solutions-in-Java)
