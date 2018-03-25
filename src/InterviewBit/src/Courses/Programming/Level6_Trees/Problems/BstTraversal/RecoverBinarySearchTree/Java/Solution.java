8e0dac4f135f73c1297b7e55779349de873869cb
Manas Talukdar <manas.talukdar@gmail.com>
2018-03-24 21:21:55 -0700

    Recover Binary Search Tree

---
+++ src/InterviewBit/src/Courses/Programming/Level6_Trees/Problems/BstTraversal/RecoverBinarySearchTree/Java/Solution.java
@@ -0,0 +1,64 @@
+package InterviewBit.src.Courses.Programming.Level6_Trees.Problems.BstTraversal.RecoverBinarySearchTree.Java;
+
+import java.util.ArrayList;
+
+public class Solution {
+    public static void main(String[] args) {
+        TreeNode a = new TreeNode(1);
+        TreeNode b1 = new TreeNode(2);
+        TreeNode b2 = new TreeNode(3);
+
+        a.left = b1;
+        a.right = b2;
+
+        Solution solution = new Solution();
+        ArrayList<Integer> result = solution.recoverTree(a);
+        for (Integer item : result) {
+            System.out.print(item + " ");
+        }
+    }
+
+    private TreeNode first;
+    private TreeNode second; 
+    private TreeNode pre; 
+ 
+    private void inorder(TreeNode root){
+        if(root == null) {
+            return;
+        }
+ 
+        inorder(root.left);
+ 
+        if(pre == null) {
+            pre = root;
+        } else {
+            if(root.val < pre.val) {
+                if(first == null) {
+                    first = pre;
+                }
+ 
+                second = root;
+            }
+            pre = root;
+        }
+ 
+        inorder(root.right);
+    }
+    
+    public ArrayList<Integer> recoverTree(TreeNode A) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        if(A == null) {
+            return result;
+        }
+ 
+        inorder(A);
+        if(second != null && first != null) {
+            result.add(second.val);
+            result.add(first.val);
+            int val = second.val;
+            second.val = first.val;
+            first.val = val;
+        }
+        return result;
+    }
+}
---
+++ src/InterviewBit/src/Courses/Programming/Level6_Trees/Problems/BstTraversal/RecoverBinarySearchTree/readme.md
@@ -0,0 +1,34 @@
+# Problem Definition
+
+## Description
+
+Two elements of a binary search tree (BST) are swapped by mistake. Tell us the 2 values swapping which the tree will be restored.
+
+Note: A solution using O(n) space is pretty straight forward. Could you devise a constant space solution?
+
+Example:
+
+```text
+Input:
+         1
+        / \
+       2   3
+
+Output:
+       [1, 2]
+
+Explanation: Swapping 1 and 2 will change the BST to be
+
+         2
+        / \
+       1   3
+
+which is a valid BST
+```
+
+## Notes
+
+1. [ProgramCreek](https://www.programcreek.com/2014/05/leetcode-recover-binary-search-tree-java/): Inorder traversal will return values in an increasing order. So if an element is less than its previous element, the previous element is a swapped node.
+1. [LeetCode discussion - Detail Explain about How Morris Traversal Finds two Incorrect Pointer](https://leetcode.com/problems/recover-binary-search-tree/discuss/32559/Detail-Explain-about-How-Morris-Traversal-Finds-two-Incorrect-Pointer)
+1. [LeetCode discussion - No Fancy Algorithm, just Simple and Powerful In-Order Traversal](https://leetcode.com/problems/recover-binary-search-tree/discuss/32535/No-Fancy-Algorithm-just-Simple-and-Powerful-In-Order-Traversal)
+1. [Morris traversal utilization](http://www.cnblogs.com/AnnieKim/archive/2013/06/15/morristraversal.html)
