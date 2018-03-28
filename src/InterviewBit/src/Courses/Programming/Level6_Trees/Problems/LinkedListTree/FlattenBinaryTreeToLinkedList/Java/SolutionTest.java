22fcd6dbc22f75fa6e40525b6b3fa7c0962b6b49
Manas Talukdar <manas.talukdar@gmail.com>
2018-03-28 01:26:50 -0700

    Flatten Binary Tree To LinkedList

---
+++ src/InterviewBit/src/Courses/Programming/Level6_Trees/Problems/LinkedListTree/FlattenBinaryTreeToLinkedList/Java/SolutionTest.java
@@ -0,0 +1,49 @@
+package InterviewBit.src.Courses.Programming.Level6_Trees.Problems.LinkedListTree.FlattenBinaryTreeToLinkedList.Java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class SolutionTest {
+    
+    Solution solution;
+    
+    @BeforeEach
+    public void setUp() throws Exception {
+        solution = new Solution();
+    }
+    
+    @AfterEach
+    public void tearDown() throws Exception {
+        solution = null;
+    }
+    
+    @Test
+    public void MainFunction() {
+        assertTimeout(Duration.ofMillis(500), () -> {
+             String[] args = new String[0];
+             assertAll(() -> Solution.main(args));
+        });
+    }
+    
+    @Test
+    public void TrivialCase1() {
+        int[] values = {
+            1,
+            2, 5,
+            3, 4, -1, 6};
+        TreeNode a = TreeNode.fromArray(values);
+        assertTimeout(Duration.ofMillis(500), () -> {
+            ArrayList<Integer> expected = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6));
+            TreeNode result = Solution.flatten(a);
+            ArrayList<Integer> actual = Solution.levelOrder(result);
+            assertEquals(expected, actual);
+        });
+    }
+}
---
+++ src/InterviewBit/src/Courses/Programming/Level6_Trees/Problems/LinkedListTree/FlattenBinaryTreeToLinkedList/readme.md
@@ -0,0 +1,41 @@
+# Problem Definition
+
+## Description
+
+Given a binary tree, flatten it to a linked list in-place.
+
+Example:
+
+Given
+
+```text
+         1
+        / \
+       2   5
+      / \   \
+     3   4   6
+```
+
+The flattened tree should look like:
+
+```text
+   1
+    \
+     2
+      \
+       3
+        \
+         4
+          \
+           5
+            \
+             6
+```
+
+Note that the left child of all nodes should be `NULL`.
+
+## Notes
+
+1. [LeetCode discussion - Straightforward Java Solution](https://leetcode.com/problems/flatten-binary-tree-to-linked-list/discuss/36987/Straightforward-Java-Solution)
+1. [LeetCode discussion - Share my simple NON-recursive solution, O(1) space complexity! - Morris traversal](https://leetcode.com/problems/flatten-binary-tree-to-linked-list/discuss/37010/Share-my-simple-NON-recursive-solution-O(1)-space-complexity!)
+1. [ProgramCreek](https://www.programcreek.com/2013/01/leetcode-flatten-binary-tree-to-linked-list/)
