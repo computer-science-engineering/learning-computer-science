992ac33c51984c74cd2f3867207c05d603eb8ea9
Manas Talukdar <manas.talukdar@gmail.com>
2018-03-30 13:46:00 -0700

    Seats

---
+++ src/InterviewBit/src/Courses/Programming/Level7_Greedy/Problems/Bucket4/Seats/Java/SolutionTest.java
@@ -0,0 +1,43 @@
+package InterviewBit.src.Courses.Programming.Level7_Greedy.Problems.Bucket4.Seats.Java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class SolutionTest {
+    
+    Solution solution;
+    
+    @BeforeEach
+    public void setUp() throws Exception {
+        solution = new Solution();
+    }
+    
+    @AfterEach
+    public void tearDown() throws Exception {
+        solution = null;
+    }
+    
+    @Test
+    public void MainFunction() {
+        assertTimeout(Duration.ofMillis(500), () -> {
+             String[] args = new String[0];
+             assertAll(() -> Solution.main(args));
+        });
+    }
+    
+    @Test
+    public void TrivialCase1() {
+        String A = ". . . . x . . x x . . . x . .";
+        assertTimeout(Duration.ofMillis(500), () -> {
+            int expected = 5;
+            int actual1 = Solution.seats(A);
+            assertEquals(expected, actual1);
+            int actual2 = Solution.seats_BruteForce(A);
+            assertEquals(expected, actual2);
+        });
+    }
+}
