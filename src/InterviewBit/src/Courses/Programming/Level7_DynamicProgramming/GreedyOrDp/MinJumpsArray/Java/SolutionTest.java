84998dd08abfca4222af11ff86a45b9f5541fccc
Manas Talukdar <manas.talukdar@gmail.com>
2018-04-03 00:56:29 -0700

    Min Jumps Array

---
+++ src/InterviewBit/src/Courses/Programming/Level7_DynamicProgramming/GreedyOrDp/MinJumpsArray/Java/SolutionTest.java
@@ -0,0 +1,67 @@
+package InterviewBit.src.Courses.Programming.Level7_DynamicProgramming.GreedyOrDp.MinJumpsArray.Java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class SolutionTest {
+    
+    Solution solution;
+    
+    @BeforeEach
+    public void setUp() throws Exception {
+        solution = new Solution();
+    }
+    
+    @AfterEach
+    public void tearDown() throws Exception {
+        solution = null;
+    }
+    
+    @Test
+    public void MainFunction() {
+        assertTimeout(Duration.ofMillis(500), () -> {
+             String[] args = new String[0];
+             assertAll(() -> Solution.main(args));
+        });
+    }
+    
+    @Test
+    public void TrivialCase1() {
+        int[] A = {2,3,1,1,4};
+        assertTimeout(Duration.ofMillis(500), () -> {
+            int expected = 2;
+            int actual = Solution.jump(A);
+            assertEquals(expected, actual);
+            int actual_Editorial = Solution.jump(A);
+            assertEquals(expected, actual_Editorial);
+        });
+    }
+
+    @Test
+    public void SpecialCase1() {
+        int[] A = {0};
+        assertTimeout(Duration.ofMillis(500), () -> {
+            int expected = 0;
+            int actual = Solution.jump(A);
+            assertEquals(expected, actual);
+            int actual_Editorial = Solution.jump(A);
+            assertEquals(expected, actual_Editorial);
+        });
+    }
+
+    @Test
+    public void SpecialCase2() {
+        int[] A = { 0, 46, 46, 0, 2, 47, 1, 24, 45, 0, 0, 24, 18, 29, 27, 11, 0, 0, 40, 12, 4, 0, 0, 0, 49, 42, 13, 5, 12, 45, 10, 0, 29, 11, 22, 15, 17, 41, 34, 23, 11, 35, 0, 18, 47, 0, 38, 37, 3, 37, 0, 43, 50, 0, 25, 12, 0, 38, 28, 37, 5, 4, 12, 23, 31, 9, 26, 19, 11, 21, 0, 0, 40, 18, 44, 0, 0, 0, 0, 30, 26, 37, 0, 26, 39, 10, 1, 0, 0, 3, 50, 46, 1, 38, 38, 7, 6, 38, 27, 7, 25, 30, 0, 0, 36, 37, 6, 39, 40, 32, 41, 12, 3, 44, 44, 39, 2, 26, 40, 36, 35, 21, 14, 41, 48, 50, 21, 0, 0, 23, 49, 21, 11, 27, 40, 47, 49 };
+        assertTimeout(Duration.ofMillis(500), () -> {
+            int expected = -1;
+            int actual = Solution.jump(A);
+            assertEquals(expected, actual);
+            int actual_Editorial = Solution.jump(A);
+            assertEquals(expected, actual_Editorial);
+        });
+    }
+}
---
+++ src/InterviewBit/src/Courses/Programming/Level7_DynamicProgramming/GreedyOrDp/MinJumpsArray/readme.md
@@ -0,0 +1,20 @@
+# Problem Definition
+
+## Description
+
+Given an array of non-negative integers, you are initially positioned at the first index of the array.
+
+Each element in the array represents your maximum jump length at that position.
+
+Your goal is to reach the last index in the minimum number of jumps.
+
+**Example:**
+Given array `A = [2,3,1,1,4]`
+
+The minimum number of jumps to reach the last index is `2`. (Jump 1 step from index 0 to 1, then 3 steps to the last index.)
+
+If it is not possible to reach the end index, return `-1`.
+
+## Notes
+
+1. [Geeks for Geeks - Minimum number of jumps to reach end](https://www.geeksforgeeks.org/minimum-number-of-jumps-to-reach-end-of-a-given-array/)
