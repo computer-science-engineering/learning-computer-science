730ead5dbd4b54198a76917d2ab017d8c0432a2f
Manas Talukdar <manas.talukdar@gmail.com>
2018-04-10 16:05:20 -0700

    cleanup.

--- src/InterviewBit/src/Courses/Programming/Level7_DynamicProgramming/DpOptimizedBacktrack/WordBreak2/Java/Solution.java
+++ src/InterviewBit/src/Courses/Programming/Level7_DynamicProgramming/DpOptimizedBacktrack/WordBreak2/Java/Solution.java
@@ -27,8 +27,9 @@ public class Solution {
     }
 
     private static List<String> DFS(String s, Set<String> wordDict, HashMap<String, LinkedList<String>>map) {
-        if (map.containsKey(s)) 
+        if (map.containsKey(s)) {
             return map.get(s);
+        }
             
         LinkedList<String> res = new LinkedList<String>();     
         if (s.length() == 0) {
