1c48a6d175ad1c1793ec0a28d35e7dfa11cbde1a
Manas Talukdar <manas.talukdar@gmail.com>
2018-04-11 00:08:34 -0700

    Edit Distance

---
+++ src/InterviewBit/src/Courses/Programming/Level7_DynamicProgramming/TwoDStringDp/EditDistance/Java/Solution.java
@@ -0,0 +1,42 @@
+package InterviewBit.src.Courses.Programming.Level7_DynamicProgramming.TwoDStringDp.EditDistance.Java;
+
+public class Solution {
+    public static void main(String[] args) {
+        String A = "Anshuman";
+        String B = "Antihuman";
+        System.out.println(minDistance(A, B));
+    }
+    
+    public static int minDistance(String A, String B) {
+        int m = A.length();
+        int n = B.length();
+        
+        int[][] cost = new int[m + 1][n + 1];
+        for(int i = 0; i <= m; i++) {
+            cost[i][0] = i;
+        }
+        for(int i = 1; i <= n; i++) {
+            cost[0][i] = i;
+        }
+        
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(A.charAt(i) == B.charAt(j)) {
+                    cost[i + 1][j + 1] = cost[i][j];
+                }
+                else {
+                    // int a = cost[i][j];
+                    // int b = cost[i][j + 1];
+                    // int c = cost[i + 1][j];
+                    // cost[i + 1][j + 1] = a < b ? (a < c ? a : c) : (b < c ? b : c);
+                    // cost[i + 1][j + 1]++;
+                    int replace = cost[i][j];
+                    int delete = cost[i][j + 1];
+                    int insert = cost[i + 1][j];
+                    cost[i + 1][j + 1] = Math.min(replace, Math.min(delete, insert)) + 1;
+                }
+            }
+        }
+        return cost[m][n];
+    }
+}
---
+++ src/InterviewBit/src/Courses/Programming/Level7_DynamicProgramming/TwoDStringDp/EditDistance/readme.md
@@ -0,0 +1,21 @@
+# Problem Definition
+
+## Description
+
+Given two words `A` and `B`, find the minimum number of steps required to convert `A` to `B`. (each operation is counted as 1 step.)
+
+You have the following 3 operations permitted on a word:
+
+Insert a character
+Delete a character
+Replace a character
+
+**Example :**
+
+edit distance between
+`"Anshuman"` and `"Antihuman"` is 2.
+
+* Operation 1: Replace s with t.
+* Operation 2: Insert i.
+
+## Notes
