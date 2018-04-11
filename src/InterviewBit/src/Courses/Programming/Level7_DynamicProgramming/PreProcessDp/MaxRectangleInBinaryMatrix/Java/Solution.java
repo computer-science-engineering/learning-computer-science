90713792e1e96d56d2bea62b665f685c998a27ec
Manas Talukdar <manas.talukdar@gmail.com>
2018-04-10 19:18:36 -0700

    cleanup.

--- src/InterviewBit/src/Courses/Programming/Level7_DynamicProgramming/PreProcessDp/MaxRectangleInBinaryMatrix/Java/Solution.java
+++ src/InterviewBit/src/Courses/Programming/Level7_DynamicProgramming/PreProcessDp/MaxRectangleInBinaryMatrix/Java/Solution.java
@@ -36,42 +36,45 @@ public class Solution {
         int area = 0;
         int i;
         for(i=0; i < input.length;) {
-            if(stack.isEmpty() || input[stack.peekFirst()] <= input[i]){
+            if(stack.isEmpty() || input[stack.peekFirst()] <= input[i]) {
                 stack.offerFirst(i++);
-            }else{
+            } else {
                 int top = stack.pollFirst();
                 //if stack is empty means everything till i has to be
                 //greater or equal to input[top] so get area by
                 //input[top] * i;
-                if(stack.isEmpty()){
+                if(stack.isEmpty()) {
                     area = input[top] * i;
                 }
                 //if stack is not empty then everything from i-1 to input.peek() + 1
                 //has to be greater or equal to input[top]
                 //so area = input[top]*(i - stack.peek() - 1);
-                else{
+                else {
                     area = input[top] * (i - stack.peekFirst() - 1);
                 }
-                if(area > maxArea){
+
+                if(area > maxArea) {
                     maxArea = area;
                 }
             }
         }
-        while(!stack.isEmpty()){
+
+        while(!stack.isEmpty()) {
             int top = stack.pollFirst();
             //if stack is empty means everything till i has to be
             //greater or equal to input[top] so get area by
             //input[top] * i;
-            if(stack.isEmpty()){
+            if(stack.isEmpty()) {
                 area = input[top] * i;
             }
             //if stack is not empty then everything from i-1 to input.peek() + 1
             //has to be greater or equal to input[top]
             //so area = input[top]*(i - stack.peek() - 1);
-            else{
+            else {
                 area = input[top] * (i - stack.peekFirst() - 1);
             }
-            if(area > maxArea){
+
+            if(area > maxArea) {
                     maxArea = area;
             }
         }
