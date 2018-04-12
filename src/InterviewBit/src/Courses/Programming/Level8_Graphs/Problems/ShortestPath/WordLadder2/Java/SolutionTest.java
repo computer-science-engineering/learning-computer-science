400488cd324ab48a91914ef63182b77cb54717fd
Manas Talukdar <manas.talukdar@gmail.com>
2018-04-12 15:52:31 -0700

    Word Ladder 2

---
+++ src/InterviewBit/src/Courses/Programming/Level8_Graphs/Problems/ShortestPath/WordLadder2/Java/SolutionTest.java
@@ -0,0 +1,48 @@
+package InterviewBit.src.Courses.Programming.Level8_Graphs.Problems.ShortestPath.WordLadder2.Java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class SolutionTest {
+    
+    Solution solution;
+    
+    @BeforeEach
+    public void setUp() throws Exception {
+        solution = new Solution();
+    }
+    
+    @AfterEach
+    public void tearDown() throws Exception {
+        solution = null;
+    }
+    
+    @Test
+    public void MainFunction() {
+        assertTimeout(Duration.ofMillis(500), () -> {
+             String[] args = new String[0];
+             assertAll(() -> Solution.main(args));
+        });
+    }
+    
+    @Test
+    public void TrivialCase1() {
+        String start = "hit";
+        String end = "cog";
+        ArrayList<String> dict = new ArrayList<String>(Arrays.asList("hot","dot","dog","lot","log"));
+        assertTimeout(Duration.ofMillis(500), () -> {
+            ArrayList<ArrayList<String>> expected = new ArrayList<ArrayList<String>>();
+            expected.add(new ArrayList<String>(Arrays.asList("hit", "hot", "dot", "dog", "cog")));
+            expected.add(new ArrayList<String>(Arrays.asList("hit", "hot", "lot", "log", "cog")));
+            ArrayList<ArrayList<String>> actual = Solution.findLadders(start, end, dict);
+            assertEquals(expected, actual);
+        });
+    }
+}
---
+++ src/InterviewBit/src/Courses/Programming/Level8_Graphs/Problems/ShortestPath/WordLadder2/Java/WordNode.java
@@ -0,0 +1,16 @@
+package InterviewBit.src.Courses.Programming.Level8_Graphs.Problems.ShortestPath.WordLadder2.Java;
+
+public class WordNode {
+    String word;
+    int numSteps;
+    WordNode pre;
+    public WordNode(String word, int numSteps, WordNode pre) {
+        this.word = word;
+        this.numSteps = numSteps;
+        this.pre = pre;
+    }
+     public WordNode(String word, int numSteps) {
+        this.word = word;
+        this.numSteps = numSteps;        
+    }
+}
