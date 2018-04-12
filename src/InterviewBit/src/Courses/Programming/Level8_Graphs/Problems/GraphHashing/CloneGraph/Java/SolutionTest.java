f2733b01200ff23ce2064d30ab5c0aa267f7f76c
Manas Talukdar <manas.talukdar@gmail.com>
2018-04-12 14:08:26 -0700

    Clone Graph

---
+++ src/InterviewBit/src/Courses/Programming/Level8_Graphs/Problems/GraphHashing/CloneGraph/Java/SolutionTest.java
@@ -0,0 +1,49 @@
+package InterviewBit.src.Courses.Programming.Level8_Graphs.Problems.GraphHashing.CloneGraph.Java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class SolutionTest {
+    
+    Solution solution;
+    
+    @BeforeEach
+    public void setUp() throws Exception {
+        solution = new Solution();
+    }
+    
+    @AfterEach
+    public void tearDown() throws Exception {
+        solution = null;
+    }
+    
+    @Test
+    public void MainFunction() {
+        assertTimeout(Duration.ofMillis(500), () -> {
+             String[] args = new String[0];
+             assertAll(() -> Solution.main(args));
+        });
+    }
+    
+    @Test
+    public void TrivialCase1() {
+        UndirectedGraphNode a = new UndirectedGraphNode(1);
+        UndirectedGraphNode b = new UndirectedGraphNode(2);
+        UndirectedGraphNode c = new UndirectedGraphNode(3);
+        a.neighbors.add(b);
+        a.neighbors.add(c);
+        assertTimeout(Duration.ofMillis(500), () -> {
+            ArrayList<Integer> expected = new ArrayList<Integer>(Arrays.asList(1, 2, 3));
+            UndirectedGraphNode clone = Solution.cloneGraph(a);
+            ArrayList<Integer> actual = Solution.dfs(clone);
+            assertEquals(expected, actual);
+        });
+    }
+}
---
+++ src/InterviewBit/src/Courses/Programming/Level8_Graphs/Problems/GraphHashing/CloneGraph/Java/UndirectedGraphNode.java
@@ -0,0 +1,10 @@
+package InterviewBit.src.Courses.Programming.Level8_Graphs.Problems.GraphHashing.CloneGraph.Java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class UndirectedGraphNode {
+    int label;
+    List<UndirectedGraphNode> neighbors;
+    UndirectedGraphNode(int x) { label = x; neighbors = new ArrayList<UndirectedGraphNode>(); }
+};
