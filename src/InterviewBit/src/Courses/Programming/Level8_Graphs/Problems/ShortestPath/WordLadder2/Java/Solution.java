bc936159d06019a52aeade146cc7e31a4fc8d98f
Manas Talukdar <manas.talukdar@gmail.com>
2018-04-12 15:52:31 -0700

    Word Ladder 2

---
+++ src/InterviewBit/src/Courses/Programming/Level8_Graphs/Problems/ShortestPath/WordLadder2/Java/Solution.java
@@ -0,0 +1,80 @@
+package InterviewBit.src.Courses.Programming.Level8_Graphs.Problems.ShortestPath.WordLadder2.Java;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+
+public class Solution {
+    public static void main(String[] args) {
+        String start = "hit";
+        String end = "cog";
+        ArrayList<String> dict = new ArrayList<String>(Arrays.asList("hot","dot","dog","lot","log"));
+        System.out.println(findLadders(start, end, dict));
+    }
+
+    public static ArrayList<ArrayList<String>> findLadders(String start, String end, ArrayList<String> dict) {
+        ArrayList<ArrayList<String>> result = new ArrayList<ArrayList<String>>();
+        Queue<WordNode> queue  = new LinkedList<WordNode>();
+        queue.offer(new WordNode(start, 1, null));                
+        
+        Set<String> dictSet = new HashSet<String>(dict);
+        dictSet.add(end);
+        int minStep  = 0;
+        
+        Set<String> visited = new HashSet<String>();
+        Set<String> unvisited  = new HashSet<String>();
+        unvisited.addAll(dictSet);
+        
+        int preNumSteps = 0;
+        
+        while(!queue.isEmpty()){   
+            //System.out.println("Queue : " + queue);
+            WordNode top = queue.poll();
+            //System.out.println("For Word : " + top.word);
+            String word = top.word;
+            int currentNumSteps = top.numSteps;
+            
+            if(word.equals(end)) {
+                if(minStep == 0) {
+                    minStep = top.numSteps;
+                }
+                if(top.numSteps == minStep && minStep != 0) {
+                    ArrayList<String> t = new ArrayList<String>();
+                    t.add(top.word);
+                    while(top.pre != null) {
+                        t.add(0, top.pre.word);
+                        top = top.pre;
+                    }
+                    result.add(t);
+                    continue;
+                }
+            }
+            
+            if(preNumSteps < currentNumSteps) {
+                unvisited.removeAll(visited);
+            }
+            
+            preNumSteps = currentNumSteps;
+            
+            char[] arr = word.toCharArray();
+            for(int i = 0; i < arr.length; i++) {
+                for(char c = 'a'; c <= 'z'; c++) {
+                    char temp = arr[i];
+                    if(arr[i] != c) {
+                        arr[i] = c;
+                    }
+                    String newWord = new String(arr);
+                    if(unvisited.contains(newWord)) {                       
+                        queue.offer(new WordNode(newWord, top.numSteps + 1, top));
+                        visited.add(newWord);
+                    }
+                    arr[i] = temp;
+                }
+            }            
+        }
+        return result;
+	}
+}
---
+++ src/InterviewBit/src/Courses/Programming/Level8_Graphs/Problems/ShortestPath/WordLadder2/readme.md
@@ -0,0 +1,41 @@
+# Problem Definition
+
+## Description
+
+Given two words (start and end), and a dictionary, find the shortest transformation sequence from start to end, such that:
+
+* Only one letter can be changed at a time
+* Each intermediate word must exist in the dictionary
+
+If there are multiple such sequence of shortest length, return all of them. Refer to the example for more details.
+
+**Example:**
+
+Given:
+
+```text
+start = "hit"
+end = "cog"
+dict = ["hot","dot","dog","lot","log"]
+```
+
+Return
+
+```text
+  [
+    ["hit","hot","dot","dog","cog"],
+    ["hit","hot","lot","log","cog"]
+  ]
+```
+
+**Note:**
+
+* All words have the same length.
+* All words contain only lowercase alphabetic characters.
+
+## Notes
+
+1. [GitHub - ismagilov](https://github.com/ismagilov/interviewbit/blob/f70118970261234c2c7932513094fff481c6856a/src/graphs/WordLadderII.java)
+1. [GitHub - fakemonk1](https://github.com/fakemonk1/DataStructures-And-Algorithms-IB/blob/c94d48fc99282825ffb78144baddeeb990243145/src/graphs/WordLadder2.java)
+1. [GitHub - nagajyothi](https://github.com/nagajyothi/InterviewBit/blob/master/Graphs/WordLadder2.java)
+1. [LeetCode discussion - My concise JAVA solution based on BFS and DFS](https://leetcode.com/problems/word-ladder-ii/discuss/40475/My-concise-JAVA-solution-based-on-BFS-and-DFS)
