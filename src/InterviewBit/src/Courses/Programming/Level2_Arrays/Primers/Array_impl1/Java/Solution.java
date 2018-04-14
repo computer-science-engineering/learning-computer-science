8a50f1015c3108ea59deee4cb203faca32a84fbd
Manas Talukdar <manas.talukdar@gmail.com>
2018-04-13 18:07:34 -0700

    Array Impl 1 - added test.

--- src/InterviewBit/src/Courses/Programming/Level2_Arrays/Primers/Array_impl1/Java/Solution.java
+++ src/InterviewBit/src/Courses/Programming/Level2_Arrays/Primers/Array_impl1/Java/Solution.java
@@ -1,32 +1,30 @@
 package InterviewBit.src.Courses.Programming.Level2_Arrays.Primers.Array_impl1.Java;
 
-import java.util.*;
+import java.util.ArrayList;
 
 public class Solution {
-	public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
         ArrayList<Integer> input = new ArrayList<Integer>();
         input.add(5);
         input.add(10);
         input.add(2);
         input.add(1);
         ArrayList<Integer> result = performOps(input);
-		scan.close();
-		for (int i = 0; i < result.size(); i++) {
-			System.out.print(result.get(i) + " ");
-		}
-        //System.out.println(result);
-	}	
-	
-	public static ArrayList<Integer> performOps(ArrayList<Integer> A) {
+        for (int i = 0; i < result.size(); i++) {
+            System.out.print(result.get(i) + " ");
+        }
+        System.out.println(result);
+    }	
+
+    public static ArrayList<Integer> performOps(ArrayList<Integer> A) {
         ArrayList<Integer> B = new ArrayList<Integer>();
         for (int i = 0; i < 2 * A.size(); i++) {
-			B.add(0);
-		}
+            B.add(0);
+        }
         for (int i = 0; i < A.size(); i++) {
-                B.set(i, A.get(i));
-                B.set(i + A.size(), A.get((A.size() - i) % A.size()));
+            B.set(i, A.get(i));
+            B.set(i + A.size(), A.get((A.size() - i) % A.size()));
         }
         return B;
-}
+    }
 }
