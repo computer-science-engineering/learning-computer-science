36c4abc2fb822c768a007f1ab67c9747bc99be48
Manas Talukdar <manas.talukdar@gmail.com>
2018-04-16 23:33:46 -0700

    added tests.

--- src/InterviewBit/src/Courses/Programming/Level2_Arrays/Problems/ValueRanges/MergeIntervals/Java/SolutionTest.java
+++ src/InterviewBit/src/Courses/Programming/Level2_Arrays/Problems/ValueRanges/MergeIntervals/Java/SolutionTest.java
@@ -3,7 +3,10 @@ package InterviewBit.src.Courses.Programming.Level2_Arrays.Problems.ValueRanges.
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.Duration;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -38,12 +41,19 @@ public class SolutionTest {
         Interval newInterval = new Interval(2, 5);
         assertTimeout(Duration.ofMillis(500), () -> {
             ArrayList<Interval> actual = Solution.insert(input, newInterval);
+            ArrayList<Interval> actual2 = Solution.insert2(input, newInterval);
             List<Interval> expected = Arrays.asList(new Interval(1, 5), new Interval(6, 9));
             int index = 0;
             for (Interval interval : actual) {
                 assertEquals(interval.start, expected.get(index).start);
                 assertEquals(interval.end, expected.get(index).end);
                 index++;
+            }
+            int index2 = 0;
+            for (Interval interval : actual2) {
+                assertEquals(interval.start, expected.get(index2).start);
+                assertEquals(interval.end, expected.get(index2).end);
+                index2++;
             }
         });
     }
@@ -59,6 +69,7 @@ public class SolutionTest {
         Interval newInterval = new Interval(4, 9);
         assertTimeout(Duration.ofMillis(500), () -> {
             ArrayList<Interval> actual = Solution.insert(input, newInterval);
+            ArrayList<Interval> actual2 = Solution.insert2(input, newInterval);
             List<Interval> expected = Arrays.asList(new Interval(1, 2), new Interval(3, 10), new Interval(12, 16));
             int index = 0;
             for (Interval interval : actual) {
@@ -66,23 +77,36 @@ public class SolutionTest {
                 assertEquals(interval.end, expected.get(index).end);
                 index++;
             }
+            int index2 = 0;
+            for (Interval interval : actual2) {
+                assertEquals(interval.start, expected.get(index2).start);
+                assertEquals(interval.end, expected.get(index2).end);
+                index2++;
+            }
         });
     }
 
     @Test
-    public void PreceedingInterval() {
+    public void PrecedingInterval() {
         ArrayList<Interval> input = new ArrayList<Interval>();
         input.add(new Interval(3, 6));
         input.add(new Interval(8, 10));
         Interval newInterval = new Interval(1, 2);
         assertTimeout(Duration.ofMillis(500), () -> {
             ArrayList<Interval> actual = Solution.insert(input, newInterval);
+            ArrayList<Interval> actual2 = Solution.insert2(input, newInterval);
             List<Interval> expected = Arrays.asList(new Interval(1, 2), new Interval(3, 6), new Interval(8, 10));
             int index = 0;
             for (Interval interval : actual) {
                 assertEquals(interval.start, expected.get(index).start);
                 assertEquals(interval.end, expected.get(index).end);
                 index++;
+            }
+            int index2 = 0;
+            for (Interval interval : actual2) {
+                assertEquals(interval.start, expected.get(index2).start);
+                assertEquals(interval.end, expected.get(index2).end);
+                index2++;
             }
         });
     }
@@ -95,12 +119,19 @@ public class SolutionTest {
         Interval newInterval = new Interval(8, 10);
         assertTimeout(Duration.ofMillis(500), () -> {
             ArrayList<Interval> actual = Solution.insert(input, newInterval);
+            ArrayList<Interval> actual2 = Solution.insert2(input, newInterval);
             List<Interval> expected = Arrays.asList(new Interval(1, 2), new Interval(3, 6), new Interval(8, 10));
             int index = 0;
             for (Interval interval : actual) {
                 assertEquals(interval.start, expected.get(index).start);
                 assertEquals(interval.end, expected.get(index).end);
                 index++;
+            }
+            int index2 = 0;
+            for (Interval interval : actual2) {
+                assertEquals(interval.start, expected.get(index2).start);
+                assertEquals(interval.end, expected.get(index2).end);
+                index2++;
             }
         });
     }
@@ -113,12 +144,19 @@ public class SolutionTest {
         Interval newInterval = new Interval(1, 10);
         assertTimeout(Duration.ofMillis(500), () -> {
             ArrayList<Interval> actual = Solution.insert(input, newInterval);
+            ArrayList<Interval> actual2 = Solution.insert2(input, newInterval);
             List<Interval> expected = Arrays.asList(new Interval(1, 10));
             int index = 0;
             for (Interval interval : actual) {
                 assertEquals(interval.start, expected.get(index).start);
                 assertEquals(interval.end, expected.get(index).end);
                 index++;
+            }
+            int index2 = 0;
+            for (Interval interval : actual2) {
+                assertEquals(interval.start, expected.get(index2).start);
+                assertEquals(interval.end, expected.get(index2).end);
+                index2++;
             }
         });
     }
