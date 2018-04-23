2b5a492c7e7f6a5d5582e4f0cf6487ef3df54e3a
Manas Talukdar <manas.talukdar@gmail.com>
2018-04-23 00:06:53 -0700

    cleanup.

--- src/InterviewBit/src/Courses/Programming/Level2_Math/Problems/DigitOp/PalindromeInteger/Java/Solution.java
+++ src/InterviewBit/src/Courses/Programming/Level2_Math/Problems/DigitOp/PalindromeInteger/Java/Solution.java
@@ -19,13 +19,11 @@ public class Solution {
     public static int reverse(int A) {
         long reversedNum = 0;        
         long input_long = A;    
-        while (input_long != 0)
-        {
+        while (input_long != 0) {
             reversedNum = reversedNum * 10 + input_long % 10;
             input_long = input_long / 10;
         }    
-        if (reversedNum > Integer.MAX_VALUE || reversedNum < Integer.MIN_VALUE)
-        {
+        if (reversedNum > Integer.MAX_VALUE || reversedNum < Integer.MIN_VALUE) {
             reversedNum = 0;
         }
         return (int)reversedNum;
