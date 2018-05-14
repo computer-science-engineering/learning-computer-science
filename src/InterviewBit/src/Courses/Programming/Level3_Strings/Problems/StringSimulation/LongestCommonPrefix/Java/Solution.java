07a9781b991c43079b56bc89c14d90107e0d13b0
Manas Talukdar <manas.talukdar@gmail.com>
2018-05-14 00:48:39 -0700

    cleanup.

--- src/InterviewBit/src/Courses/Programming/Level3_Strings/Problems/StringSimulation/LongestCommonPrefix/Java/Solution.java
+++ src/InterviewBit/src/Courses/Programming/Level3_Strings/Problems/StringSimulation/LongestCommonPrefix/Java/Solution.java
@@ -9,18 +9,19 @@ public class Solution {
 
     public static String longestCommonPrefix(String[] A) {
         int minLen = Integer.MAX_VALUE;
-        for(int i = 0; i < A.length; i++){
-            if(minLen > A[i].length())
+        for(int i = 0; i < A.length; i++) {
+            if(minLen > A[i].length()) {
                 minLen = A[i].length();
+            }
         }
         
         if(minLen == 0) {
             return "";
         }
 
-        for(int j = 0; j < minLen; j++){
+        for(int j = 0; j < minLen; j++) {
             char prev = '0';
-            for(int i = 0; i < A.length; i++){
+            for(int i = 0; i < A.length; i++) {
                 if(i == 0) {
                     prev = A[i].charAt(j);
                     continue;
