b36eec33cd494ad922cfef02fd7f3fc00644487e
Manas Talukdar <manas.talukdar@gmail.com>
2018-06-28 00:15:52 -0700

    updates

---
+++ src/HackerRank/src/Tracks/ProblemSolving/Algorithms/Warmup/SolveMeFirst/Java/Solution.java
@@ -0,0 +1,23 @@
+package HackerRank.src.Tracks.ProblemSolving.Algorithms.Warmup.SolveMeFirst.Java;
+
+import java.util.*;
+
+public class Solution {
+
+    static int solveMeFirst(int a, int b) {
+         return a + b;
+   }
+
+   
+ public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a;
+        a = in.nextInt();
+        int b;
+        b = in.nextInt();
+        int sum;
+        sum = solveMeFirst(a, b);
+        System.out.println(sum);
+        in.close();
+   }
+}
