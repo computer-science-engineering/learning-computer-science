e91c8777b592e3cb9aee0cc058fe155b0341b6ec
Manas Talukdar <manas.talukdar@gmail.com>
2018-06-28 17:38:58 -0700

    ProductOfArrayExceptSelf - DCP

---
+++ src/DailyCodingProblem/src/ProductOfArrayExceptSelf/Java/Solution.java
@@ -0,0 +1,28 @@
+package DailyCodingProblem.src.ProductOfArrayExceptSelf.Java;
+
+public class Solution {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5};
+        int[] result = productExceptSelf(nums);
+        for(int i=0; i<result.length; i++) {
+            System.out.print(result[i] + " ");
+        }
+    }
+
+    public static int[] productExceptSelf(int[] nums) {
+        int[] result = new int[nums.length];
+     
+        result[nums.length-1] = 1;
+        for(int i=nums.length-2; i>=0; i--) {
+            result[i] = result[i+1]*nums[i+1];
+        }
+     
+        int left=1;
+        for(int i=0; i<nums.length; i++) {
+            result[i] = result[i]*left;
+            left = left*nums[i];
+        }
+     
+        return result;
+    }
+}
---
+++ src/DailyCodingProblem/src/ProductOfArrayExceptSelf/Java/SolutionTest.java
@@ -0,0 +1,41 @@
+package DailyCodingProblem.src.ProductOfArrayExceptSelf.Java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class SolutionTest {
+    
+    Solution solution;
+    
+    @BeforeEach
+    public void setUp() throws Exception {
+        solution = new Solution();
+    }
+    
+    @AfterEach
+    public void tearDown() throws Exception {
+        solution = null;
+    }
+    
+    @Test
+    public void MainFunction() {
+        assertTimeout(Duration.ofMillis(500), () -> {
+             String[] args = new String[0];
+             assertAll(() -> Solution.main(args));
+        });
+    }
+    
+    @Test
+    public void TrivialCase1() {
+        int[] nums = {1, 2, 3, 4, 5};
+        assertTimeout(Duration.ofMillis(500), () -> {
+            int[] expected = {120, 60, 40, 30, 24};
+            int[] actual = Solution.productExceptSelf(nums);
+            assertArrayEquals(expected, actual);
+        });
+    }
+}
