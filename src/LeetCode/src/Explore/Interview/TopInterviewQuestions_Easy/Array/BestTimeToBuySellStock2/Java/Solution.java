cb9b9c4f655605ad6d67405e991f10dd676e27b1
Manas Talukdar <manas.talukdar@gmail.com>
2018-07-01 20:23:46 -0700

    BestTimeToBuySellStock2

---
+++ src/LeetCode/src/Explore/Interview/TopInterviewQuestions_Easy/Array/BestTimeToBuySellStock2/Java/Solution.java
@@ -0,0 +1,19 @@
+package LeetCode.src.Explore.Interview.TopInterviewQuestions_Easy.Array.BestTimeToBuySellStock2.Java;
+
+public class Solution {
+    public static void main(String[] args) {
+        int[] prices = {1,2,3,4,5};
+        System.out.println(maxProfit(prices));
+    }
+
+    public static int maxProfit(int[] prices) {
+        int total = 0;
+        for (int i=0; i< prices.length-1; i++) {
+            if (prices[i+1] > prices[i]) {
+                total += prices[i+1]-prices[i];
+            }
+        }
+        
+        return total;
+    }
+}
