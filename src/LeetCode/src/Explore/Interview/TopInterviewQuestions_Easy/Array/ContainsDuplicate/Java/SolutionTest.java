23c2eff27706432e390e46f5d7b444d2f99e7016
Manas Talukdar <manas.talukdar@gmail.com>
2018-07-01 22:32:12 -0700

    ContainsDuplicate

---
+++ src/LeetCode/src/Explore/Interview/TopInterviewQuestions_Easy/Array/ContainsDuplicate/Java/SolutionTest.java
@@ -0,0 +1,51 @@
+package LeetCode.src.Explore.Interview.TopInterviewQuestions_Easy.Array.ContainsDuplicate.Java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class SolutionTest {
+    
+    Solution solution;
+    
+    @BeforeEach
+    public void setUp() throws Exception {
+        solution = new Solution();
+    }
+    
+    @AfterEach
+    public void tearDown() throws Exception {
+        solution = null;
+    }
+    
+    @Test
+    public void MainFunction() {
+        assertTimeout(Duration.ofMillis(500), () -> {
+             String[] args = new String[0];
+             assertAll(() -> Solution.main(args));
+        });
+    }
+    
+    @Test
+    public void TrivialCase1() {
+        int[] nums = {1,2,3,1};
+        assertTimeout(Duration.ofMillis(500), () -> {
+            boolean expected = true;
+            boolean actual = Solution.containsDuplicate(nums);
+            assertEquals(expected, actual);
+        });
+    }
+
+    @Test
+    public void TrivialCase2() {
+        int[] nums = {1,2,3,4};
+        assertTimeout(Duration.ofMillis(500), () -> {
+            boolean expected = false;
+            boolean actual = Solution.containsDuplicate(nums);
+            assertEquals(expected, actual);
+        });
+    }
+}
---
+++ src/LeetCode/src/Explore/Interview/TopInterviewQuestions_Easy/Array/ContainsDuplicate/readme.md
@@ -0,0 +1,30 @@
+# Problem Definition
+
+## Description
+
+Given an array of integers, find if the array contains any duplicates.
+
+Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+
+**Example 1:**
+
+```text
+Input: [1,2,3,1]
+Output: true
+```
+
+**Example 2:**
+
+```text
+Input: [1,2,3,4]
+Output: false
+```
+
+**Example 3:**
+
+```text
+Input: [1,1,1,3,3,4,3,2,4,2]
+Output: true
+```
+
+## Notes
