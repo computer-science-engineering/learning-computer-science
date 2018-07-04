6f58b1f6e7c295736727b66f951be1a35afe3a0a
Manas Talukdar <manas.talukdar@gmail.com>
2018-07-03 23:29:26 -0700

    FirstUniqueCharacterInAString

---
+++ src/LeetCode/src/Explore/Interview/TopInterviewQuestions_Easy/Strings/FirstUniqueCharacterInAString/Java/SolutionTest.java
@@ -0,0 +1,55 @@
+package LeetCode.src.Explore.Interview.TopInterviewQuestions_Easy.Strings.FirstUniqueCharacterInAString.Java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class SolutionTest {
+    
+    Solution solution;
+    
+    @BeforeEach
+    public void setUp() throws Exception {
+        solution = new Solution();
+    }
+    
+    @AfterEach
+    public void tearDown() throws Exception {
+        solution = null;
+    }
+    
+    @Test
+    public void MainFunction() {
+        assertTimeout(Duration.ofMillis(500), () -> {
+             String[] args = new String[0];
+             assertAll(() -> Solution.main(args));
+        });
+    }
+    
+    @Test
+    public void TrivialCase1() {
+        String s = "loveleetcode";
+        assertTimeout(Duration.ofMillis(500), () -> {
+            int expected = 2;
+            int actual1 = Solution.firstUniqChar(s);
+            assertEquals(expected, actual1);
+            int actual2 = Solution.firstUniqChar_optimized(s);
+            assertEquals(expected, actual2);
+        });
+    }
+
+    @Test
+    public void TrivialCase2() {
+        String s = "dogdog";
+        assertTimeout(Duration.ofMillis(500), () -> {
+            int expected = -1;
+            int actual = Solution.firstUniqChar(s);
+            assertEquals(expected, actual);
+            int actual2 = Solution.firstUniqChar(s);
+            assertEquals(expected, actual2);
+        });
+    }
+}
---
+++ src/LeetCode/src/Explore/Interview/TopInterviewQuestions_Easy/Strings/FirstUniqueCharacterInAString/readme.md
@@ -0,0 +1,41 @@
+# Problem Definition
+
+## Description
+
+Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+
+**Examples:**
+
+```text
+s = "leetcode"
+return 0.
+
+s = "loveleetcode",
+return 2.
+```
+
+**Note:** You may assume the string contain only lowercase letters.
+
+## Notes
+
+1. [LeetCode discussion - Java 7 lines solution 29ms](https://leetcode.com/problems/first-unique-character-in-a-string/discuss/86348/Java-7-lines-solution-29ms)
+
+    Hey guys. My solution is pretty straightforward. It takes O(n) and goes through the string twice:
+
+    Get the frequency of each character.
+    Get the first character that has a frequency of one.
+    Actually the code below passes all the cases. However, according to @xietao0221, we could change the size of the frequency array to 256 to store other kinds of characters. Thanks for all the other comments and suggestions. Fight on!
+
+    ```java
+    public class Solution {
+        public int firstUniqChar(String s) {
+            int freq [] = new int[26];
+            for(int i = 0; i < s.length(); i ++)
+                freq [s.charAt(i) - 'a'] ++;
+            for(int i = 0; i < s.length(); i ++)
+                if(freq [s.charAt(i) - 'a'] == 1)
+                    return i;
+            return -1;
+        }
+    }
+    ```
