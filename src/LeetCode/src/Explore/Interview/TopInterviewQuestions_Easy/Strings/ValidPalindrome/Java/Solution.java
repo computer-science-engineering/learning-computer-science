68f46a5e2017652f5d62f044b21efd21905c3442
Manas Talukdar <manas.talukdar@gmail.com>
2018-07-04 00:34:38 -0700

    ValidPalindrome

---
+++ src/LeetCode/src/Explore/Interview/TopInterviewQuestions_Easy/Strings/ValidPalindrome/Java/Solution.java
@@ -0,0 +1,31 @@
+package LeetCode.src.Explore.Interview.TopInterviewQuestions_Easy.Strings.ValidPalindrome.Java;
+
+public class Solution {
+    public static void main(String[] args) {
+        String s = "A man, a plan, a canal: Panama";
+        System.out.println(isPalindrome(s));
+    }
+
+    public static boolean isPalindrome(String s) {
+        char[] ch = s.toLowerCase().toCharArray();
+        int i = 0;
+        int j = ch.length-1;
+        while(i<j) {
+            if (!Character.isLetterOrDigit(ch[i])) {
+                i++;
+                continue;
+            }
+            if (!Character.isLetterOrDigit(ch[j])) {
+                j--;
+                continue;
+            }
+            if (ch[i] != ch[j]) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+}
---
+++ src/LeetCode/src/Explore/Interview/TopInterviewQuestions_Easy/Strings/ValidPalindrome/readme.md
@@ -0,0 +1,23 @@
+# Problem Definition
+
+## Description
+
+Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+**Note:** For the purpose of this problem, we define empty string as valid palindrome.
+
+**Example 1:**
+
+```text
+Input: "A man, a plan, a canal: Panama"
+Output: true
+```
+
+**Example 2:**
+
+```text
+Input: "race a car"
+Output: false
+```
+
+## Notes
