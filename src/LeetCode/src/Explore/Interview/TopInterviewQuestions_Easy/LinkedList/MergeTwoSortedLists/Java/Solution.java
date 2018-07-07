80f2546159d7494aecaf29632c6339e10b3cc299
Manas Talukdar <manas.talukdar@gmail.com>
2018-07-06 22:29:59 -0700

    Merge Two Sorted Lists

---
+++ src/LeetCode/src/Explore/Interview/TopInterviewQuestions_Easy/LinkedList/MergeTwoSortedLists/Java/Solution.java
@@ -0,0 +1,61 @@
+package LeetCode.src.Explore.Interview.TopInterviewQuestions_Easy.LinkedList.MergeTwoSortedLists.Java;
+
+public class Solution {
+    public static void main(String[] args) {
+        ListNode a1 = new ListNode(1);
+        ListNode a2 = new ListNode(2);
+        ListNode a3 = new ListNode(4);
+        a1.next = a2;
+        a2.next = a3;
+        ListNode b1 = new ListNode(1);
+        ListNode b2 = new ListNode(3);
+        ListNode b3 = new ListNode(4);
+        b1.next = b2;
+        b2.next = b3;
+        ListNode result = mergeTwoLists(a1, b1);
+        while(result.next != null) {
+            System.out.print(result.val + "->");
+            result = result.next;
+        }
+        System.out.print(result.val);
+    }
+
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1 == null) {
+            return l2;
+        }
+		if(l2 == null) {
+            return l1;
+        }
+		if(l1.val < l2.val) {
+			l1.next = mergeTwoLists(l1.next, l2);
+			return l1;
+		} else {
+			l2.next = mergeTwoLists(l1, l2.next);
+			return l2;
+		}
+    }
+
+    public static ListNode mergeTwoLists_Iterative(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode handler = head;
+        while(l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                handler.next = l1;
+                l1 = l1.next;
+            } else {
+                handler.next = l2;
+                l2 = l2.next;
+            }
+            handler = handler.next;
+        }
+        
+        if (l1 != null) {
+            handler.next = l1;
+        } else if (l2 != null) {
+            handler.next = l2;
+        }
+        
+        return head.next;
+    }
+}
