893faa20f20a0b4c853c9faf563d1b4eea14bbb4
Manas Talukdar <manas.talukdar@gmail.com>
2018-07-07 23:53:28 -0700

    Symmetric Tree

---
+++ src/LeetCode/src/Explore/Interview/TopInterviewQuestions_Easy/Trees/SymmetricTree/Java/SolutionTest.java
@@ -0,0 +1,63 @@
+package LeetCode.src.Explore.Interview.TopInterviewQuestions_Easy.Trees.SymmetricTree.Java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class SolutionTest {
+    
+    Solution solution;
+    
+    @BeforeEach
+    public void setUp() throws Exception {
+        solution = new Solution();
+    }
+    
+    @AfterEach
+    public void tearDown() throws Exception {
+        solution = null;
+    }
+    
+    @Test
+    public void MainFunction() {
+        assertTimeout(Duration.ofMillis(500), () -> {
+             String[] args = new String[0];
+             assertAll(() -> Solution.main(args));
+        });
+    }
+    
+    @Test
+    public void TrivialCase1() {
+        int[] values = {
+            1,
+            2, 2,
+            3, 4, 4, 3};
+        TreeNode root = TreeNode.fromArray(values);
+        assertTimeout(Duration.ofMillis(500), () -> {
+            boolean expected = true;
+            boolean actual_recursive = Solution.isSymmetric(root);
+            assertEquals(expected, actual_recursive);
+            boolean actual_iterative = Solution.isSymmetric_Iterative(root);
+            assertEquals(expected, actual_iterative);
+        });
+    }
+
+    @Test
+    public void TrivialCase2() {
+        int[] values = {
+            1,
+            2, 2,
+            -1, 3, -1, 3};
+        TreeNode root = TreeNode.fromArray(values);
+        assertTimeout(Duration.ofMillis(500), () -> {
+            boolean expected = false;
+            boolean actual_recursive = Solution.isSymmetric(root);
+            assertEquals(expected, actual_recursive);
+            boolean actual_iterative = Solution.isSymmetric_Iterative(root);
+            assertEquals(expected, actual_iterative);
+        });
+    }
+}
---
+++ src/LeetCode/src/Explore/Interview/TopInterviewQuestions_Easy/Trees/SymmetricTree/readme.md
@@ -0,0 +1,32 @@
+# Problem Definition
+
+## Description
+
+Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+
+For example, this binary tree `[1,2,2,3,4,4,3]` is symmetric:
+
+```text
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+```
+
+But the following `[1,2,2,null,3,null,3]` is not:
+
+```text
+    1
+   / \
+  2   2
+   \   \
+   3    3
+```
+
+**Note:**
+Bonus points if you could solve it both recursively and iteratively.
+
+## Notes
+
+1. [LeetCode discussion - Recursive and non-recursive solutions in Java](https://leetcode.com/explore/interview/card/top-interview-questions-easy/94/trees/627/discuss/33054/Recursive-and-non-recursive-solutions-in-Java)
