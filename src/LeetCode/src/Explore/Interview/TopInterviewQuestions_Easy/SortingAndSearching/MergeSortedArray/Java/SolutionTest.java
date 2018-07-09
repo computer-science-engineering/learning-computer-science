d9f35dd1e033d208fef51b259d152552e4d8e725
Manas Talukdar <manas.talukdar@gmail.com>
2018-07-08 19:01:27 -0700

    Merge Sorted Array

---
+++ src/LeetCode/src/Explore/Interview/TopInterviewQuestions_Easy/SortingAndSearching/MergeSortedArray/Java/SolutionTest.java
@@ -0,0 +1,45 @@
+package LeetCode.src.Explore.Interview.TopInterviewQuestions_Easy.SortingAndSearching.MergeSortedArray.Java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class SolutionTest {
+    
+    Solution solution;
+    
+    @BeforeEach
+    public void setUp() throws Exception {
+        solution = new Solution();
+    }
+    
+    @AfterEach
+    public void tearDown() throws Exception {
+        solution = null;
+    }
+    
+    @Test
+    public void MainFunction() {
+        assertTimeout(Duration.ofMillis(500), () -> {
+             String[] args = new String[0];
+             assertAll(() -> Solution.main(args));
+        });
+    }
+    
+    @Test
+    public void TrivialCase1() {
+        int[] nums1 = {1,2,3,0,0,0};
+        int m = 3;
+        int[] nums2 = {2,5,6};
+        int n = 3;
+        assertTimeout(Duration.ofMillis(500), () -> {
+            int[] expected = {1,2,2,3,5,6};
+            Solution.merge(nums1, m, nums2, n);
+            int[] actual = nums1;
+            assertArrayEquals(expected, actual);
+        });
+    }
+}
---
+++ src/LeetCode/src/Explore/Interview/TopInterviewQuestions_Easy/SortingAndSearching/MergeSortedArray/readme.md
@@ -0,0 +1,24 @@
+# Problem Definition
+
+## Description
+
+Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+
+**Note:**
+
+* The number of elements initialized in nums1 and nums2 are m and n respectively.
+* You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2.
+
+**Example:**
+
+```text
+Input:
+nums1 = [1,2,3,0,0,0], m = 3
+nums2 = [2,5,6],       n = 3
+
+Output: [1,2,2,3,5,6]
+```
+
+## Notes
+
+1. [LeetCode discussion - Share my accepted Java solution!](https://leetcode.com/explore/interview/card/top-interview-questions-easy/96/sorting-and-searching/587/discuss/29578/Share-my-accepted-Java-solution!)
