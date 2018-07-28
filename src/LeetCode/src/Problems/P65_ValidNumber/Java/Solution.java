36d4be1ae5cccb1dcec73d320e66a933d0c4a9df
Manas Talukdar <manas.talukdar@gmail.com>
2018-07-28 16:35:04 -0700

    Valid Number

---
+++ src/LeetCode/src/Problems/P65_ValidNumber/Java/Solution.java
@@ -0,0 +1,23 @@
+package LeetCode.src.Problems.P65_ValidNumber.Java;
+
+public class Solution {
+    public static void main(String[] args) {
+        String s = "0";
+        System.out.println(isNumber(s));
+    }
+
+    public static boolean isNumber(String s) {
+        try {
+            s = s.trim();
+            int n = s.length();
+            if ( n == 0 || (s.charAt(n-1) != '.' && (s.charAt(n-1) - '0' < 0 || s.charAt(n-1) - '0' > 9 )) ) {
+                return false;
+            }
+            double i = Double.parseDouble(s);
+            return true;
+        }
+        catch (NumberFormatException e) {
+            return false;
+        }
+    }
+}
