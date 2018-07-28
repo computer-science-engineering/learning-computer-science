c7e78d77df7c8bc3ac1fcbc976f0d3dc5eec6c59
Manas Talukdar <manas.talukdar@gmail.com>
2018-07-28 15:40:52 -0700

    Repeated DNA Sequences

---
+++ src/LeetCode/src/Problems/P187_RepeatedDnaSequences/Java/SolutionTest.java
@@ -0,0 +1,44 @@
+package LeetCode.src.Problems.P187_RepeatedDnaSequences.Java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class SolutionTest {
+    
+    Solution solution;
+    
+    @BeforeEach
+    public void setUp() throws Exception {
+        solution = new Solution();
+    }
+    
+    @AfterEach
+    public void tearDown() throws Exception {
+        solution = null;
+    }
+    
+    @Test
+    public void MainFunction() {
+        assertTimeout(Duration.ofMillis(500), () -> {
+             String[] args = new String[0];
+             assertAll(() -> Solution.main(args));
+        });
+    }
+    
+    @Test
+    public void TrivialCase1() {
+        String s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT";
+        assertTimeout(Duration.ofMillis(500), () -> {
+            List<String> expected = Arrays.asList("AAAAACCCCC", "CCCCCAAAAA");
+            List<String> actual = Solution.findRepeatedDnaSequences(s);
+            assertEquals(expected, actual);
+        });
+    }
+}
---
+++ src/LeetCode/src/Problems/P187_RepeatedDnaSequences/readme.md
@@ -0,0 +1,20 @@
+# Problem Definition
+
+## Description
+
+All DNA is composed of a series of nucleotides abbreviated as A, C, G, and T, for example: "ACGAATTCCG". When studying DNA, it is sometimes useful to identify repeated sequences within the DNA.
+
+Write a function to find all the 10-letter-long sequences (substrings) that occur more than once in a DNA molecule.
+
+**Example:**
+
+```text
+Input: s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"
+
+Output: ["AAAAACCCCC", "CCCCCAAAAA"]
+```
+
+## Notes
+
+1. [LeetCode discussion - 7 lines simple Java, O(n)](https://leetcode.com/problems/repeated-dna-sequences/discuss/53855/7-lines-simple-Java-O(n))
+1. [LeetCode discussion - Clean Java solution (hashmap + bits manipulation)](https://leetcode.com/problems/repeated-dna-sequences/discuss/53867/Clean-Java-solution-(hashmap-+-bits-manipulation))
