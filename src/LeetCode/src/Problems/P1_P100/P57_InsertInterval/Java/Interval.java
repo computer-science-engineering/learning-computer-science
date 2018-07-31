538ad14a9076e47c68020f7c97545bfaf597ae95
Manas Talukdar <manas.talukdar@gmail.com>
2018-07-31 14:44:03 -0700

    Insert Interval

---
+++ src/LeetCode/src/Problems/P1_P100/P57_InsertInterval/Java/Interval.java
@@ -0,0 +1,8 @@
+package LeetCode.src.Problems.P1_P100.P57_InsertInterval.Java;
+
+public class Interval {
+    int start;
+    int end;
+    Interval() { start = 0; end = 0; }
+    Interval(int s, int e) { start = s; end = e; }
+}
