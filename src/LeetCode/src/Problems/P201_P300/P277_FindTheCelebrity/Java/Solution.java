3a140d4f2913b4ca435c43d5be7432b03709a967
Manas Talukdar <manas.talukdar@gmail.com>
2018-07-31 15:22:14 -0700

    P277_FindTheCelebrity

---
+++ src/LeetCode/src/Problems/P201_P300/P277_FindTheCelebrity/Java/Solution.java
@@ -0,0 +1,11 @@
+package LeetCode.src.Problems.P201_P300.P277_FindTheCelebrity.Java;
+
+public class Solution extends Relation {
+    public static void main(String[] args) {
+        
+    }
+
+    public int findCelebrity(int n) {
+        return 0;
+    }
+}
