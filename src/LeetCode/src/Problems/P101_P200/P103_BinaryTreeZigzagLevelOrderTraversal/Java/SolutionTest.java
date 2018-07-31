44de633cff6a7239af23b832bb77708261066d4e
Manas Talukdar <manas.talukdar@gmail.com>
2018-07-30 23:36:04 -0700

    Binary Tree Zigzag Level Order Traversal

---
+++ src/LeetCode/src/Problems/P101_P200/P103_BinaryTreeZigzagLevelOrderTraversal/Java/SolutionTest.java
@@ -0,0 +1,54 @@
+package LeetCode.src.Problems.P101_P200.P103_BinaryTreeZigzagLevelOrderTraversal.Java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class SolutionTest {
+    
+    Solution solution;
+    
+    @BeforeEach
+    public void setUp() throws Exception {
+        solution = new Solution();
+    }
+    
+    @AfterEach
+    public void tearDown() throws Exception {
+        solution = null;
+    }
+    
+    @Test
+    public void MainFunction() {
+        assertTimeout(Duration.ofMillis(500), () -> {
+             String[] args = new String[0];
+             assertAll(() -> Solution.main(args));
+        });
+    }
+    
+    @Test
+    public void TrivialCase1() {
+        int[] values = {
+            5,
+            4, 8,
+            11, -1, 13, 4,
+            7, 2, -1, -1, -1, -1, 5, 1};
+        TreeNode a = TreeNode.fromArray(values);
+        assertTimeout(Duration.ofMillis(500), () -> {
+            List<List<Integer>> expected = Arrays.asList(
+                Arrays.asList(5),
+                Arrays.asList(8, 4),
+                Arrays.asList(11, 13, 4),
+                Arrays.asList(1, 5, 2, 7)
+            );
+            List<List<Integer>> actual = Solution.zigzagLevelOrder(a);
+            assertEquals(expected, actual);
+        });
+    }
+}
---
+++ src/LeetCode/src/Problems/P101_P200/P103_BinaryTreeZigzagLevelOrderTraversal/readme.md
@@ -0,0 +1,30 @@
+# Problem Definition
+
+## Description
+
+Given a binary tree, return the *zigzag level order* traversal of its nodes' values. (ie, from left to right, then right to left for the next level and alternate between).
+
+For example:
+Given binary tree `[3,9,20,null,null,15,7]`,
+
+```text
+    3
+   / \
+  9  20
+    /  \
+   15   7
+```
+
+return its zigzag level order traversal as:
+
+```text
+[
+  [3],
+  [20,9],
+  [15,7]
+]
+```
+
+## Notes
+
+1. [LeetCode discussion - My accepted JAVA solution](https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/discuss/33815/My-accepted-JAVA-solution)
