140c390faac81a0cc682319045c0aa36c9bae3f9
Manas Talukdar <manas.talukdar@gmail.com>
2018-08-01 10:31:58 -0700

    P277_FindTheCelebrity

--- src/LeetCode/src/Problems/P201_P300/P277_FindTheCelebrity/Java/Solution.java
+++ src/LeetCode/src/Problems/P201_P300/P277_FindTheCelebrity/Java/Solution.java
@@ -2,10 +2,23 @@ package LeetCode.src.Problems.P201_P300.P277_FindTheCelebrity.Java;
 
 public class Solution extends Relation {
     public static void main(String[] args) {
-        
+        System.out.println(findCelebrity(10));
     }
 
-    public int findCelebrity(int n) {
-        return 0;
+    public static int findCelebrity(int n) {
+        if (n <= 0) {
+            return -1;
+        }
+        int candidate = 0;
+        for(int i = 1; i < n; i++) {
+            if(knows(candidate, i))
+                candidate = i;
+        }
+        for(int i = 0; i < n; i++) {
+            if(i != candidate && (knows(candidate, i) || !knows(i, candidate))) {
+                return -1;
+            }
+        }
+        return candidate;
     }
 }
