1e68bf19d29995f398a2c1d0d9cb6637dd25d8d0
Manas Talukdar <manas.talukdar@gmail.com>
2018-08-01 00:12:08 -0700

    P277_FindTheCelebrity

--- src/LeetCode/src/Problems/P201_P300/P277_FindTheCelebrity/Java/Relation.java
+++ src/LeetCode/src/Problems/P201_P300/P277_FindTheCelebrity/Java/Relation.java
@@ -1,7 +1,7 @@
 package LeetCode.src.Problems.P201_P300.P277_FindTheCelebrity.Java;
 
 public class Relation {
-    boolean knows(int a, int b) {
+    public static boolean knows(int a, int b) {
         return a == b;
     }
 }
