967a0735e888e76c49480979fe60a6b53f676f82
Manas Talukdar <manas.talukdar@gmail.com>
2018-08-23 23:58:01 -0700

    Decrypt Message

---
+++ src/Pramp/src/Problems/DecryptMessage/Java/Solution.java
@@ -0,0 +1,27 @@
+package Pramp.src.Problems.DecryptMessage.Java;
+
+public class Solution {
+    public static void main(String[] args) {
+        String word = "dnotq";
+        System.out.println(decrypt(word));
+    }
+
+    public static String decrypt(String word) {
+        int secondStep = 1;
+        String decryption = "";
+
+        for(int i = 0; i < word.length(); i++) {
+            int newLetterAscii = (int)(word.charAt(i));
+            newLetterAscii -= secondStep;
+
+            while(newLetterAscii < 97) {
+                newLetterAscii += 26;
+            }
+
+            decryption += (char) newLetterAscii;
+            secondStep += newLetterAscii;
+        }
+
+        return decryption;
+    }
+}
