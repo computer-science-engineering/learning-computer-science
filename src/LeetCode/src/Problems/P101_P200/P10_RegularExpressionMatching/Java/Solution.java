350e8e99d2a7c519ba8790e25e839271fca9bd1d
Manas Talukdar <manas.talukdar@gmail.com>
2018-08-27 00:22:05 -0700

    updates.

---
+++ src/LeetCode/src/Problems/P101_P200/P10_RegularExpressionMatching/Java/Solution.java
@@ -0,0 +1,78 @@
+package LeetCode.src.Problems.P101_P200.P10_RegularExpressionMatching.Java;
+
+class Solution {     
+    
+    public static void main(String[] args) {
+        String text =    "";
+        String pattern = "a*";
+        System.out.println(isMatch(text, pattern));
+    }
+
+    public static boolean isMatch(String s, String p) {
+        if (s == null || p == null) {
+            return false;
+        }
+        boolean[][] dp = new boolean[s.length()+1][p.length()+1];
+        dp[0][0] = true;
+        for (int i = 0; i < p.length(); i++) {
+            if (p.charAt(i) == '*' && dp[0][i-1]) {
+                dp[0][i+1] = true;
+            }
+        }
+        for (int i = 0 ; i < s.length(); i++) {
+            for (int j = 0; j < p.length(); j++) {
+                if (p.charAt(j) == '.') {
+                    dp[i+1][j+1] = dp[i][j];
+                }
+                if (p.charAt(j) == s.charAt(i)) {
+                    dp[i+1][j+1] = dp[i][j];
+                }
+                if (p.charAt(j) == '*') {
+                    if (p.charAt(j-1) != s.charAt(i) && p.charAt(j-1) != '.') {
+                        dp[i+1][j+1] = dp[i+1][j-1];
+                    } else {
+                        dp[i+1][j+1] = (dp[i+1][j] || dp[i][j+1] || dp[i+1][j-1]);
+                    }
+                }
+            }
+        }
+        return dp[s.length()][p.length()];
+    }
+  
+    public static boolean isMatch_gfg(String text, String pattern) {
+        int x = text.length();
+        int y = pattern.length();
+        
+        // easiest case
+        if (x == 0) {
+        return y == 0;
+        }
+        
+        boolean[][] track = new boolean[x+1][y+1];
+        
+        track[0][0] = true;
+        
+        // handle "*" during initialization
+        for(int j=1; j<=y; j++) {
+        if (pattern.charAt(j) == '*' && track[0][j-1]) {
+            track[0][j+1] = true;
+        }
+        }    
+
+        // populate matrix
+        for(int i=1; i<=x; i++) {
+            for(int j=1; j<=y; j++) {
+                // handle "*"
+                if (pattern.charAt(j-1) == '*') {
+                track[i][j] = track[i][j-1] || track[i-1][j] || track[i][j-2];
+                } else if (pattern.charAt(j-1) == '.' || text.charAt(i-1) == pattern.charAt(j-1)) {
+                track[i][j] = track[i-1][j-1];
+                } else  {// no match
+                track[i][j] = false;
+                }
+            }
+        }
+        
+        return track[x][y];    
+    }  
+}
--- src/LeetCode/src/Problems/P201_P300/P211_AddAndSearchWord_DataStructureDesign/readme.md
+++ src/LeetCode/src/Problems/P201_P300/P211_AddAndSearchWord_DataStructureDesign/readme.md
@@ -29,4 +29,44 @@ You may assume that all words are consist of lowercase letters `a-z`.
 ## Notes
 
 1. [LeetCode discussion - Java Solution, easy understand](https://leetcode.com/problems/add-and-search-word-data-structure-design/discuss/59669/Java-Solution-easy-understand)
+
+    ```java
+    Map<Integer, List<String>> wordsMap = new HashMap<>();
+  
+    public void AddWord(String word) {
+        int index = word.length();
+        if (wordsMap.contains(index)) {
+        wordsMap.get(index).add(word);
+        } else {
+        List<String> words = new List<>();
+        words.add(word);
+        wordsMap.add(index, words);
+        }
+    }
+
+    public bool SearchWord(String pattern) {
+        int index = pattern.length();
+        if (!wordsMap.containsKey(index)) {
+        return false;
+        }
+
+        List<String> words = wordsMap.get(index);
+        for(String word : words) {
+        if (compare(word, pattern)) {
+            return true;
+        }
+        }
+        return false;
+    }
+
+    private boolean compare(String word, String pattern) {
+        for (int i=0; i<word.length(); i++) {
+        if (pattern.charAt(i) != '.' && word.charAt(i) != pattern.charAt(i)) {
+            return false;
+        }
+        }
+        return true;
+    }
+    ```
+
 1. [LeetCode discussion - My simple and clean Java code](https://leetcode.com/problems/add-and-search-word-data-structure-design/discuss/59554/My-simple-and-clean-Java-code)
