91e641cc19c229b1ca6638ba2fb6635d311a3480
Manas Talukdar <manas.talukdar@gmail.com>
2018-09-03 23:52:14 -0700

    Three Sum

---
+++ src/LeetCode/src/Problems/P1_P100/P15_ThreeSum/Java/Solution.java
@@ -0,0 +1,43 @@
+package LeetCode.src.Problems.P1_P100.P15_ThreeSum.Java;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+        List<List<Integer>> result = threeSum(nums);
+        System.out.println(result);
+    }
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i + 2 < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {              // skip same result
+                continue;
+            }
+            int j = i + 1, k = nums.length - 1;  
+            int target = -nums[i];
+            while (j < k) {
+                if (nums[j] + nums[k] == target) {
+                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+                    while (j < k && nums[j] == nums[j - 1]) { 
+                        j++;  // skip same result
+                    }
+                    while (j < k && nums[k] == nums[k + 1]) {
+                        k--;  // skip same result
+                    }
+                } else if (nums[j] + nums[k] > target) {
+                    k--;
+                } else {
+                    j++;
+                }
+            }
+        }
+        return res;
+    }
+}
