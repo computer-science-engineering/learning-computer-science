22cece38d4bbc7278f0f8463ad8eb405fbc8c525
Manas Talukdar <manas.talukdar@gmail.com>
2018-10-08 09:13:47 -0700

    Mapping Between Message And Code

---
+++ src/Miscellaneous/src/Problems/MappingBetweenMessageAndCode/Java/SolutionTest.java
@@ -0,0 +1,86 @@
+package Miscellaneous.src.Problems.MappingBetweenMessageAndCode.Java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class SolutionTest {
+    
+    Solution solution;
+    
+    @BeforeEach
+    public void setUp() throws Exception {
+        solution = new Solution();
+    }
+    
+    @AfterEach
+    public void tearDown() throws Exception {
+        solution = null;
+    }
+    
+    @Test
+    public void MainFunction() {
+        assertTimeout(Duration.ofMillis(500), () -> {
+             String[] args = new String[0];
+             assertAll(() -> Solution.main(args));
+        });
+    }
+    
+    @Test
+    public void TrivialCase1() {
+        String message = "red green blue";
+        String code = "abc";
+        assertTimeout(Duration.ofMillis(500), () -> {
+            boolean expected = true;
+            boolean actual = Solution.doesMappingExist(message, code);
+            assertEquals(expected, actual);
+        });
+    }
+
+    @Test
+    public void TrivialCase2() {
+        String message = "red green blue";
+        String code = "abb";
+        assertTimeout(Duration.ofMillis(500), () -> {
+            boolean expected = false;
+            boolean actual = Solution.doesMappingExist(message, code);
+            assertEquals(expected, actual);
+        });
+    }
+
+    @Test
+    public void TrivialCase3() {
+        String message = "red blue blue";
+        String code = "abb";
+        assertTimeout(Duration.ofMillis(500), () -> {
+            boolean expected = true;
+            boolean actual = Solution.doesMappingExist(message, code);
+            assertEquals(expected, actual);
+        });
+    }
+
+    @Test
+    public void TrivialCase4() {
+        String message = "red blue red";
+        String code = "abaa";
+        assertTimeout(Duration.ofMillis(500), () -> {
+            boolean expected = false;
+            boolean actual = Solution.doesMappingExist(message, code);
+            assertEquals(expected, actual);
+        });
+    }
+
+    @Test
+    public void TrivialCase5() {
+        String message = "red blue red";
+        String code = "abaa";
+        assertTimeout(Duration.ofMillis(500), () -> {
+            boolean expected = false;
+            boolean actual = Solution.doesMappingExist(message, code);
+            assertEquals(expected, actual);
+        });
+    }
+}
---
+++ src/Miscellaneous/src/Problems/MappingBetweenMessageAndCode/readme.md
@@ -0,0 +1,18 @@
+# Problem Definition
+
+## Description
+
+Function takes 2 strings, message and code. Returns a boolean indicating whether can create a 1:1 mapping between words in message and letters in code (can encode/decode message with code).
+
+**Examples:**
+
+```text
+"red green blue", "abc" --> True
+"red green blue", "abb" --> False
+"red blue blue", "abb" --> True
+
+"red blue red", "ab" --> False
+"red blue red", "abaa" --> False
+```
+
+## Notes
