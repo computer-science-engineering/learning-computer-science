9e427ca1679809bb40409976f6be2a81c6e6d1bf
Manas Talukdar <manas.talukdar@gmail.com>
2018-12-13 16:44:22 -0800

    Remove Duplicates and Return List

---
+++ src/Miscellaneous/src/Problems/Programming/RemoveDuplicatesAndReturnList/Java/SolutionTest.java
@@ -0,0 +1,44 @@
+package Miscellaneous.src.Problems.Programming.RemoveDuplicatesAndReturnList.Java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class SolutionTest {
+    
+    Solution solution;
+    
+    @BeforeEach
+    public void setUp() throws Exception {
+        solution = new Solution();
+    }
+    
+    @AfterEach
+    public void tearDown() throws Exception {
+        solution = null;
+    }
+    
+    @Test
+    public void MainFunction() {
+        assertTimeout(Duration.ofMillis(500), () -> {
+             String[] args = new String[0];
+             assertAll(() -> Solution.main(args));
+        });
+    }
+    
+    @Test
+    public void TrivialCase1() {
+        ArrayList<Integer> input = new ArrayList<Integer>(Arrays.asList(1, 2, 1, 3, 2, 1, 2));
+        assertTimeout(Duration.ofMillis(500), () -> {
+            ArrayList<Integer> expected = new ArrayList<Integer>(Arrays.asList(1, 2, 3));;
+            ArrayList<Integer> actual = Solution.removeDuplicates(input);
+            assertEquals(expected, actual);
+        });
+    }
+}
---
+++ src/Miscellaneous/src/Problems/Programming/RemoveDuplicatesAndReturnList/readme.md
@@ -0,0 +1,14 @@
+# Problem Definition
+
+## Description
+
+Remove duplicates and return list. Input list is unsorted. Solve for optimal time complexity.
+
+```text
+Example:
+
+Input: [1, 2, 1, 3, 2, 1, 2]
+Output: [1, 2, 3]
+```
+
+## Notes
