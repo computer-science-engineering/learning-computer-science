4d1b42899897baf77c1c85ccc9899561002d5979
Manas Talukdar <manas.talukdar@gmail.com>
2018-12-16 13:06:41 -0800

    Plus One

---
+++ src/LeetCode/src/Explore/Interview/GoogleInterview/ArraysAndStrings/PlusOne/Java/SolutionTest.java
@@ -0,0 +1,61 @@
+package LeetCode.src.Explore.Interview.GoogleInterview.ArraysAndStrings.PlusOne.Java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class SolutionTest {
+    
+    Solution solution;
+    
+    @BeforeEach
+    public void setUp() throws Exception {
+        solution = new Solution();
+    }
+    
+    @AfterEach
+    public void tearDown() throws Exception {
+        solution = null;
+    }
+    
+    @Test
+    public void MainFunction() {
+        assertTimeout(Duration.ofMillis(500), () -> {
+             String[] args = new String[0];
+             assertAll(() -> Solution.main(args));
+        });
+    }
+    
+    @Test
+    public void TrivialCase1() {
+        int[] digits = {4,3,2,1};
+        assertTimeout(Duration.ofMillis(500), () -> {
+            int[] expected = {4,3,2,2};
+            int[] actual = Solution.plusOne(digits);
+            assertArrayEquals(expected, actual);
+        });
+    }
+
+    @Test
+    public void TrivialCase2() {
+        int[] digits = {4,3,2,9};
+        assertTimeout(Duration.ofMillis(500), () -> {
+            int[] expected = {4,3,3,0};
+            int[] actual = Solution.plusOne(digits);
+            assertArrayEquals(expected, actual);
+        });
+    }
+
+    @Test
+    public void TrivialCase3() {
+        int[] digits = {9,9,9,9};
+        assertTimeout(Duration.ofMillis(500), () -> {
+            int[] expected = {1,0,0,0,0};
+            int[] actual = Solution.plusOne(digits);
+            assertArrayEquals(expected, actual);
+        });
+    }
+}
---
+++ src/LeetCode/src/Explore/Interview/GoogleInterview/ArraysAndStrings/PlusOne/readme.md
@@ -0,0 +1,30 @@
+# Problem Definition
+
+## Description
+
+Given a **non-empty** array of digits representing a non-negative integer, plus one to the integer.
+
+The digits are stored such that the most significant digit is at the head of the list, and each element in the array contain a single digit.
+
+You may assume the integer does not contain any leading zero, except the number 0 itself.
+
+**Example 1:**
+
+```text
+Input: [1,2,3]
+Output: [1,2,4]
+Explanation: The array represents the integer 123.
+```
+
+**Example 2:**
+
+```text
+Input: [4,3,2,1]
+Output: [4,3,2,2]
+Explanation: The array represents the integer 4321.
+```
+
+## Notes
+
+1. [LeetCode discussion - My Simple Java Solution](https://leetcode.com/explore/interview/card/google/59/array-and-strings/339/discuss/24082/My-Simple-Java-Solution)
+1. [LeetCode discussion - O(n) An perfect solution in Java](https://leetcode.com/explore/interview/card/google/59/array-and-strings/339/discuss/142787/O(n)-An-perfect-solution-in-Java)
