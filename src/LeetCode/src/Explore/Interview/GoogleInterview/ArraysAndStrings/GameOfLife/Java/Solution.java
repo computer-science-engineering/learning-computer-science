0a9220a6c1f22142d57158aebc189b45f2e6883e
Manas Talukdar <manas.talukdar@gmail.com>
2018-12-17 21:09:45 -0800

    Game of Life

--- src/LeetCode/src/Explore/Interview/GoogleInterview/ArraysAndStrings/GameOfLife/Java/Solution.java
+++ src/LeetCode/src/Explore/Interview/GoogleInterview/ArraysAndStrings/GameOfLife/Java/Solution.java
@@ -2,10 +2,58 @@ package LeetCode.src.Explore.Interview.GoogleInterview.ArraysAndStrings.GameOfLi
 
 public class Solution {
     public static void main(String[] args) {
-        
+        int[][] board = {
+            {0,1,0},
+            {0,0,1},
+            {1,1,1},
+            {0,0,0}
+        };
+
+        gameOfLife(board);
+
+        int m = board.length, n = board[0].length;
+        for (int i = 0; i < m; i++) {   
+            for (int j = 0; j < n; j++) {  
+                System.out.print(board[i][j] + " ");
+            }           
+            System.out.println();
+        }
     }
 
     public static void gameOfLife(int[][] board) {
-        
+        if (board == null || board.length == 0) return;
+        int m = board.length, n = board[0].length;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int lives = liveNeighbors(board, m, n, i, j);
+
+                // In the beginning, every 2nd bit is 0;
+                // So we only need to care about when will the 2nd bit become 1.
+                if (board[i][j] == 1 && lives >= 2 && lives <= 3) {  
+                    board[i][j] = 3; // Make the 2nd bit 1: 01 ---> 11
+                }
+                if (board[i][j] == 0 && lives == 3) {
+                    board[i][j] = 2; // Make the 2nd bit 1: 00 ---> 10
+                }
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] >>= 1;  // Get the 2nd state. // To get the next state
+            }
+        }
+    }
+
+    private static int liveNeighbors(int[][] board, int m, int n, int i, int j) {
+        int lives = 0;
+        for (int x = Math.max(i - 1, 0); x <= Math.min(i + 1, m - 1); x++) {
+            for (int y = Math.max(j - 1, 0); y <= Math.min(j + 1, n - 1); y++) {
+                lives += board[x][y] & 1; // To get the current state
+            }
+        }
+        lives -= board[i][j] & 1;
+        return lives;
     }
 }
