9ee8e02771785e490fe471f01a2847090ce2a34a
Manas Talukdar <manas.talukdar@gmail.com>
2018-12-24 17:38:51 -0800

    update

--- src/LeetCode/src/Explore/Interview/GoogleInterview/LinkedList/InsertIntoACyclicSortedList/Java/SolutionTest.java
+++ src/LeetCode/src/Explore/Interview/GoogleInterview/LinkedList/InsertIntoACyclicSortedList/Java/SolutionTest.java
@@ -3,6 +3,9 @@ package LeetCode.src.Explore.Interview.GoogleInterview.LinkedList.InsertIntoACyc
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -31,11 +34,30 @@ public class SolutionTest {
     
     @Test
     public void TrivialCase1() {
-        // input = ;
+        Node a1 = new Node(3);
+        Node a2 = new Node(4);
+        Node a3 = new Node(1);
+        a1.next = a2;
+        a2.next = a3;
+        a3.next = a1;
         assertTimeout(Duration.ofMillis(500), () -> {
-            // expected = ;
-            // actual = Solution.;
-            // assertEquals(expected, actual);
+            ArrayList<Integer> expected = new ArrayList<Integer>(Arrays.asList(3,4,1,2));
+            Node result = Solution.insert(a1, 2);
+            ArrayList<Integer> actual = getList(result);
+            assertEquals(expected, actual);
         });
     }
+
+    private static ArrayList<Integer> getList(Node head) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        Node temp = head; 
+        if (head != null) { 
+            do { 
+                result.add(temp.val);
+                temp = temp.next; 
+            } 
+            while (temp != head); 
+        } 
+        return result;
+    } 
 }
