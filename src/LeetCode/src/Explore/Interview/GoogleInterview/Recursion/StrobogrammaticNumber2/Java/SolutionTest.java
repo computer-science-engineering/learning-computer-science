14efe5b9ed95a499db1c47e6048e3c5b9d370e5f
Manas Talukdar <manas.talukdar@gmail.com>
2018-12-29 21:15:28 -0800

    Strobogrammatic Number II

---
+++ src/LeetCode/src/Explore/Interview/GoogleInterview/Recursion/StrobogrammaticNumber2/Java/SolutionTest.java
@@ -0,0 +1,44 @@
+package LeetCode.src.Explore.Interview.GoogleInterview.Recursion.StrobogrammaticNumber2.Java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class SolutionTest {
+    
+    Solution solution;
+    
+    @BeforeEach
+    public void setUp() throws Exception {
+        solution = new Solution();
+    }
+    
+    @AfterEach
+    public void tearDown() throws Exception {
+        solution = null;
+    }
+    
+    @Test
+    public void MainFunction() {
+        assertTimeout(Duration.ofMillis(500), () -> {
+             String[] args = new String[0];
+             assertAll(() -> Solution.main(args));
+        });
+    }
+    
+    @Test
+    public void TrivialCase1() {
+        int n = 2;
+        assertTimeout(Duration.ofMillis(500), () -> {
+            List<String> expected = Arrays.asList("11", "69", "88", "96");;
+            List<String> actual = Solution.findStrobogrammatic(n);
+            assertEquals(expected, actual);
+        });
+    }
+}
---
+++ src/LeetCode/src/Explore/Interview/GoogleInterview/Recursion/StrobogrammaticNumber2/readme.md
@@ -0,0 +1,18 @@
+# Problem Definition
+
+## Description
+
+A strobogrammatic number is a number that looks the same when rotated 180 degrees (looked at upside down).
+
+Find all strobogrammatic numbers that are of length = n.
+
+**Example:**
+
+```text
+Input:  n = 2
+Output: ["11","69","88","96"]
+```
+
+## Notes
+
+1. [LeetCode discussion - AC clean Java solution](https://leetcode.com/explore/interview/card/google/62/recursion-4/399/discuss/67280/AC-clean-Java-solution)
