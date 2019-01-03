0bbe9e6fa0196c71ebaff3ac803056b8066ba16b
Manas Talukdar <manas.talukdar@gmail.com>
2019-01-03 02:19:10 -0800

    Update.

---
+++ src/LeetCode/src/Explore/Interview/GoogleInterview/SortingAndSearching/FindKPairsWithSmallestSums/Java/Solution.java
@@ -0,0 +1,14 @@
+package LeetCode.src.Explore.Interview.GoogleInterview.SortingAndSearching.FindKPairsWithSmallestSums.Java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) {
+        
+    }
+
+    public static List<int[]> kSmallestPairs(int[] nums1, int[] nums2, int k) {
+        return new ArrayList<int[]>();
+    }
+}
