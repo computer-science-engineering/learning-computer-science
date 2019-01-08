c6fe092b544886e43b15e590e85158edb59244fc
Manas Talukdar <manas.talukdar@gmail.com>
2019-01-07 19:57:53 -0800

    Pacific Atlantic Water Flow

--- src/LeetCode/src/Explore/Interview/GoogleInterview/SortingAndSearching/PacificAtlanticWaterFlow/Java/SolutionTest.java
+++ src/LeetCode/src/Explore/Interview/GoogleInterview/SortingAndSearching/PacificAtlanticWaterFlow/Java/SolutionTest.java
@@ -3,6 +3,10 @@ package LeetCode.src.Explore.Interview.GoogleInterview.SortingAndSearching.Pacif
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -31,11 +35,33 @@ public class SolutionTest {
     
     @Test
     public void TrivialCase1() {
-        // input = ;
+        int[][] matrix = {
+            {1, 2, 2, 3, 5},
+            {3, 2, 3, 4, 4},
+            {2, 4, 5, 3, 1},
+            {6, 7, 1, 4, 5},
+            {5, 1, 1, 2, 4}
+        };
         assertTimeout(Duration.ofMillis(500), () -> {
-            // expected = ;
-            // actual = Solution.;
-            // assertEquals(expected, actual);
+            List<List<Integer>> expected = Arrays.asList(
+                Arrays.asList(0,4),
+                Arrays.asList(1,3),
+                Arrays.asList(1,4),
+                Arrays.asList(2,2),
+                Arrays.asList(3,0),
+                Arrays.asList(3,1),
+                Arrays.asList(4,0)
+            );
+            List<int[]> result = Solution.pacificAtlantic(matrix);
+            List<List<Integer>> actual = new ArrayList<>();
+            for (int[] intArray : result) {
+                List<Integer> nestedList = new ArrayList<>();
+                for (int item : intArray) {
+                    nestedList.add(item);
+                }
+                actual.add(nestedList);
+            }
+            assertEquals(expected, actual);
         });
     }
 }
