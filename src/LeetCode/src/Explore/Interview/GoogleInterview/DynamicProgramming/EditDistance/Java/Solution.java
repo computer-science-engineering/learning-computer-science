1e9f46ec8950196d42d8534d75298cbb95c60554
Manas Talukdar <manas.talukdar@gmail.com>
2019-01-13 23:45:15 -0800

    Edit Distance

---
+++ src/LeetCode/src/Explore/Interview/GoogleInterview/DynamicProgramming/EditDistance/Java/Solution.java
@@ -0,0 +1,46 @@
+package LeetCode.src.Explore.Interview.GoogleInterview.DynamicProgramming.EditDistance.Java;
+
+public class Solution {
+    public static void main(String[] args) {
+        String A = "Anshuman";
+        String B = "Antihuman";
+        System.out.println(minDistance(A, B));
+    }
+    
+    public static int minDistance(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+
+        // if one of the strings is empty
+        if (n * m == 0) {
+            return n + m;
+        }
+        
+        // array to store the conversion history
+        int[][] cost = new int[m + 1][n + 1];
+
+        // init boundaries
+        for(int i = 0; i <= m; i++) {
+            cost[i][0] = i;
+        }
+        for(int i = 1; i <= n; i++) {
+            cost[0][i] = i;
+        }
+        
+        // DP compute 
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(word1.charAt(i) == word2.charAt(j)) {
+                    cost[i + 1][j + 1] = cost[i][j];
+                }
+                else {
+                    int replace = cost[i][j];
+                    int delete = cost[i][j + 1];
+                    int insert = cost[i + 1][j];
+                    cost[i + 1][j + 1] = Math.min(replace, Math.min(delete, insert)) + 1;
+                }
+            }
+        }
+        return cost[m][n];
+    }
+}
---
+++ src/LeetCode/src/Explore/Interview/GoogleInterview/DynamicProgramming/EditDistance/Java/SolutionTest.java
@@ -0,0 +1,64 @@
+package LeetCode.src.Explore.Interview.GoogleInterview.DynamicProgramming.EditDistance.Java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class SolutionTest {
+    
+    Solution solution;
+    
+    @BeforeEach
+    public void setUp() throws Exception {
+        solution = new Solution();
+    }
+    
+    @AfterEach
+    public void tearDown() throws Exception {
+        solution = null;
+    }
+    
+    @Test
+    public void MainFunction() {
+        assertTimeout(Duration.ofMillis(500), () -> {
+             String[] args = new String[0];
+             assertAll(() -> Solution.main(args));
+        });
+    }
+    
+    @Test
+    public void TrivialCase1() {
+        String A = "Anshuman";
+        String B = "Antihuman";
+        assertTimeout(Duration.ofMillis(500), () -> {
+            int expected = 2;
+            int actual = Solution.minDistance(A, B);
+            assertEquals(expected, actual);
+        });
+    }
+
+    @Test
+    public void TrivialCase2() {
+        String A = "horse";
+        String B = "ros";
+        assertTimeout(Duration.ofMillis(500), () -> {
+            int expected = 3;
+            int actual = Solution.minDistance(A, B);
+            assertEquals(expected, actual);
+        });
+    }
+
+    @Test
+    public void TrivialCase3() {
+        String A = "intention";
+        String B = "execution";
+        assertTimeout(Duration.ofMillis(500), () -> {
+            int expected = 5;
+            int actual = Solution.minDistance(A, B);
+            assertEquals(expected, actual);
+        });
+    }
+}
