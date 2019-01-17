6f14ab45794a6b917865f94908d5b34e4fb8e19e
Manas Talukdar <manas.talukdar@gmail.com>
2019-01-17 01:29:26 -0800

    UTF-8 Validation [WIP]

---
+++ src/LeetCode/src/Explore/Interview/GoogleInterview/Others/UTF8Validation/Java/SolutionTest.java
@@ -0,0 +1,41 @@
+package LeetCode.src.Explore.Interview.GoogleInterview.Others.UTF8Validation.Java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class SolutionTest {
+    
+    Solution solution;
+    
+    @BeforeEach
+    public void setUp() throws Exception {
+        solution = new Solution();
+    }
+    
+    @AfterEach
+    public void tearDown() throws Exception {
+        solution = null;
+    }
+    
+    @Test
+    public void MainFunction() {
+        assertTimeout(Duration.ofMillis(500), () -> {
+             String[] args = new String[0];
+             assertAll(() -> Solution.main(args));
+        });
+    }
+    
+    @Test
+    public void TrivialCase1() {
+        // input = ;
+        assertTimeout(Duration.ofMillis(500), () -> {
+            // expected = ;
+            // actual = Solution.;
+            // assertEquals(expected, actual);
+        });
+    }
+}
