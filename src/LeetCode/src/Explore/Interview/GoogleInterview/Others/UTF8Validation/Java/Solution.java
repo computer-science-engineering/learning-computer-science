646731ba60a426a9aba14d7586cce51a52d9da3b
Manas Talukdar <manas.talukdar@gmail.com>
2019-01-17 01:29:26 -0800

    UTF-8 Validation [WIP]

---
+++ src/LeetCode/src/Explore/Interview/GoogleInterview/Others/UTF8Validation/Java/Solution.java
@@ -0,0 +1,11 @@
+package LeetCode.src.Explore.Interview.GoogleInterview.Others.UTF8Validation.Java;
+
+public class Solution {
+    public static void main(String[] args) {
+        
+    }
+
+    public static boolean validUtf8(int[] data) {
+        return true;
+    }
+}
