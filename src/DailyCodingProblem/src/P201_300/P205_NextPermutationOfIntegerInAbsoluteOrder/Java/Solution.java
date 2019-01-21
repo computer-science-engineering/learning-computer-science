1e7510c9e0cd324351885a879207580ce9bacbc9
Manas Talukdar <manas.talukdar@gmail.com>
2019-01-21 01:19:27 -0800

    205. Next Permutation Of Integer In Absolute Order [DCP] [WIP]

---
+++ src/DailyCodingProblem/src/P201_300/P205_NextPermutationOfIntegerInAbsoluteOrder/Java/Solution.java
@@ -0,0 +1,11 @@
+package DailyCodingProblem.src.P201_300.P205_NextPermutationOfIntegerInAbsoluteOrder.Java;
+
+public class Solution {
+    public static void main(String[] args) {
+        
+    }
+
+    public static Integer nextPermutation(int number) {
+        return 0;
+    }
+}
