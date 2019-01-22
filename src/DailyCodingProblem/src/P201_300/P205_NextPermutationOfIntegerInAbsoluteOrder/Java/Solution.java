c29962a968660087e6fd524fc1d091a8c0574203
Manas Talukdar <manas.talukdar@gmail.com>
2019-01-21 17:12:40 -0800

    Updates.

--- src/DailyCodingProblem/src/P201_300/P205_NextPermutationOfIntegerInAbsoluteOrder/Java/Solution.java
+++ src/DailyCodingProblem/src/P201_300/P205_NextPermutationOfIntegerInAbsoluteOrder/Java/Solution.java
@@ -2,10 +2,11 @@ package DailyCodingProblem.src.P201_300.P205_NextPermutationOfIntegerInAbsoluteO
 
 public class Solution {
     public static void main(String[] args) {
-        
+        System.out.println(nextPermutation(10));
     }
 
     public static Integer nextPermutation(int number) {
-        return 0;
+        var result = 0;
+        return result;
     }
 }
