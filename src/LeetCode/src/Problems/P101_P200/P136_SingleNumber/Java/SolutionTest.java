bfae414c701189bd85e14c0cfc230e95adc89a85
Manas Talukdar <manas.talukdar@gmail.com>
2019-01-22 23:33:22 -0800

    136. Single Number

---
+++ src/LeetCode/src/Problems/P101_P200/P136_SingleNumber/Java/SolutionTest.java
@@ -0,0 +1,41 @@
+package LeetCode.src.Problems.P101_P200.P136_SingleNumber.Java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class SolutionTest {
+    
+    Solution solution;
+    
+    @BeforeEach
+    public void setUp() throws Exception {
+        solution = new Solution();
+    }
+    
+    @AfterEach
+    public void tearDown() throws Exception {
+        solution = null;
+    }
+    
+    @Test
+    public void MainFunction() {
+        assertTimeout(Duration.ofMillis(500), () -> {
+             String[] args = new String[0];
+             assertAll(() -> Solution.main(args));
+        });
+    }
+    
+    @Test
+    public void TrivialCase1() {
+        int[] nums = {4,1,2,1,2};
+        assertTimeout(Duration.ofMillis(500), () -> {
+            int expected = 4;
+            int actual = Solution.singleNumber(nums);
+            assertEquals(expected, actual);
+        });
+    }
+}
