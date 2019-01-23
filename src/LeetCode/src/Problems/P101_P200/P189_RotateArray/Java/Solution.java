f76f3f492769659d3daff5d7caadb73fb58e986a
Manas Talukdar <manas.talukdar@gmail.com>
2019-01-22 22:52:00 -0800

    189. Rotate Array

---
+++ src/LeetCode/src/Problems/P101_P200/P189_RotateArray/Java/Solution.java
@@ -0,0 +1,29 @@
+package LeetCode.src.Problems.P101_P200.P189_RotateArray.Java;
+
+public class Solution {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5,6,7};
+        int k = 3;
+        rotate(nums, k);
+        for (int n : nums) {
+            System.out.print(n + " ");
+        }
+    }
+
+    public static void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    private static void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
