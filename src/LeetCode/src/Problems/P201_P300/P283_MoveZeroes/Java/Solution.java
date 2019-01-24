dce1c30f97eaf6b1150c88176d01abf3e91c8409
Manas Talukdar <manas.talukdar@gmail.com>
2019-01-24 00:38:09 -0800

    Move Zeroes

--- src/LeetCode/src/Problems/P201_P300/P283_MoveZeroes/Java/Solution.java
+++ src/LeetCode/src/Problems/P201_P300/P283_MoveZeroes/Java/Solution.java
@@ -2,8 +2,8 @@ package LeetCode.src.Problems.P201_P300.P283_MoveZeroes.Java;
 
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = {0,1,0,3,12};
-        moveZeroes(nums);
+        int[] nums = {1,1,0,3,12};
+        moveZeroes2(nums);
         for (int n : nums) {
             System.out.print(n + " ");
         }
@@ -24,5 +24,20 @@ public class Solution {
                 nums[j] = 0;
             }
         }
+    }
+
+    public static void moveZeroes2(int[] nums) {
+        int lastNonZeroFoundAt = 0;
+        for (int cur = 0; cur < nums.length; cur++) {
+            if (nums[cur] != 0) {
+                swap(nums, lastNonZeroFoundAt++, cur);
+            }
+        }
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
