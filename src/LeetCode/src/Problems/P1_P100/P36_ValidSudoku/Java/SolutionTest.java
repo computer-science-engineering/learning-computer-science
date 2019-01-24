f4b5361fb566ce3d03ffe1536c995fe15c59f24c
Manas Talukdar <manas.talukdar@gmail.com>
2019-01-24 15:15:03 -0800

    Valid Sudoku

---
+++ src/LeetCode/src/Problems/P1_P100/P36_ValidSudoku/Java/SolutionTest.java
@@ -0,0 +1,77 @@
+package LeetCode.src.Problems.P1_P100.P36_ValidSudoku.Java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class SolutionTest {
+    
+    Solution solution;
+    
+    @BeforeEach
+    public void setUp() throws Exception {
+        solution = new Solution();
+    }
+    
+    @AfterEach
+    public void tearDown() throws Exception {
+        solution = null;
+    }
+    
+    @Test
+    public void MainFunction() {
+        assertTimeout(Duration.ofMillis(500), () -> {
+             String[] args = new String[0];
+             assertAll(() -> Solution.main(args));
+        });
+    }
+    
+    @Test
+    public void TrivialCase1() {
+        char[][] board = 
+        {
+            {'5','3','.','.','7','.','.','.','.'},
+            {'6','.','.','1','9','5','.','.','.'},
+            {'.','9','8','.','.','.','.','6','.'},
+            {'8','.','.','.','6','.','.','.','3'},
+            {'4','.','.','8','.','3','.','.','1'},
+            {'7','.','.','.','2','.','.','.','6'},
+            {'.','6','.','.','.','.','2','8','.'},
+            {'.','.','.','4','1','9','.','.','5'},
+            {'.','.','.','.','8','.','.','7','9'}
+        };
+        assertTimeout(Duration.ofMillis(500), () -> {
+            boolean expected = true;
+            boolean actual1 = Solution.isValidSudoku(board);
+            boolean actual2 = Solution.isValidSudoku_threeHashSets(board);
+            assertEquals(expected, actual1);
+            assertEquals(expected, actual2);
+        });
+    }
+
+    @Test
+    public void TrivialCase2() {
+        char[][] board = 
+        {
+            {'8','3','.','.','7','.','.','.','.'},
+            {'6','.','.','1','9','5','.','.','.'},
+            {'.','9','8','.','.','.','.','6','.'},
+            {'8','.','.','.','6','.','.','.','3'},
+            {'4','.','.','8','.','3','.','.','1'},
+            {'7','.','.','.','2','.','.','.','6'},
+            {'.','6','.','.','.','.','2','8','.'},
+            {'.','.','.','4','1','9','.','.','5'},
+            {'.','.','.','.','8','.','.','7','9'}
+        };
+        assertTimeout(Duration.ofMillis(500), () -> {
+            boolean expected = false;
+            boolean actual1 = Solution.isValidSudoku(board);
+            boolean actual2 = Solution.isValidSudoku_threeHashSets(board);
+            assertEquals(expected, actual1);
+            assertEquals(expected, actual2);
+        });
+    }
+}
