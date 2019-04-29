0562eae8eeec8b6cc11c4ad8fc5a8dce4d76c7fe
Manas Talukdar <manas.talukdar@gmail.com>
2019-04-28 17:49:03 -0700

    Median Of Two Sorted Arrays - LeetCode

--- src/ByteByByte/src/CodingInterviewQuestions/Array/P1_MedianOfArrays/Java/Solution.java
+++ src/ByteByByte/src/CodingInterviewQuestions/Array/P1_MedianOfArrays/Java/Solution.java
@@ -7,40 +7,30 @@ public class Solution {
         System.out.println(findMedianSortedArrays(input1, input2));
     }
 
-    public static double findMedianSortedArrays(int input1[], int input2[]) {
-        if (input1.length > input2.length) {
-            findMedianSortedArrays(input2, input1);
-        }
-
-        int x = input1.length;
-        int y = input2.length;
-
-        int low = 0;
-        int high = x;
-        while(low <= high) {
-            int partition_X = (low + high)/2;
-            int partition_Y = (x + y + 1)/2 -  partition_X;
-
-            int maxLeft_X = (partition_X == 0) ? Integer.MIN_VALUE : input1[partition_X - 1];
-            int minRight_X = (partition_X == x) ? Integer.MAX_VALUE : input1[partition_X];
-
-            int maxLeft_Y = (partition_Y == 0) ? Integer.MIN_VALUE : input2[partition_Y - 1];
-            int minRight_Y = (partition_Y == y) ? Integer.MAX_VALUE : input2[partition_Y];
-
-            if (maxLeft_X <= minRight_Y && maxLeft_Y <= minRight_X) {
-                if ((x + y) % 2 == 0) {
-                    return (double)(Math.max(maxLeft_X, maxLeft_Y) + Math.min(minRight_X, minRight_Y))/2;
-                } else {
-                    return (double)Math.max(maxLeft_X, maxLeft_Y);
-                }
-            } else if (maxLeft_X > minRight_Y) { // we are too far on right side for partition_X. Go on left side.
-                high = partition_X - 1;
-            } else { // we are too far on left side for partition_X. Go on right side.
-                low = partition_X + 1;
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int len1 = nums1.length;
+        int len2 = nums2.length;
+        if (len1 > len2) return findMedianSortedArrays(nums2, nums1);
+        int k = (len1 + len2 + 1) / 2;
+        int l = 0;
+        int r = len1;
+        //find the m1 so that nums1[m1] >= nums2[m2 - 1]
+        while(l < r){
+            int m1 = l + (r - l) / 2;
+            int m2 = k - m1;
+            if (nums1[m1] < nums2[m2 - 1]){
+                l = m1 + 1;
+            } else {
+                r = m1;
             }
-        }        
-
-        // Input arrays were not sorted.
-        throw new IllegalArgumentException();
+        }
+        int m1 = l;
+        int m2 = k - l;
+        int c1 = Math.max(m1 <= 0 ? Integer.MIN_VALUE : nums1[m1 - 1],
+                         m2 <= 0? Integer.MIN_VALUE: nums2[m2 - 1]);
+        if ((len1 + len2) % 2 ==1) return c1;
+        int c2 = Math.min(m1 >= len1 ? Integer.MAX_VALUE : nums1[m1],
+                         m2 >= len2 ? Integer.MAX_VALUE: nums2[m2]);
+        return (c1 + c2) / 2.0;
     }
 }
