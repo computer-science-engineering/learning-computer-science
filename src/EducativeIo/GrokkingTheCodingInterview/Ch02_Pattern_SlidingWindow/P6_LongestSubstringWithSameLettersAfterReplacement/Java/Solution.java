3c1f638540f5c7e5c607010efeda2150ec45305b
Manas Talukdar <manas.talukdar@gmail.com>
2019-07-12 17:01:10 -0700

    Longest Substring w/ Same Letters Post Replacement

---
+++ src/EducativeIo/GrokkingTheCodingInterview/Ch02_Pattern_SlidingWindow/P6_LongestSubstringWithSameLettersAfterReplacement/Java/Solution.java
@@ -0,0 +1,37 @@
+package EducativeIo.GrokkingTheCodingInterview.Ch02_Pattern_SlidingWindow.P6_LongestSubstringWithSameLettersAfterReplacement.Java;
+
+import java.util.HashMap;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(findLength("aabccbb", 2));
+        System.out.println(findLength("abbcb", 1));
+        System.out.println(findLength("abccde", 1));  
+    }
+
+    public static int findLength(String str, int k) {
+        int maxLength = Integer.MIN_VALUE;
+        int windowStart = 0;
+        int maxRepeatLetterCount = 0;
+        HashMap<Character, Integer> letterFrequencyMap = new HashMap<>();
+        for (int windowEnd = 0; windowEnd < str.length(); windowEnd++) {
+            char rightChar = str.charAt(windowEnd);
+            letterFrequencyMap.put(rightChar, letterFrequencyMap.getOrDefault(rightChar, 0) + 1);
+            maxRepeatLetterCount = Math.max(maxRepeatLetterCount, letterFrequencyMap.get(rightChar));
+
+            // current window size is from windowStart to windowEnd, overall we have a letter which is
+            // repeating 'maxRepeatLetterCount' times, this means we can have a window which has one letter 
+            // repeating 'maxRepeatLetterCount' times and the remaining letters we should replace.
+            // if the remaining letters are more than 'k', it is the time to shrink the window as we
+            // are not allowed to replace more than 'k' letters
+            if (windowEnd - windowStart + 1 - maxRepeatLetterCount > k) {
+                char leftChar = str.charAt(windowStart);
+                letterFrequencyMap.put(leftChar, letterFrequencyMap.get(leftChar) - 1);
+                windowStart++;
+            }
+
+            maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
+        }
+        return maxLength;
+    }
+}
---
+++ src/EducativeIo/GrokkingTheCodingInterview/Ch02_Pattern_SlidingWindow/P6_LongestSubstringWithSameLettersAfterReplacement/Java/SolutionTest.java
@@ -0,0 +1,64 @@
+package EducativeIo.GrokkingTheCodingInterview.Ch02_Pattern_SlidingWindow.P6_LongestSubstringWithSameLettersAfterReplacement.Java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class SolutionTest {
+    
+    Solution solution;
+    
+    @BeforeEach
+    public void setUp() throws Exception {
+        solution = new Solution();
+    }
+    
+    @AfterEach
+    public void tearDown() throws Exception {
+        solution = null;
+    }
+    
+    @Test
+    public void MainFunction() {
+        assertTimeout(Duration.ofMillis(500), () -> {
+             String[] args = new String[0];
+             assertAll(() -> Solution.main(args));
+        });
+    }
+    
+    @Test
+    public void TrivialCase1() {
+        String str = "aabccbb";
+        int k = 2;
+        assertTimeout(Duration.ofMillis(500), () -> {
+            int expected = 5;
+            int actual = Solution.findLength(str, k);
+            assertEquals(expected, actual);
+        });
+    }
+
+    @Test
+    public void TrivialCase2() {
+        String str = "abbcb";
+        int k = 1;
+        assertTimeout(Duration.ofMillis(500), () -> {
+            int expected = 4;
+            int actual = Solution.findLength(str, k);
+            assertEquals(expected, actual);
+        });
+    }
+
+    @Test
+    public void TrivialCase3() {
+        String str = "abccde";
+        int k = 1;
+        assertTimeout(Duration.ofMillis(500), () -> {
+            int expected = 3;
+            int actual = Solution.findLength(str, k);
+            assertEquals(expected, actual);
+        });
+    }
+}
