c65cd9934431a4d327a6ad4c55112c43633b3394
Manas Talukdar <manas.talukdar@gmail.com>
2019-07-24 14:42:09 -0700

    Plumbing for nextjs and material-ui

--- website-react-next/pages/about.js
+++ website-react-next/pages/about.js
@@ -18,18 +18,14 @@ function MadeWithLove() {
   );
 }
 
-export default Index;
-
-function Index() {
+export default function About() {
   return (
     <Container maxWidth="sm">
       <Box my={4}>
         <Typography variant="h4" component="h1" gutterBottom>
           Next.js example
         </Typography>
-        <Link href="/about" color="secondary">
-          Go to the about page
-        </Link>
+        <Link href="/">Go to the main page</Link>
         <ProTip />
         <MadeWithLove />
       </Box>
