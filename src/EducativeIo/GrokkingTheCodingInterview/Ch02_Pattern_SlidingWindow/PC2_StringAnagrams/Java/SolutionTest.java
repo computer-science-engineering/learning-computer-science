360180af019bb88faef38bd5b4764a801222c866
Manas Talukdar <manas.talukdar@gmail.com>
2019-07-25 16:03:06 -0700

    String Anagrams

---
+++ src/EducativeIo/GrokkingTheCodingInterview/Ch02_Pattern_SlidingWindow/PC2_StringAnagrams/Java/SolutionTest.java
@@ -0,0 +1,56 @@
+package EducativeIo.GrokkingTheCodingInterview.Ch02_Pattern_SlidingWindow.PC2_StringAnagrams.Java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class SolutionTest {
+    
+    Solution solution;
+    
+    @BeforeEach
+    public void setUp() throws Exception {
+        solution = new Solution();
+    }
+    
+    @AfterEach
+    public void tearDown() throws Exception {
+        solution = null;
+    }
+    
+    @Test
+    public void MainFunction() {
+        assertTimeout(Duration.ofMillis(500), () -> {
+             String[] args = new String[0];
+             assertAll(() -> Solution.main(args));
+        });
+    }
+    
+    @Test
+    public void TrivialCase1() {
+        String str = "ppqp";
+        String pattern = "pq";
+        assertTimeout(Duration.ofMillis(500), () -> {
+            List<Integer> expected = Arrays.asList(1, 2);
+            List<Integer> actual = Solution.findStringAnagrams(str, pattern);
+            assertEquals(expected, actual);
+        });
+    }
+
+    @Test
+    public void TrivialCase2() {
+        String str = "abbcabc";
+        String pattern = "abc";
+        assertTimeout(Duration.ofMillis(500), () -> {
+            List<Integer> expected = Arrays.asList(2, 3, 4);
+            List<Integer> actual = Solution.findStringAnagrams(str, pattern);
+            assertEquals(expected, actual);
+        });
+    }
+}
