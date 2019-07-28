67bc174739191980e1163aac1675bf431f1415fc
Manas Talukdar <manas.talukdar@gmail.com>
2019-07-28 14:11:23 -0700

    Words Concatenation

--- src/EducativeIo/GrokkingTheCodingInterview/Ch02_Pattern_SlidingWindow/PC4_WordsConcatenation/Java/Solution.java
+++ src/EducativeIo/GrokkingTheCodingInterview/Ch02_Pattern_SlidingWindow/PC4_WordsConcatenation/Java/Solution.java
@@ -1,7 +1,9 @@
 package EducativeIo.GrokkingTheCodingInterview.Ch02_Pattern_SlidingWindow.PC4_WordsConcatenation.Java;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Solution {
     public static void main(String[] args) {
@@ -14,7 +16,33 @@ public class Solution {
 
     public static List<Integer> findWordConcatenation(String str, String[] words) {
         List<Integer> resultIndices = new ArrayList<Integer>();
-        // TODO: Write your code here
+        Map<String, Integer> wordFrequencyMap = new HashMap<>();
+        for (String word : words) {
+            wordFrequencyMap.put(word, wordFrequencyMap.getOrDefault(word, 0) + 1);
+        }
+
+        int wordsCount = words.length, wordLength = words[0].length();
+        for (int i = 0; i <= str.length() - wordsCount * wordLength; i++) {
+            Map<String, Integer> wordsSeen = new HashMap<>();
+            for (int j = 0; j < wordsCount; j++) {
+                int nextWordIndex = i + j * wordLength;
+                // get the next word from the string
+                String word = str.substring(nextWordIndex, nextWordIndex + wordLength);
+                if (!wordFrequencyMap.containsKey(word)) { // break if we don't need this word
+                    break;
+                }
+                // add the word to the 'wordsSeen' map
+                wordsSeen.put(word, wordsSeen.getOrDefault(word, 0) + 1);
+                // no need to process further if the word has higher frequency than required
+                if (wordsSeen.get(word) > wordFrequencyMap.getOrDefault(word, 0)) {
+                    break;
+                }
+                if (j + 1 == wordsCount) { // store index if we have found all the words
+                    resultIndices.add(i);
+                }
+            }
+        }
+
         return resultIndices;
     }
 }
