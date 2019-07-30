e98c53168a4d3c7b09f704ee3fa23b63e4136fbd
Manas Talukdar <manas.talukdar@gmail.com>
2019-07-29 17:25:38 -0700

    Triplet Sum to Zero - WIP

---
+++ src/EducativeIo/GrokkingTheCodingInterview/Ch03_TwoPointers/P4_TripletSumToZero/Java/Solution.java
@@ -0,0 +1,16 @@
+package EducativeIo.GrokkingTheCodingInterview.Ch03_TwoPointers.P4_TripletSumToZero.Java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(searchTriplets(new int[] {-3, 0, 1, 2, -1, 1, -2}));
+        System.out.println(searchTriplets(new int[] {-5, 2, -1, -2, 3}));
+    }
+
+    public static List<List<Integer>> searchTriplets(int[] arr) {
+        List<List<Integer>> triplets = new ArrayList<>();
+        return triplets;
+    }
+}
