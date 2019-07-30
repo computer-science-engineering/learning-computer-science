76d2f4cb12afa95c971e9b87fa3b60f2c0eb4aa4
Manas Talukdar <manas.talukdar@gmail.com>
2019-07-30 15:33:49 -0700

    formatting

--- src/EducativeIo/GrokkingTheCodingInterview/Ch03_TwoPointers/P5_TripleSumCloseToTarget/Java/Solution.java
+++ src/EducativeIo/GrokkingTheCodingInterview/Ch03_TwoPointers/P5_TripleSumCloseToTarget/Java/Solution.java
@@ -15,16 +15,19 @@ public class Solution {
                 // comparing the sum of three numbers to the 'targetSum' can cause overflow
                 // so, we will try to find a target difference
                 int targetDiff = targetSum - arr[i] - arr[left] - arr[right];
-                if (targetDiff == 0) // we've found a triplet with an exact sum
+                if (targetDiff == 0) { // we've found a triplet with an exact sum
                     return targetSum - targetDiff; // return sum of all the numbers
+                }
 
-                if (Math.abs(targetDiff) < Math.abs(smallestDifference))
+                if (Math.abs(targetDiff) < Math.abs(smallestDifference)) {
                     smallestDifference = targetDiff; // save the closest difference
+                }
 
-                if (targetDiff > 0)
+                if (targetDiff > 0) {
                     left++; // we need a triplet with a bigger sum
-                else
+                } else {
                     right--; // we need a triplet with a smaller sum
+                }
             }
         }
         return targetSum - smallestDifference;
