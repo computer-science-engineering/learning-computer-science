a27865f32c97872a6856191e39d473ea37a23526
Manas Talukdar <manas.talukdar@gmail.com>
2019-08-07 16:20:49 -0700

    Start of LinkedList Cycle

---
+++ src/EducativeIo/GrokkingTheCodingInterview/Ch04_FastAndSlowPointers/P2_StartOfLinkedListCycle/Java/Solution.java
@@ -0,0 +1,62 @@
+package EducativeIo.GrokkingTheCodingInterview.Ch04_FastAndSlowPointers.P2_StartOfLinkedListCycle.Java;
+
+public class Solution {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next = new ListNode(6);
+
+        head.next.next.next.next.next.next = head.next.next;
+        System.out.println("LinkedList cycle start: " + findCycleStart(head).value);
+
+        head.next.next.next.next.next.next = head.next.next.next;
+        System.out.println("LinkedList cycle start: " + findCycleStart(head).value);
+
+        head.next.next.next.next.next.next = head;
+        System.out.println("LinkedList cycle start: " + findCycleStart(head).value);
+    }
+
+    public static ListNode findCycleStart(ListNode head) {
+        ListNode fast = head, slow = head;
+        int cycleLength = 0;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                cycleLength = CalculateLength(slow);
+                break;
+            }
+        }
+        return findStart(head, cycleLength);
+    }
+
+    private static ListNode findStart(ListNode head, int cycleLength) {
+        ListNode pointer1 = head, pointer2 = head;
+        // move pointer2 ahead 'cycleLength' nodes
+        while (cycleLength > 0) {
+            pointer2 = pointer2.next;
+            cycleLength--;
+        }
+
+        // increment both pointers until they meet at the start of the cycle
+        while (pointer1 != pointer2) {
+            pointer1 = pointer1.next;
+            pointer2 = pointer2.next;
+        }
+
+        return pointer1;
+    }
+
+    private static int CalculateLength(ListNode start) {
+        int length = 0;
+        ListNode startPointer = start;
+        do {
+            start = start.next;
+            length++;
+        } while (start != startPointer);
+        return length;
+    }
+}
---
+++ src/EducativeIo/GrokkingTheCodingInterview/Ch04_FastAndSlowPointers/P2_StartOfLinkedListCycle/Java/SolutionTest.java
@@ -0,0 +1,84 @@
+package EducativeIo.GrokkingTheCodingInterview.Ch04_FastAndSlowPointers.P2_StartOfLinkedListCycle.Java;
+
+import static org.junit.jupiter.api.Assertions.*;
+import java.time.Duration;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class SolutionTest {
+
+    Solution solution;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        solution = new Solution();
+    }
+
+    @AfterEach
+    public void tearDown() throws Exception {
+        solution = null;
+    }
+
+    @Test
+    public void MainFunction() {
+        assertTimeout(Duration.ofMillis(500), () -> {
+            String[] args = new String[0];
+            assertAll(() -> Solution.main(args));
+        });
+    }
+
+    @Test
+    public void TrivialCase1() {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next = new ListNode(6);
+
+        head.next.next.next.next.next.next = head.next.next;
+        assertTimeout(Duration.ofMillis(500), () -> {
+            int expected = 3;
+            int actual = Solution.findCycleStart(head).value;
+            assertEquals(expected, actual);
+        });
+    }
+
+    @Test
+    public void TrivialCase2() {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next = new ListNode(6);
+
+        head.next.next.next.next.next.next = head.next.next;
+        head.next.next.next.next.next.next = head.next.next.next;
+        assertTimeout(Duration.ofMillis(500), () -> {
+            int expected = 4;
+            int actual = Solution.findCycleStart(head).value;
+            assertEquals(expected, actual);
+        });
+    }
+
+    @Test
+    public void TrivialCase3() {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next = new ListNode(6);
+
+        head.next.next.next.next.next.next = head.next.next;
+        head.next.next.next.next.next.next = head.next.next.next;
+        head.next.next.next.next.next.next = head;
+        assertTimeout(Duration.ofMillis(500), () -> {
+            int expected = 1;
+            int actual = Solution.findCycleStart(head).value;
+            assertEquals(expected, actual);
+        });
+    }
+}
