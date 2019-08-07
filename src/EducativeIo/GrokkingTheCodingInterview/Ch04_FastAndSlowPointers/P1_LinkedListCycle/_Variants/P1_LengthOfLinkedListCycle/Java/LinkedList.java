c2fa1b9782eb72f32d6acd000bb4c0f5b12adb91
Manas Talukdar <manas.talukdar@gmail.com>
2019-08-07 01:12:31 -0700

    Length of Linked List Cycle - WIP.

---
+++ src/EducativeIo/GrokkingTheCodingInterview/Ch04_FastAndSlowPointers/P1_LinkedListCycle/_Variants/P1_LengthOfLinkedListCycle/Java/LinkedList.java
@@ -0,0 +1,10 @@
+package EducativeIo.GrokkingTheCodingInterview.Ch04_FastAndSlowPointers.P1_LinkedListCycle._Variants.P1_LengthOfLinkedListCycle.Java;
+
+class ListNode {
+    int value = 0;
+    ListNode next;
+
+    ListNode(int value) {
+        this.value = value;
+    }
+}
