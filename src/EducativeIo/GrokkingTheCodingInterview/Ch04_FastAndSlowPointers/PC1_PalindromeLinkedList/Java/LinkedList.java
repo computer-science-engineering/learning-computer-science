ad5030351ef519ed59acd9a58c366d7e4abcfda1
Manas Talukdar <manas.talukdar@gmail.com>
2019-08-08 14:40:51 -0700

    Palindrome LinkedList

---
+++ src/EducativeIo/GrokkingTheCodingInterview/Ch04_FastAndSlowPointers/PC1_PalindromeLinkedList/Java/LinkedList.java
@@ -0,0 +1,10 @@
+package EducativeIo.GrokkingTheCodingInterview.Ch04_FastAndSlowPointers.PC1_PalindromeLinkedList.Java;
+
+class ListNode {
+    int value = 0;
+    ListNode next;
+
+    ListNode(int value) {
+        this.value = value;
+    }
+}
