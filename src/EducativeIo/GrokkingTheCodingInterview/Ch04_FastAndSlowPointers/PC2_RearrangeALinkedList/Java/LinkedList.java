b2867934f967884a27984ec4d6d5373a2add677b
Manas Talukdar <manas.talukdar@gmail.com>
2019-08-08 20:38:49 -0700

    Rearrange a LinkedList

---
+++ src/EducativeIo/GrokkingTheCodingInterview/Ch04_FastAndSlowPointers/PC2_RearrangeALinkedList/Java/LinkedList.java
@@ -0,0 +1,10 @@
+package EducativeIo.GrokkingTheCodingInterview.Ch04_FastAndSlowPointers.PC2_RearrangeALinkedList.Java;
+
+class ListNode {
+    int value = 0;
+    ListNode next;
+
+    ListNode(int value) {
+        this.value = value;
+    }
+}
