687abb10e15be33117619410e7f9838339b30557
Manas Talukdar <manas.talukdar@gmail.com>
2019-08-11 13:39:55 -0700

    Insert Interval

--- src/EducativeIo/GrokkingTheCodingInterview/Ch05_MergeIntervals/P2_InsertInterval/Java/SolutionTest.java
+++ src/EducativeIo/GrokkingTheCodingInterview/Ch05_MergeIntervals/P2_InsertInterval/Java/SolutionTest.java
@@ -2,6 +2,9 @@ package EducativeIo.GrokkingTheCodingInterview.Ch05_MergeIntervals.P2_InsertInte
 
 import static org.junit.jupiter.api.Assertions.*;
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -30,11 +33,55 @@ public class SolutionTest {
 
     @Test
     public void TrivialCase1() {
-        // input = ;
+        List<Interval> intervals = new ArrayList<Interval>();
+        intervals.add(new Interval(1, 3));
+        intervals.add(new Interval(5, 7));
+        intervals.add(new Interval(8, 12));
+        Interval newInterval = new Interval(4, 6);
         assertTimeout(Duration.ofMillis(500), () -> {
-            // expected = ;
-            // actual = Solution.;
-            // assertEquals(expected, actual);
+            List<List<Integer>> expected =
+                    Arrays.asList(Arrays.asList(1, 3), Arrays.asList(4, 7), Arrays.asList(8, 12));
+            List<List<Integer>> actual = new ArrayList<>();
+            List<Interval> result = Solution.insert(intervals, newInterval);
+            for (Interval interval : result) {
+                actual.add(Arrays.asList(interval.start, interval.end));
+            }
+            assertEquals(expected, actual);
+        });
+    }
+
+    @Test
+    public void TrivialCase2() {
+        List<Interval> intervals = new ArrayList<Interval>();
+        intervals.add(new Interval(1, 3));
+        intervals.add(new Interval(5, 7));
+        intervals.add(new Interval(8, 12));
+        Interval newInterval = new Interval(4, 10);
+        assertTimeout(Duration.ofMillis(500), () -> {
+            List<List<Integer>> expected = Arrays.asList(Arrays.asList(1, 3), Arrays.asList(4, 12));
+            List<List<Integer>> actual = new ArrayList<>();
+            List<Interval> result = Solution.insert(intervals, newInterval);
+            for (Interval interval : result) {
+                actual.add(Arrays.asList(interval.start, interval.end));
+            }
+            assertEquals(expected, actual);
+        });
+    }
+
+    @Test
+    public void TrivialCase3() {
+        List<Interval> intervals = new ArrayList<Interval>();
+        intervals.add(new Interval(2, 3));
+        intervals.add(new Interval(5, 7));
+        Interval newInterval = new Interval(1, 4);
+        assertTimeout(Duration.ofMillis(500), () -> {
+            List<List<Integer>> expected = Arrays.asList(Arrays.asList(1, 4), Arrays.asList(5, 7));
+            List<List<Integer>> actual = new ArrayList<>();
+            List<Interval> result = Solution.insert(intervals, newInterval);
+            for (Interval interval : result) {
+                actual.add(Arrays.asList(interval.start, interval.end));
+            }
+            assertEquals(expected, actual);
         });
     }
 }
