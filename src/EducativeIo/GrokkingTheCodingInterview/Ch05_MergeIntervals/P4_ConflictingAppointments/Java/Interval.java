6ad433a0a82ace3f21a9aa2fbb6c545a03948a83
Manas Talukdar <manas.talukdar@gmail.com>
2019-08-12 00:37:48 -0700

    Conflicting Appointments

---
+++ src/EducativeIo/GrokkingTheCodingInterview/Ch05_MergeIntervals/P4_ConflictingAppointments/Java/Interval.java
@@ -0,0 +1,11 @@
+package EducativeIo.GrokkingTheCodingInterview.Ch05_MergeIntervals.P4_ConflictingAppointments.Java;
+
+class Interval {
+    int start;
+    int end;
+
+    public Interval(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+}
