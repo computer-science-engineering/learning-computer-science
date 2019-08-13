359ca2304e4b6f8cd1a62e733be0b652d836a747
Manas Talukdar <manas.talukdar@gmail.com>
2019-08-13 01:01:12 -0700

    Minimum Meeting Rooms - WIP.

---
+++ src/EducativeIo/GrokkingTheCodingInterview/Ch05_MergeIntervals/PC1_MinimumMeetingRooms/Java/Solution.java
@@ -0,0 +1,65 @@
+package EducativeIo.GrokkingTheCodingInterview.Ch05_MergeIntervals.PC1_MinimumMeetingRooms.Java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) {
+        List<Meeting> input = new ArrayList<Meeting>() {
+            {
+                add(new Meeting(4, 5));
+                add(new Meeting(2, 3));
+                add(new Meeting(2, 4));
+                add(new Meeting(3, 5));
+            }
+        };
+        int result = findMinimumMeetingRooms(input);
+        System.out.println("Minimum meeting rooms required: " + result);
+
+        input = new ArrayList<Meeting>() {
+            {
+                add(new Meeting(1, 4));
+                add(new Meeting(2, 5));
+                add(new Meeting(7, 9));
+            }
+        };
+        result = findMinimumMeetingRooms(input);
+        System.out.println("Minimum meeting rooms required: " + result);
+
+        input = new ArrayList<Meeting>() {
+            {
+                add(new Meeting(6, 7));
+                add(new Meeting(2, 4));
+                add(new Meeting(8, 12));
+            }
+        };
+        result = findMinimumMeetingRooms(input);
+        System.out.println("Minimum meeting rooms required: " + result);
+
+        input = new ArrayList<Meeting>() {
+            {
+                add(new Meeting(1, 4));
+                add(new Meeting(2, 3));
+                add(new Meeting(3, 6));
+            }
+        };
+        result = findMinimumMeetingRooms(input);
+        System.out.println("Minimum meeting rooms required: " + result);
+
+        input = new ArrayList<Meeting>() {
+            {
+                add(new Meeting(4, 5));
+                add(new Meeting(2, 3));
+                add(new Meeting(2, 4));
+                add(new Meeting(3, 5));
+            }
+        };
+        result = findMinimumMeetingRooms(input);
+        System.out.println("Minimum meeting rooms required: " + result);
+    }
+
+    public static int findMinimumMeetingRooms(List<Meeting> meetings) {
+        // TODO: Write your code here
+        return -1;
+    }
+}
