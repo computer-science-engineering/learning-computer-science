eceb86347779bfbd2ed22b50a9969b0b3ac35dce
Manas Talukdar <manas.talukdar@gmail.com>
2019-08-15 00:10:51 -0700

    Employee Free Time - WIP.

---
+++ src/EducativeIo/GrokkingTheCodingInterview/Ch05_MergeIntervals/PC3_EmployeeFreeTime/Java/Interval.java
@@ -0,0 +1,11 @@
+package EducativeIo.GrokkingTheCodingInterview.Ch05_MergeIntervals.PC3_EmployeeFreeTime.Java;
+
+class Interval {
+    int start;
+    int end;
+
+    public Interval(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+}
---
+++ src/EducativeIo/GrokkingTheCodingInterview/Ch05_MergeIntervals/PC3_EmployeeFreeTime/Java/Solution.java
@@ -0,0 +1,43 @@
+package EducativeIo.GrokkingTheCodingInterview.Ch05_MergeIntervals.PC3_EmployeeFreeTime.Java;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) {
+        List<List<Interval>> input = new ArrayList<>();
+        input.add(new ArrayList<Interval>(Arrays.asList(new Interval(1, 3), new Interval(5, 6))));
+        input.add(new ArrayList<Interval>(Arrays.asList(new Interval(2, 3), new Interval(6, 8))));
+        List<Interval> result = findEmployeeFreeTime(input);
+        System.out.print("Free intervals: ");
+        for (Interval interval : result)
+            System.out.print("[" + interval.start + ", " + interval.end + "] ");
+        System.out.println();
+
+        input = new ArrayList<>();
+        input.add(new ArrayList<Interval>(Arrays.asList(new Interval(1, 3), new Interval(9, 12))));
+        input.add(new ArrayList<Interval>(Arrays.asList(new Interval(2, 4))));
+        input.add(new ArrayList<Interval>(Arrays.asList(new Interval(6, 8))));
+        result = findEmployeeFreeTime(input);
+        System.out.print("Free intervals: ");
+        for (Interval interval : result)
+            System.out.print("[" + interval.start + ", " + interval.end + "] ");
+        System.out.println();
+
+        input = new ArrayList<>();
+        input.add(new ArrayList<Interval>(Arrays.asList(new Interval(1, 3))));
+        input.add(new ArrayList<Interval>(Arrays.asList(new Interval(2, 4))));
+        input.add(new ArrayList<Interval>(Arrays.asList(new Interval(3, 5), new Interval(7, 9))));
+        result = findEmployeeFreeTime(input);
+        System.out.print("Free intervals: ");
+        for (Interval interval : result)
+            System.out.print("[" + interval.start + ", " + interval.end + "] ");
+    }
+
+    public static List<Interval> findEmployeeFreeTime(List<List<Interval>> schedule) {
+        List<Interval> result = new ArrayList<>();
+        // TODO: Write your code here
+        return result;
+    }
+}
