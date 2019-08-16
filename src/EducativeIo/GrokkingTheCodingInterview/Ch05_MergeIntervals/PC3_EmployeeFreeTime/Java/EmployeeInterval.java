e06bd379e321b1def63f6cc5d9dc8fd91b000b53
Manas Talukdar <manas.talukdar@gmail.com>
2019-08-15 21:59:25 -0700

    Employee Free Time

---
+++ src/EducativeIo/GrokkingTheCodingInterview/Ch05_MergeIntervals/PC3_EmployeeFreeTime/Java/EmployeeInterval.java
@@ -0,0 +1,13 @@
+package EducativeIo.GrokkingTheCodingInterview.Ch05_MergeIntervals.PC3_EmployeeFreeTime.Java;
+
+class EmployeeInterval {
+    Interval interval; // interval representing employee's working hours
+    int employeeIndex; // index of the list containing working hours of this employee
+    int intervalIndex; // index of the interval in the employee list
+
+    public EmployeeInterval(Interval interval, int employeeIndex, int intervalIndex) {
+        this.interval = interval;
+        this.employeeIndex = employeeIndex;
+        this.intervalIndex = intervalIndex;
+    }
+}
--- src/EducativeIo/GrokkingTheCodingInterview/Ch05_MergeIntervals/PC3_EmployeeFreeTime/Java/Solution.java
+++ src/EducativeIo/GrokkingTheCodingInterview/Ch05_MergeIntervals/PC3_EmployeeFreeTime/Java/Solution.java
@@ -3,6 +3,7 @@ package EducativeIo.GrokkingTheCodingInterview.Ch05_MergeIntervals.PC3_EmployeeF
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.PriorityQueue;
 
 public class Solution {
     public static void main(String[] args) {
@@ -37,7 +38,34 @@ public class Solution {
 
     public static List<Interval> findEmployeeFreeTime(List<List<Interval>> schedule) {
         List<Interval> result = new ArrayList<>();
-        // TODO: Write your code here
+        // PriorityQueue to store one interval from each employee
+        PriorityQueue<EmployeeInterval> minHeap =
+                new PriorityQueue<>((a, b) -> Integer.compare(a.interval.start, b.interval.start));
+
+        // insert the first interval of each employee to the queue
+        for (int i = 0; i < schedule.size(); i++)
+            minHeap.offer(new EmployeeInterval(schedule.get(i).get(0), i, 0));
+
+        Interval previousInterval = minHeap.peek().interval;
+        while (!minHeap.isEmpty()) {
+            EmployeeInterval queueTop = minHeap.poll();
+            // if previousInterval is not overlapping with the next interval, insert a free interval
+            if (previousInterval.end < queueTop.interval.start) {
+                result.add(new Interval(previousInterval.end, queueTop.interval.start));
+                previousInterval = queueTop.interval;
+            } else { // overlapping intervals, update the previousInterval if needed
+                if (previousInterval.end < queueTop.interval.end)
+                    previousInterval = queueTop.interval;
+            }
+
+            // if there are more intervals available for the same employee, add their next interval
+            List<Interval> employeeSchedule = schedule.get(queueTop.employeeIndex);
+            if (employeeSchedule.size() > queueTop.intervalIndex + 1) {
+                minHeap.offer(new EmployeeInterval(employeeSchedule.get(queueTop.intervalIndex + 1),
+                        queueTop.employeeIndex, queueTop.intervalIndex + 1));
+            }
+        }
+
         return result;
     }
 }
