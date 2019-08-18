ed2a2921f87f207938afce67daddcbd43276227d
Manas Talukdar <manas.talukdar@gmail.com>
2019-08-17 23:35:26 -0700

    refactor

---
+++ src/EducativeIo/GrokkingTheCodingInterview/Ch06_CyclicSort/P4_FindTheDuplicateNumber/Java/Solution.java
@@ -0,0 +1,14 @@
+package EducativeIo.GrokkingTheCodingInterview.Ch06_CyclicSort.P4_FindTheDuplicateNumber.Java;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(findNumber(new int[] {1, 4, 4, 3, 2}));
+        System.out.println(findNumber(new int[] {2, 1, 3, 3, 5, 4}));
+        System.out.println(findNumber(new int[] {2, 4, 1, 4, 4}));
+    }
+
+    public static int findNumber(int[] nums) {
+        // TODO: Write your code here
+        return -1;
+    }
+}
