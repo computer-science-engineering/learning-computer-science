59e300f9e4ca188c006b5f421ea47bfbe4df92e5
Manas Talukdar <manas.talukdar@gmail.com>
2019-08-21 17:01:13 -0700

    WIP.

--- src/EducativeIo/GrokkingTheCodingInterview/Ch06_CyclicSort/P4_FindTheDuplicateNumber/Java/Solution.java
+++ src/EducativeIo/GrokkingTheCodingInterview/Ch06_CyclicSort/P4_FindTheDuplicateNumber/Java/Solution.java
@@ -24,9 +24,47 @@ public class Solution {
         return -1;
     }
 
+    // O(1) space without modifying the input array
+    public static int findDuplicate(int[] nums) {
+        int slow = 0, fast = 0;
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        // find cycle length
+        int current = nums[slow];
+        int cycleLength = 0;
+        do {
+            current = nums[current];
+            cycleLength++;
+        } while (current != nums[slow]);
+
+        return findStart(nums, cycleLength);
+    }
+
+
     private static void swap(int[] arr, int i, int j) {
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
     }
+
+    private static int findStart(int[] arr, int cycleLength) {
+        int pointer1 = arr[0], pointer2 = arr[0];
+        // move pointer2 ahead 'cycleLength' steps
+        while (cycleLength > 0) {
+            pointer2 = arr[pointer2];
+            cycleLength--;
+        }
+
+        // increment both pointers until they meet at the start of the cycle
+        while (pointer1 != pointer2) {
+            pointer1 = arr[pointer1];
+            pointer2 = arr[pointer2];
+        }
+
+        return pointer1;
+    }
+
 }
