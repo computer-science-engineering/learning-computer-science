af5b25808a03d2ec0aba848b04ea3c4c757a22e0
Manas Talukdar <manas.talukdar@gmail.com>
2019-08-24 15:50:47 -0700

    Reverse a Sub-list

---
+++ src/EducativeIo/GrokkingTheCodingInterview/Ch07_InPlaceReversalOfALinkedList/P2_ReverseASublist/Java/ListNode.java
@@ -0,0 +1,10 @@
+package EducativeIo.GrokkingTheCodingInterview.Ch07_InPlaceReversalOfALinkedList.P2_ReverseASublist.Java;
+
+class ListNode {
+    int value = 0;
+    ListNode next;
+
+    ListNode(int value) {
+        this.value = value;
+    }
+}
