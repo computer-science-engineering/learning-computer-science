2f3199f5eb4ee40f12b41ecd5fec7187c050f98c
Manas Talukdar <manas.talukdar@gmail.com>
2019-08-26 17:28:09 -0700

    Rotate a LinkedList

---
+++ src/EducativeIo/GrokkingTheCodingInterview/Ch07_InPlaceReversalOfALinkedList/PC2_RotateALinkedList/Java/ListNode.java
@@ -0,0 +1,10 @@
+package EducativeIo.GrokkingTheCodingInterview.Ch07_InPlaceReversalOfALinkedList.PC2_RotateALinkedList.Java;
+
+class ListNode {
+    int value = 0;
+    ListNode next;
+
+    ListNode(int value) {
+        this.value = value;
+    }
+}
