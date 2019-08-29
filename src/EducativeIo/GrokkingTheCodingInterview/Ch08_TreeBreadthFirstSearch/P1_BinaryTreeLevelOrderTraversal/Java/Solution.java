c0f02d707a77c6e0c7ca31c0d558a3a208e547de
Manas Talukdar <manas.talukdar@gmail.com>
2019-08-28 19:37:45 -0700

    Binary Tree Level Order Traversal

--- src/EducativeIo/GrokkingTheCodingInterview/Ch08_TreeBreadthFirstSearch/P1_BinaryTreeLevelOrderTraversal/Java/Solution.java
+++ src/EducativeIo/GrokkingTheCodingInterview/Ch08_TreeBreadthFirstSearch/P1_BinaryTreeLevelOrderTraversal/Java/Solution.java
@@ -1,7 +1,9 @@
 package EducativeIo.GrokkingTheCodingInterview.Ch08_TreeBreadthFirstSearch.P1_BinaryTreeLevelOrderTraversal.Java;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class Solution {
     public static void main(String[] args) {
@@ -13,7 +15,28 @@ public class Solution {
 
     public static List<List<Integer>> traverse(TreeNode root) {
         List<List<Integer>> result = new ArrayList<List<Integer>>();
-        // TODO: Write your code here
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            List<Integer> currentLevel = new ArrayList<>(levelSize);
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode currentNode = queue.poll();
+                // add the node to the current level
+                currentLevel.add(currentNode.val);
+                // insert the children of current node in the queue
+                if (currentNode.left != null)
+                    queue.offer(currentNode.left);
+                if (currentNode.right != null)
+                    queue.offer(currentNode.right);
+            }
+            result.add(currentLevel);
+        }
+
         return result;
     }
 }
