5689f66984b6f17e552ec42fe5d26c95ddd14630
Manas Talukdar <manas.talukdar@gmail.com>
2019-08-31 12:57:59 -0700

    Level Averages in a Binary Tree

---
+++ src/EducativeIo/GrokkingTheCodingInterview/Ch08_TreeBreadthFirstSearch/P4_LevelAveragesInABinaryTree/Java/SolutionTest.java
@@ -0,0 +1,48 @@
+package EducativeIo.GrokkingTheCodingInterview.Ch08_TreeBreadthFirstSearch.P4_LevelAveragesInABinaryTree.Java;
+
+import static org.junit.jupiter.api.Assertions.*;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class SolutionTest {
+
+    Solution solution;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        solution = new Solution();
+    }
+
+    @AfterEach
+    public void tearDown() throws Exception {
+        solution = null;
+    }
+
+    @Test
+    public void MainFunction() {
+        assertTimeout(Duration.ofMillis(500), () -> {
+            String[] args = new String[0];
+            assertAll(() -> Solution.main(args));
+        });
+    }
+
+    @Test
+    public void TrivialCase1() {
+        TreeNode root = new TreeNode(12);
+        root.left = new TreeNode(7);
+        root.right = new TreeNode(1);
+        root.left.left = new TreeNode(9);
+        root.left.right = new TreeNode(2);
+        root.right.left = new TreeNode(10);
+        root.right.right = new TreeNode(5);
+        assertTimeout(Duration.ofMillis(500), () -> {
+            List<Double> expected = Arrays.asList(12.0, 4.0, 6.5);
+            List<Double> actual = Solution.findLevelAverages(root);
+            assertEquals(expected, actual);
+        });
+    }
+}
