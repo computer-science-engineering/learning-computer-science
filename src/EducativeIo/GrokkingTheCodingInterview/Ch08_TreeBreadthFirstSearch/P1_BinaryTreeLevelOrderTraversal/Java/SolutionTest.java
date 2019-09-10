045029c7ef25be50ba1eb8834ef67884fb386dce
Manas Talukdar <manas.talukdar@gmail.com>
2019-09-09 17:21:16 -0700

    cleanup.

--- src/EducativeIo/GrokkingTheCodingInterview/Ch08_TreeBreadthFirstSearch/P1_BinaryTreeLevelOrderTraversal/Java/SolutionTest.java
+++ src/EducativeIo/GrokkingTheCodingInterview/Ch08_TreeBreadthFirstSearch/P1_BinaryTreeLevelOrderTraversal/Java/SolutionTest.java
@@ -2,7 +2,6 @@ package EducativeIo.GrokkingTheCodingInterview.Ch08_TreeBreadthFirstSearch.P1_Bi
 
 import static org.junit.jupiter.api.Assertions.*;
 import java.time.Duration;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.Test;
