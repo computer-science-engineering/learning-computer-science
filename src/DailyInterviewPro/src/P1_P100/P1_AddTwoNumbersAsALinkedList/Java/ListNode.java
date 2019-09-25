4285440712f4604bbf8a40ea95fbcfae0f69afc3
Manas Talukdar <manas.talukdar@gmail.com>
2019-09-25 11:19:53 -0700

    Add two numbers as a linked list

---
+++ src/DailyInterviewPro/src/P1_P100/P1_AddTwoNumbersAsALinkedList/Java/ListNode.java
@@ -0,0 +1,10 @@
+package DailyInterviewPro.src.P1_P100.P1_AddTwoNumbersAsALinkedList.Java;
+
+class ListNode {
+    int value = 0;
+    ListNode next;
+
+    ListNode(int value) {
+        this.value = value;
+    }
+}
---
+++ src/DailyInterviewPro/src/P1_P100/P1_AddTwoNumbersAsALinkedList/Java/Solution.java
@@ -0,0 +1,44 @@
+package DailyInterviewPro.src.P1_P100.P1_AddTwoNumbersAsALinkedList.Java;
+
+public class Solution {
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
+
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
+
+        ListNode result = addTwoNumbers(l1, l2);
+        while (result != null) {
+            System.out.print(result.value + " ");
+            result = result.next;
+        }
+    }
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode c1 = l1;
+        ListNode c2 = l2;
+        ListNode sentinel = new ListNode(0);
+        ListNode d = sentinel;
+        int sum = 0;
+        while (c1 != null || c2 != null) {
+            sum /= 10;
+            if (c1 != null) {
+                sum += c1.value;
+                c1 = c1.next;
+            }
+            if (c2 != null) {
+                sum += c2.value;
+                c2 = c2.next;
+            }
+            d.next = new ListNode(sum % 10);
+            d = d.next;
+        }
+        if (sum / 10 == 1) {
+            d.next = new ListNode(1);
+        }
+        return sentinel.next;
+    }
+}
