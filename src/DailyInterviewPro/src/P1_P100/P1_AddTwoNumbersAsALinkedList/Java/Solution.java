641b9273829629a43beeb819307e56271e935adb
Manas Talukdar <manas.talukdar@gmail.com>
2019-09-25 13:35:58 -0700

    Update.

--- src/DailyInterviewPro/src/P1_P100/P1_AddTwoNumbersAsALinkedList/Java/Solution.java
+++ src/DailyInterviewPro/src/P1_P100/P1_AddTwoNumbersAsALinkedList/Java/Solution.java
@@ -18,27 +18,24 @@ public class Solution {
     }
 
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode c1 = l1;
-        ListNode c2 = l2;
-        ListNode sentinel = new ListNode(0);
-        ListNode d = sentinel;
-        int sum = 0;
-        while (c1 != null || c2 != null) {
-            sum /= 10;
-            if (c1 != null) {
-                sum += c1.value;
-                c1 = c1.next;
-            }
-            if (c2 != null) {
-                sum += c2.value;
-                c2 = c2.next;
-            }
-            d.next = new ListNode(sum % 10);
-            d = d.next;
+        ListNode dummyHead = new ListNode(0);
+        ListNode p = l1, q = l2, curr = dummyHead;
+        int carry = 0;
+        while (p != null || q != null) {
+            int x = (p != null) ? p.value : 0;
+            int y = (q != null) ? q.value : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (p != null)
+                p = p.next;
+            if (q != null)
+                q = q.next;
         }
-        if (sum / 10 == 1) {
-            d.next = new ListNode(1);
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
         }
-        return sentinel.next;
+        return dummyHead.next;
     }
 }
