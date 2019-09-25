21f8d9bc327fd69b177b7054cbf61f7d1ecccba4
Manas Talukdar <manas.talukdar@gmail.com>
2019-09-25 15:46:12 -0700

    Permutations

---
+++ src/EducativeIo/Courses/GrokkingTheCodingInterview/Ch11_Subsets/P3_Permutations/Java/Solution.java
@@ -0,0 +1,37 @@
+package EducativeIo.Courses.GrokkingTheCodingInterview.Ch11_Subsets.P3_Permutations.Java;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Solution {
+    public static void main(String[] args) {
+        List<List<Integer>> result = findPermutations(new int[] {1, 3, 5});
+        System.out.print("Here are all the permutations: " + result);
+    }
+
+    public static List<List<Integer>> findPermutations(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Queue<List<Integer>> permutations = new LinkedList<>();
+        permutations.add(new ArrayList<>());
+        for (int currentNumber : nums) {
+            // we will take all existing permutations and add the current number to create new
+            // permutations
+            int n = permutations.size();
+            for (int i = 0; i < n; i++) {
+                List<Integer> oldPermutation = permutations.poll();
+                // create a new permutation by adding the current number at every position
+                for (int j = 0; j <= oldPermutation.size(); j++) {
+                    List<Integer> newPermutation = new ArrayList<Integer>(oldPermutation);
+                    newPermutation.add(j, currentNumber);
+                    if (newPermutation.size() == nums.length)
+                        result.add(newPermutation);
+                    else
+                        permutations.add(newPermutation);
+                }
+            }
+        }
+        return result;
+    }
+}
