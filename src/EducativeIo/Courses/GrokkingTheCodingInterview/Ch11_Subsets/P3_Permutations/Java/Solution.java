dbc15bec01d1decb7ca8bfb578159787140b75f4
Manas Talukdar <manas.talukdar@gmail.com>
2019-09-25 15:50:49 -0700

    Added recursive solution.

--- src/EducativeIo/Courses/GrokkingTheCodingInterview/Ch11_Subsets/P3_Permutations/Java/Solution.java
+++ src/EducativeIo/Courses/GrokkingTheCodingInterview/Ch11_Subsets/P3_Permutations/Java/Solution.java
@@ -34,4 +34,24 @@ public class Solution {
         }
         return result;
     }
+
+    public static List<List<Integer>> generatePermutations(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        generatePermutationsRecursive(nums, 0, new ArrayList<Integer>(), result);
+        return result;
+    }
+
+    private static void generatePermutationsRecursive(int[] nums, int index,
+            List<Integer> currentPermutation, List<List<Integer>> result) {
+        if (index == nums.length) {
+            result.add(currentPermutation);
+        } else {
+            // create a new permutation by adding the current number at every position
+            for (int i = 0; i <= currentPermutation.size(); i++) {
+                List<Integer> newPermutation = new ArrayList<Integer>(currentPermutation);
+                newPermutation.add(i, nums[index]);
+                generatePermutationsRecursive(nums, index + 1, newPermutation, result);
+            }
+        }
+    }
 }
