594ce324470c313bc4c9711a3af92c77d97592d0
Manas Talukdar <manas.talukdar@gmail.com>
2019-10-03 13:50:46 -0700

    4. Validate Balanced Parentheses

---
+++ src/DailyInterviewPro/src/P1_P100/P4_ValidateBalancedParentheses/Java/Solution.java
@@ -0,0 +1,25 @@
+package DailyInterviewPro.src.P1_P100.P4_ValidateBalancedParentheses.Java;
+
+import java.util.Stack;
+
+public class Solution {
+    public static void main(String[] args) {
+        String s = "((()))";
+        System.out.println(isValid(s));
+    }
+
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for (char c : s.toCharArray()) {
+            if (c == '(')
+                stack.push(')');
+            else if (c == '{')
+                stack.push('}');
+            else if (c == '[')
+                stack.push(']');
+            else if (stack.isEmpty() || stack.pop() != c)
+                return false;
+        }
+        return stack.isEmpty();
+    }
+}
