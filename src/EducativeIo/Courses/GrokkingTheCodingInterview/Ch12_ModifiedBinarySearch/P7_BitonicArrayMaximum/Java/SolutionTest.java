5ae613cefbaab7d389ace218afbc912c35f5ed75
Manas Talukdar <manas.talukdar@gmail.com>
2019-10-11 12:54:27 -0700

    Bitonic Array Maximum

---
+++ src/EducativeIo/Courses/GrokkingTheCodingInterview/Ch12_ModifiedBinarySearch/P7_BitonicArrayMaximum/Java/SolutionTest.java
@@ -0,0 +1,71 @@
+package EducativeIo.Courses.GrokkingTheCodingInterview.Ch12_ModifiedBinarySearch.P7_BitonicArrayMaximum.Java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class SolutionTest {
+
+    Solution solution;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        solution = new Solution();
+    }
+
+    @AfterEach
+    public void tearDown() throws Exception {
+        solution = null;
+    }
+
+    @Test
+    public void MainFunction() {
+        assertTimeout(Duration.ofMillis(500), () -> {
+            String[] args = new String[0];
+            assertAll(() -> Solution.main(args));
+        });
+    }
+
+    @Test
+    public void TrivialCase1() {
+        int[] arr = new int[] {1, 3, 8, 12, 4, 2};
+        assertTimeout(Duration.ofMillis(500), () -> {
+            int expected = 12;
+            int actual = Solution.findMax(arr);
+            assertEquals(expected, actual);
+        });
+    }
+
+    @Test
+    public void TrivialCase2() {
+        int[] arr = new int[] {3, 8, 3, 1};
+        assertTimeout(Duration.ofMillis(500), () -> {
+            int expected = 8;
+            int actual = Solution.findMax(arr);
+            assertEquals(expected, actual);
+        });
+    }
+
+    @Test
+    public void TrivialCase3() {
+        int[] arr = new int[] {1, 3, 8, 12};
+        assertTimeout(Duration.ofMillis(500), () -> {
+            int expected = 12;
+            int actual = Solution.findMax(arr);
+            assertEquals(expected, actual);
+        });
+    }
+
+    @Test
+    public void TrivialCase4() {
+        int[] arr = new int[] {10, 9, 8};
+        assertTimeout(Duration.ofMillis(500), () -> {
+            int expected = 10;
+            int actual = Solution.findMax(arr);
+            assertEquals(expected, actual);
+        });
+    }
+}
