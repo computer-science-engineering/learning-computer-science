8030839a7de39ce64d86ee3df5df8acbf2aa412c
Manas Talukdar <manas.talukdar@gmail.com>
2019-10-11 21:20:29 -0700

    Search Bitonic Array

--- src/EducativeIo/Courses/GrokkingTheCodingInterview/Ch12_ModifiedBinarySearch/PC1_SearchBitonicArray/Java/Solution.java
+++ src/EducativeIo/Courses/GrokkingTheCodingInterview/Ch12_ModifiedBinarySearch/PC1_SearchBitonicArray/Java/Solution.java
@@ -9,7 +9,52 @@ public class Solution {
     }
 
     public static int search(int[] arr, int key) {
-        // TODO: Write your code here
-        return -1;
+        int maxIndex = findMax(arr);
+        int keyIndex = binarySearch(arr, key, 0, maxIndex);
+        if (keyIndex != -1)
+            return keyIndex;
+        return binarySearch(arr, key, maxIndex + 1, arr.length - 1);
     }
+
+    // find index of the maximum value in a bitonic array
+    private static int findMax(int[] arr) {
+        int start = 0, end = arr.length - 1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] > arr[mid + 1]) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        // at the end of the while loop, 'start == end'
+        return start;
+    }
+
+    // order-agnostic binary search
+    private static int binarySearch(int[] arr, int key, int start, int end) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (key == arr[mid])
+                return mid;
+
+            if (arr[start] < arr[end]) { // ascending order
+                if (key < arr[mid]) {
+                    end = mid - 1;
+                } else { // key > arr[mid]
+                    start = mid + 1;
+                }
+            } else { // descending order
+                if (key > arr[mid]) {
+                    end = mid - 1;
+                } else { // key < arr[mid]
+                    start = mid + 1;
+                }
+            }
+        }
+        return -1; // element is not found
+    }
+
 }
