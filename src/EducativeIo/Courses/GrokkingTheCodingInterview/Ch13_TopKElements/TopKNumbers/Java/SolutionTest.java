d916ad5fc16bc11c222f1d7019b995f6b4554641
Manas Talukdar <manas.talukdar@gmail.com>
2019-10-13 01:06:53 -0700

    Top 'K' Numbers

---
+++ src/EducativeIo/Courses/GrokkingTheCodingInterview/Ch13_TopKElements/TopKNumbers/Java/SolutionTest.java
@@ -0,0 +1,55 @@
+package EducativeIo.Courses.GrokkingTheCodingInterview.Ch13_TopKElements.TopKNumbers.Java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class SolutionTest {
+
+    Solution solution;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        solution = new Solution();
+    }
+
+    @AfterEach
+    public void tearDown() throws Exception {
+        solution = null;
+    }
+
+    @Test
+    public void MainFunction() {
+        assertTimeout(Duration.ofMillis(500), () -> {
+            String[] args = new String[0];
+            assertAll(() -> Solution.main(args));
+        });
+    }
+
+    @Test
+    public void TrivialCase1() {
+        int[] nums = new int[] {3, 1, 5, 12, 2, 11};
+        int k = 3;
+        assertTimeout(Duration.ofMillis(500), () -> {
+            List<Integer> expected = Arrays.asList(5, 12, 11);
+            List<Integer> actual = Solution.findKLargestNumbers(nums, k);
+            assertEquals(expected, actual);
+        });
+    }
+
+    @Test
+    public void TrivialCase2() {
+        int[] nums = new int[] {5, 12, 11, -1, 12};
+        int k = 3;
+        assertTimeout(Duration.ofMillis(500), () -> {
+            List<Integer> expected = Arrays.asList(11, 12, 12);
+            List<Integer> actual = Solution.findKLargestNumbers(nums, k);
+            assertEquals(expected, actual);
+        });
+    }
+}
