1a7431930c1368ef43ec1231992a44a435c1806e
Manas Talukdar <manas.talukdar@gmail.com>
2019-10-13 16:52:40 -0700

    'K' Closest Points to the Origin

---
+++ src/EducativeIo/Courses/GrokkingTheCodingInterview/Ch13_TopKElements/P3_KClosestPointsToTheOrigin/Java/Point.java
@@ -0,0 +1,16 @@
+package EducativeIo.Courses.GrokkingTheCodingInterview.Ch13_TopKElements.P3_KClosestPointsToTheOrigin.Java;
+
+class Point {
+    int x;
+    int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int distFromOrigin() {
+        // ignoring sqrt
+        return (x * x) + (y * y);
+    }
+}
