79234a6ced62ebecf101f15c34983ed8583d79cc
Manas Talukdar <manas.talukdar@gmail.com>
2019-10-13 19:05:45 -0700

    'K' Closest Numbers

---
+++ src/EducativeIo/Courses/GrokkingTheCodingInterview/Ch13_TopKElements/P8_KClosestNumbers/Java/Entry.java
@@ -0,0 +1,11 @@
+package EducativeIo.Courses.GrokkingTheCodingInterview.Ch13_TopKElements.P8_KClosestNumbers.Java;
+
+class Entry {
+    int key;
+    int value;
+
+    public Entry(int key, int value) {
+        this.key = key;
+        this.value = value;
+    }
+}
