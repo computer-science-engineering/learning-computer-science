10af28d929e2489b1853de8f68bdbd8d20dc9804
Manas Talukdar <manas.talukdar@gmail.com>
2019-10-15 16:08:04 -0700

    Scheduling Tasks

---
+++ src/EducativeIo/Courses/GrokkingTheCodingInterview/Ch13_TopKElements/PC2_SchedulingTasks/Java/Solution.java
@@ -0,0 +1,51 @@
+package EducativeIo.Courses.GrokkingTheCodingInterview.Ch13_TopKElements.PC2_SchedulingTasks.Java;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+public class Solution {
+    public static void main(String[] args) {
+        char[] tasks = new char[] {'a', 'a', 'a', 'b', 'c', 'c'};
+        System.out.println(
+                "Minimum intervals needed to execute all tasks: " + scheduleTasks(tasks, 2));
+
+        tasks = new char[] {'a', 'b', 'a'};
+        System.out.println(
+                "Minimum intervals needed to execute all tasks: " + scheduleTasks(tasks, 3));
+    }
+
+    public static int scheduleTasks(char[] tasks, int k) {
+        int intervalCount = 0;
+        Map<Character, Integer> taskFrequencyMap = new HashMap<>();
+        for (char chr : tasks)
+            taskFrequencyMap.put(chr, taskFrequencyMap.getOrDefault(chr, 0) + 1);
+
+        PriorityQueue<Map.Entry<Character, Integer>> maxHeap =
+                new PriorityQueue<Map.Entry<Character, Integer>>(
+                        (e1, e2) -> e2.getValue() - e1.getValue());
+
+        // add all tasks to the max heap
+        maxHeap.addAll(taskFrequencyMap.entrySet());
+
+        while (!maxHeap.isEmpty()) {
+            List<Map.Entry<Character, Integer>> waitList = new ArrayList<>();
+            int n = k + 1; // try to execute as many as 'k+1' tasks from the max-heap
+            for (; n > 0 && !maxHeap.isEmpty(); n--) {
+                intervalCount++;
+                Map.Entry<Character, Integer> currentEntry = maxHeap.poll();
+                if (currentEntry.getValue() > 1) {
+                    currentEntry.setValue(currentEntry.getValue() - 1);
+                    waitList.add(currentEntry);
+                }
+            }
+            maxHeap.addAll(waitList); // put all the waiting list back on the heap
+            if (!maxHeap.isEmpty())
+                intervalCount += n; // we'll be having 'n' idle intervals for the next iteration
+        }
+
+        return intervalCount;
+    }
+}
