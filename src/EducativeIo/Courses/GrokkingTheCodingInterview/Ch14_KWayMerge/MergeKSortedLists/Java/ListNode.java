4a38c31c43059d72329f9437031a7244b2a0046b
Manas Talukdar <manas.talukdar@gmail.com>
2019-10-17 23:45:15 -0700

    Merge K Sorted Lists

---
+++ src/EducativeIo/Courses/GrokkingTheCodingInterview/Ch14_KWayMerge/MergeKSortedLists/Java/ListNode.java
@@ -0,0 +1,10 @@
+package EducativeIo.Courses.GrokkingTheCodingInterview.Ch14_KWayMerge.MergeKSortedLists.Java;
+
+class ListNode {
+    int value;
+    ListNode next;
+
+    ListNode(int value) {
+        this.value = value;
+    }
+}
