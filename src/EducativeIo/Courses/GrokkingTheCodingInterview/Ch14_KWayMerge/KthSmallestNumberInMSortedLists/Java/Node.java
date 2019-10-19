29a234d0d8af121fc3f9585ffc23113290876ae8
Manas Talukdar <manas.talukdar@gmail.com>
2019-10-18 23:03:24 -0700

    Kth Smallest Number in M Sorted Lists

---
+++ src/EducativeIo/Courses/GrokkingTheCodingInterview/Ch14_KWayMerge/KthSmallestNumberInMSortedLists/Java/Node.java
@@ -0,0 +1,11 @@
+package EducativeIo.Courses.GrokkingTheCodingInterview.Ch14_KWayMerge.KthSmallestNumberInMSortedLists.Java;
+
+class Node {
+    int elementIndex;
+    int arrayIndex;
+
+    Node(int elementIndex, int arrayIndex) {
+        this.elementIndex = elementIndex;
+        this.arrayIndex = arrayIndex;
+    }
+}
---
+++ src/EducativeIo/Courses/GrokkingTheCodingInterview/Ch14_KWayMerge/KthSmallestNumberInMSortedLists/Java/SolutionTest.java
@@ -0,0 +1,49 @@
+package EducativeIo.Courses.GrokkingTheCodingInterview.Ch14_KWayMerge.KthSmallestNumberInMSortedLists.Java;
+
+import static org.junit.jupiter.api.Assertions.*;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class SolutionTest {
+
+    Solution solution;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        solution = new Solution();
+    }
+
+    @AfterEach
+    public void tearDown() throws Exception {
+        solution = null;
+    }
+
+    @Test
+    public void MainFunction() {
+        assertTimeout(Duration.ofMillis(500), () -> {
+            String[] args = new String[0];
+            assertAll(() -> Solution.main(args));
+        });
+    }
+
+    @Test
+    public void TrivialCase1() {
+        Integer[] l1 = new Integer[] {2, 6, 8};
+        Integer[] l2 = new Integer[] {3, 6, 7};
+        Integer[] l3 = new Integer[] {1, 3, 4};
+        List<Integer[]> lists = new ArrayList<Integer[]>();
+        lists.add(l1);
+        lists.add(l2);
+        lists.add(l3);
+        int k = 5;
+        assertTimeout(Duration.ofMillis(500), () -> {
+            int expected = 4;
+            int actual = Solution.findKthSmallest(lists, k);
+            assertEquals(expected, actual);
+        });
+    }
+}
